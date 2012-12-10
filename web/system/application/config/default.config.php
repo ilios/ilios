ad2b2b4e7265f6ac7d1068082f19f2495bcf6954
Stefan Topfstedt <stefan.topfstedt@ucsf.edu>
2012-12-10 14:57:31 -0800

    replaced CodeIgniter class extension prefix from I2_ to Ilios_CI_ and renamed helper script filenames accordingly. refs #3052

--- web/system/application/config/default.config.php
+++ web/system/application/config/default.config.php
@@ -126,7 +126,7 @@ $config['enable_hooks'] = true;
 | http://codeigniter.com/user_guide/general/creating_libraries.html
 |
 */
-$config['subclass_prefix'] = 'I2_';
+$config['subclass_prefix'] = 'Ilios_CI_';
 
 
 /*
