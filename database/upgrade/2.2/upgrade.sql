c08787215f766aa950194149dc417cff18a3ab10
Jason Hedrick <jason.hedrick@ucsf.edu>
2012-12-13 15:18:45 -0800

    added indexes/foreign key constraints for user_x_user_role table, also updated alert_instigator and alert_hchanged tables to have custom-labelled foreign key constraints like the rest of the FK's seem to have, refs #2874, refs #1050

--- database/upgrade/2.2/upgrade.sql
+++ database/upgrade/2.2/upgrade.sql
@@ -8,10 +8,20 @@
 ALTER TABLE  `alert_instigator` ADD INDEX (`alert_id`);
 ALTER TABLE  `alert_instigator` ADD INDEX (`user_id`);
 ALTER TABLE  `alert_instigator` ADD INDEX  `alert_id_user_id` (`alert_id`,`user_id`);
-ALTER TABLE  `alert_instigator` ADD CONSTRAINT FOREIGN KEY (`alert_id`) REFERENCES `alert` (`alert_id`) ON DELETE CASCADE;
-ALTER TABLE  `alert_instigator` ADD CONSTRAINT FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE;
+ALTER TABLE  `alert_instigator` ADD CONSTRAINT `fkey_alert_instigator_alert_id` FOREIGN KEY (`alert_id`) REFERENCES `alert` (`alert_id`) ON DELETE CASCADE;
+ALTER TABLE  `alert_instigator` ADD CONSTRAINT `fkey_alert_instigator_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE;
 
 -- alert_change
 ALTER TABLE  `alert_change` ADD INDEX (`alert_id`);
 ALTER TABLE  `alert_change` ADD INDEX `alert_id_alert_change_type_id` (`alert_id`,`alert_change_type_id`);
-ALTER TABLE  `alert_change` ADD CONSTRAINT FOREIGN KEY (`alert_id`) REFERENCES `alert` (`alert_id`) ON DELETE CASCADE;
+ALTER TABLE  `alert_change` ADD CONSTRAINT `fkey_alert_change_alert_id` FOREIGN KEY (`alert_id`) REFERENCES `alert` (`alert_id`) ON DELETE CASCADE;
+
+
+-- add [composite] primary key and foreign key constraints to user_x_user_role table
+
+-- user_x_user_role
+ALTER TABLE  `user_x_user_role` ADD PRIMARY KEY (`user_id`,`user_role_id`) USING BTREE;
+ALTER TABLE  `user_x_user_role` ADD INDEX `user_x_user_role_user_id` (`user_id`);
+ALTER TABLE  `user_x_user_role` ADD INDEX `user_x_user_role_user_role_id` (`user_role_id`);
+ALTER TABLE  `user_x_user_role` ADD CONSTRAINT `fkey_user_x_user_role_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE RESTRICT;
+ALTER TABLE  `user_x_user_role` ADD CONSTRAINT `fkey_user_x_user_role_user_role_id` FOREIGN KEY (`user_role_id`) REFERENCES `user_role` (`user_role_id`) ON DELETE CASCADE ON UPDATE RESTRICT;
