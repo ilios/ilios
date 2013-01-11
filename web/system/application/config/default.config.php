f94b56cd8355ce0587458bd1a021ea3b80141db4
Stefan Topfstedt <stefan.topfstedt@ucsf.edu>
2013-01-11 15:55:27 -0800

    toggled "enable_query_strings" setting off since it was breaking redirect functionality

--- web/system/application/config/default.config.php
+++ web/system/application/config/default.config.php
@@ -155,7 +155,7 @@ $config['permitted_uri_chars'] = 'a-z 0-9~%.:_\-';
 |
 */
 $config['allow_get_array']		= TRUE;
-$config['enable_query_strings'] = TRUE;
+$config['enable_query_strings'] = FALSE;
 $config['controller_trigger']	= 'c';
 $config['function_trigger']		= 'm';
 $config['directory_trigger']	= 'd'; // experimental not currently in use
