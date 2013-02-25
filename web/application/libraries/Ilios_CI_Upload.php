534812a418a7583c2f60db9cec8362ecfe5ad346
Stefan Topfstedt <stefan.topfstedt@ucsf.edu>
2013-02-25 15:57:41 -0800

    initial check-in of extended upload class. refs #3218

---
+++ web/application/libraries/Ilios_CI_Upload.php
@@ -0,0 +1,39 @@
+<?php
+
+
+class Ilios_CI_Upload extends CI_Upload
+{
+    protected $_correctedMimeTypesMap = array(
+        '.docm|application/msword' => 'application/vnd.ms-word.document.macroEnabled.12',
+        '.docx|application/msword' => 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+        '.dotm|application/msword' => 'application/vnd.ms-word.template.macroEnabled.12',
+        '.dotx|application/msword' => 'application/vnd.openxmlformats-officedocument.wordprocessingml.template',
+        '.potm|application/vnd.ms-powerpoint' => 'application/vnd.ms-powerpoint.template.macroEnabled.12',
+        '.potx|application/vnd.ms-powerpoint' => 'application/vnd.openxmlformats-officedocument.presentationml.template',
+        '.pot|application/msword' => 'application/vnd.ms-powerpoint',
+        '.ppam|application/vnd.ms-powerpoint' => 'application/vnd.ms-powerpoint.addin.macroEnabled.12',
+        '.ppa|application/msword' => 'application/vnd.ms-powerpoint',
+        '.ppsm|application/vnd.ms-powerpoint' => 'application/vnd.ms-powerpoint.slideshow.macroEnabled.12',
+        '.ppsx|application/vnd.ms-powerpoint' => 'application/vnd.openxmlformats-officedocument.presentationml.slideshow',
+        '.pps|application/msword' => 'application/vnd.ms-powerpoint',
+        '.pptm|application/vnd.ms-powerpoint' => 'application/vnd.ms-powerpoint.presentation.macroEnabled.12',
+        '.pptx|application/vnd.ms-powerpoint' => 'application/vnd.openxmlformats-officedocument.presentationml.presentation',
+        '.ppt|application/msword' => 'application/vnd.ms-powerpoint',
+        '.xlam|application/vnd.ms-excel' => 'application/vnd.ms-excel.addin.macroEnabled.12',
+        '.xlsb|application/vnd.ms-excel' => 'application/vnd.ms-excel.sheet.binary.macroEnabled.12',
+        '.xlsm|application/vnd.ms-excel' => 'application/vnd.ms-excel.sheet.macroEnabled.12',
+        '.xlsx|application/vnd.ms-excel' => 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+        '.xltm|application/vnd.ms-excel' => 'application/vnd.ms-excel.template.macroEnabled.12',
+        '.xltx|application/vnd.ms-excel' => 'application/vnd.openxmlformats-officedocument.spreadsheetml.template'
+    );
+
+    public function getCorrectedMimeType ($fileExtension, $mimeType)
+    {
+        $key = strtolower($fileExtension . '|' . $mimeType);
+        if (array_key_exists($key, $this->_correctedMimeTypesMap)) {
+            return $this->_correctedMimeTypesMap[$key];
+        }
+        // no match? we assume that no correction was needed, and return the given mime-type.
+        return $mimeType;
+    }
+}
