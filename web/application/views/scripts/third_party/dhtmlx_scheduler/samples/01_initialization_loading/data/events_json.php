a055dd1baf627376d48fd73dabc08c89f7f4beff
Stefan Topfstedt <stefan.topfstedt@ucsf.edu>
2013-03-08 11:14:31 -0800

    removed docs, samples and connectors. refs #2744

--- web/application/views/scripts/third_party/dhtmlx_scheduler/samples/01_initialization_loading/data/events_json.php
+++
@@ -1,7 +0,0 @@
-<?php
-	require_once('../../../codebase/connector/scheduler_connector.php');
-	include ('../../common/config.php');
-
-	$scheduler = new JSONSchedulerConnector($res, $dbtype);
-	$scheduler->render_table("events","event_id","start_date,end_date,event_name,details");
-?>
