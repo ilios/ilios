ae20a52dba484a679c8be59bd00d4c42cbacc2a9
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2013-03-20 23:30:32 -0400

    reconfigured session cookie to expire when browser gets closed.

--- web/application/config/default.config.php
+++ web/application/config/default.config.php
@@ -246,7 +246,7 @@ $config['encryption_key'] = '%%ENCRYPTION_KEY%%';
 */
 $config['sess_cookie_name']		= 'ci_session';
 $config['sess_expiration']      = 14400;
-$config['sess_expire_on_close']	= FALSE;
+$config['sess_expire_on_close']	= TRUE;
 $config['sess_encrypt_cookie']	= FALSE;
 $config['sess_use_database']    = TRUE;
 $config['sess_table_name']		= 'ci_sessions';
