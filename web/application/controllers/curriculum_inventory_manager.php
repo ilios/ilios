49312ab18c662f6dcbfcc040ea7aa653f600a44f
Stefan Topfstedt <stefan.topfstedt@ucsf.edu>
2013-04-01 17:42:18 -0400

    converted line endings to unix. started implementing view action.

--- web/application/controllers/curriculum_inventory_manager.php
+++ web/application/controllers/curriculum_inventory_manager.php
@@ -27,7 +27,36 @@ class Curriculum_Inventory_Manager extends Ilios_Web_Controller
 
     public function view ()
     {
-        // @todo implement
+        $lang = $this->getLangToUse();
+
+        $data = array();
+        $data['lang'] = $lang;
+        $data['i18n'] =  $this->languagemap;
+        $data['institution_name'] = $this->config->item('ilios_institution_name');
+        $data['user_id'] = $this->session->userdata('uid');
+
+        // authorization check
+        if (! $this->session->userdata('has_admin_access')) {
+            $this->_viewAccessForbiddenPage($lang, $data);
+            return;
+        }
+
+        $schoolId = $this->session->userdata('school_id');
+        $schoolRow = $this->school->getRowForPrimaryKeyId($schoolId);
+
+        if (isset($schoolRow)) {
+            $data['school_id'] = $schoolId;
+            $data['school_name'] = $schoolRow->title;
+            $key = 'general.phrases.school_of';
+            $schoolOfStr = $this->languagemap->getI18NString($key, $lang);
+            $data['viewbar_title'] = $data['institution_name'] . ' - ' . $schoolOfStr . ' ' . $schoolRow->title;
+            $this->load->view('curriculum_inventory/curriculum_inventory_manager', $data);
+        } else {
+            $error = array();
+            $error['heading'] = 'Application error';
+            $error['message'] = 'Failed to load school data for this user session.';
+            $this->load->view('error/error_general', $error);
+        }
     }
 
     public function add ()
