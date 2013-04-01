bc3352ed780a1e7a4ebbd4a68504f59643338a66
Stefan Topfstedt <stefan.topfstedt@ucsf.edu>
2013-04-01 15:44:59 -0400

    created migration.

---
+++ web/application/migrations/002_setup_curriculum_inventory_tables.php
@@ -0,0 +1,23 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * Sets up the tables needed for mapping out the curriculum inventory according to the AAMC standard.
+ */
+class Migration_Setup_curriculum_inventory_tables extends CI_Migration
+{
+    /**
+     * @see CI_Migration::up()
+     */
+    public function up ()
+    {
+        // @todo implement
+    }
+
+    /**
+     * @see CI_Migration::down()
+     */
+    public function down ()
+    {
+        // @todo implement
+    }
+}
