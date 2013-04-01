0b6fbf68d715c9e9774946a62782b017f2ed6c96
Stefan Topfstedt <stefan.topfstedt@ucsf.edu>
2013-04-01 17:48:29 -0400

    implemented getBySchoolId() method.

--- web/application/models/curriculum_inventory_institution.php
+++ web/application/models/curriculum_inventory_institution.php
@@ -14,4 +14,20 @@ class Curriculum_Inventory_Institution extends Ilios_Base_Model
     {
         parent::__construct('curriculum_inventory_institution', array('curriculum_inventory_institution_id'));
     }
+
+    /**
+     * Retrieves institutional data for a given school.
+     * @param $schoolId the school id.
+     * @return array|boolean the institutional data as assoc. array or FALSE if none was found
+     */
+    public function getBySchoolId ($schoolId)
+    {
+        $rhett = false;
+        $query = $this->db->get_where($this->databaseTableName, array('school_id' => $schoolId), 1);
+        if (0 < $query->num_rows())
+        {
+            $rhett = $query->result_array();
+        }
+        return $rhett;
+    }
 }
