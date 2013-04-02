c6715d0f1028bef0c6e66f406c1c727162215d7b
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2013-04-02 17:20:33 -0400

    added method for retrieving curriculum inventory programs by a given ilios program id.

--- web/application/models/curriculum_inventory_program.php
+++ web/application/models/curriculum_inventory_program.php
@@ -18,12 +18,38 @@ class Curriculum_Inventory_Program extends Ilios_Base_Model
     /**
      * Creates a new curriculum inventory program record for a given program year id.
      * @param int $programYearId the ilios program year id.
-     * @return int the program year id
+     * @return boolean TRUE on success, FALSE on failure
      */
     public function create ($programYearId)
     {
         $data = array();
         $data['program_year_id'] = $programYearId;
         return $this->db->insert($this->databaseTableName, $data);
+    }
+
+    /**
+     * Lists all curriculum inventory programs associated with a given Ilios program.
+     * Note "deleted" programs and program-years are omitted.
+     * @param in $programId the program id
+     * @return array
+     */
+    public function listByProgram ($programId)
+    {
+        $rhett = array();
+        $this->db->select("{$this->databaseTableName}.*");
+        $this->db->from($this->databaseTableName);
+        $this->db->join("program_year", "program_year.program_year_id = {$this->databaseTableName}.program_year_id");
+        $this->db->join("program", "program.program_id = program_year.program_id");
+        $this->db->where("program.program_id", $programId);
+        $this->db->where("program.deleted = 0");
+        $this->db->where("program_year.deleted = 0");
+        $query = $this->db->get();
+        if (0 < $query->num_rows()) {
+            foreach ($query->result_array() as $row) {
+                $rhett[] = $row;
+            }
+        }
+        $query->free_result();
+        return $rhett;
     }
 }
