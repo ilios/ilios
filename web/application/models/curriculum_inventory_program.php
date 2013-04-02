1fc5db818ec4a69dc9ea4df75e97ef6a52f6b4b1
Stefan Topfstedt <stefan.topfstedt@ucsf.edu>
2013-04-01 20:34:49 -0400

    implemented create method. fixed pkey name in constructor.

--- web/application/models/curriculum_inventory_program.php
+++ web/application/models/curriculum_inventory_program.php
@@ -12,6 +12,18 @@ class Curriculum_Inventory_Program extends Ilios_Base_Model
      */
     public function __construct ()
     {
-        parent::__construct('curriculum_inventory_program', array('curriculum_inventory_program_id'));
+        parent::__construct('curriculum_inventory_program', array('program_year_id'));
+    }
+
+    /**
+     * Creates a new curriculum inventory program record for a given program year id.
+     * @param int $programYearId the ilios program year id.
+     * @return int the program year id
+     */
+    public function create ($programYearId)
+    {
+        $data = array();
+        $data['program_year_id'] = $programYearId;
+        return $this->db->insert($this->databaseTableName, $data);
     }
 }
