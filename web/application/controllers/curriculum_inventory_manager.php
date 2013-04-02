7fa21b4f5beda3ebcd844b89e16ae5d42bdd781b
Stefan Topfstedt <stefan.topfstedt@ucsf.edu>
2013-04-01 20:35:37 -0400

    implemented "add" action.

--- web/application/controllers/curriculum_inventory_manager.php
+++ web/application/controllers/curriculum_inventory_manager.php
@@ -14,6 +14,7 @@ class Curriculum_Inventory_Manager extends Ilios_Web_Controller
     public function __construct ()
     {
         parent::__construct();
+        $this->load->model('Curriculum_Inventory_Program', 'invProgram', true);
     }
 
     /**
@@ -54,9 +55,23 @@ class Curriculum_Inventory_Manager extends Ilios_Web_Controller
         $key = 'general.phrases.school_of';
         $schoolOfStr = $this->languagemap->getI18NString($key, $lang);
         $data['viewbar_title'] = $data['institution_name'] . ' - ' . $schoolOfStr . ' ' . $schoolRow->title;
+
+        //
+        // load curriculum inventory program for the given program year
+        //
+
+        // @todo
+
         $this->load->view('curriculum_inventory/curriculum_inventory_manager', $data);
     }
 
+    /**
+     * Creates a new curriculum inventory program for a given program year.
+     * Expects the following input in the request parameter string:
+     *    'py_id' ... the program year id.
+     * On successful creation, the user will be redirected to view the new inventory program.
+     * Any failure will cause an error page to be rendered.
+     */
     public function add ()
     {
         $lang = $this->getLangToUse();
@@ -72,6 +87,43 @@ class Curriculum_Inventory_Manager extends Ilios_Web_Controller
             $this->_viewAccessForbiddenPage($lang, $data);
             return;
         }
+
+        $schoolId = $this->session->userdata('school_id');
+        $schoolRow = $this->school->getRowForPrimaryKeyId($schoolId);
+
+        if (! isset($schoolRow)) {
+            show_error('Failed to load school data for this user session.');
+            return;
+        }
+
+        //
+        // create new inventory program for a given ilios program-year
+        //
+        $programYearId = (int) $this->input->get('py_id');
+
+        // check if ilios program year exists
+        $programYear = $this->programYear->getRowForPrimaryKeyId($programYearId, true);
+        if (! isset($programYear)) {
+            show_error('A program year with the given id does not exist.');
+            return;
+        }
+
+        // check if a curriculum inventory program already exists
+        $invProgram = $this->invProgram->getRowForPrimaryKeyId($programYearId);
+        if (isset($invProgram)) {
+            show_error('A curriculum inventory report already exists for the given program year.');
+            return;
+        }
+
+        // create the new inventory program record
+        $created = $this->invProgram->create($programYearId);
+        if (false === $created) {
+            show_error('Failed to create curriculum inventory report.');
+            return;
+        }
+
+        // success! redirect to view the new program
+        redirect('curriculum_inventory_manager/view?py_id=' . $programYearId);
     }
 
     public function preview ()
