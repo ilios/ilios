b66e6235af699b0bbfc53926bf69ad02e8ef941a
Stefan Topfstedt <stefan.topfstedt@ucsf.edu>
2013-04-01 20:11:14 -0400

    added curriculum_inventory_program creation/deletion

--- web/application/migrations/002_setup_curriculum_inventory_tables.php
+++ web/application/migrations/002_setup_curriculum_inventory_tables.php
@@ -13,14 +13,33 @@ class Migration_Setup_curriculum_inventory_tables extends CI_Migration
         $this->db->trans_start();
         $sql =<<<EOL
 CREATE TABLE `curriculum_inventory_institution` (
-	`school_id` INT(10) UNSIGNED NOT NULL,
-	`name` VARCHAR(100) NOT NULL,
-	`aamc_id` VARCHAR(10) NOT NULL,
-	`address_city` VARCHAR(100) NOT NULL,
-	`address_state_or_province` VARCHAR(50) NOT NULL,
-	`address_country_code` CHAR(2) NOT NULL,
-	PRIMARY KEY (`school_id`),
-	CONSTRAINT `fkey_curriculum_inventory_institution_school_id` FOREIGN KEY (`school_id`) REFERENCES `school` (`school_id`) ON UPDATE CASCADE ON DELETE CASCADE
+    `school_id` INT(10) UNSIGNED NOT NULL,
+    `name` VARCHAR(100) NOT NULL,
+    `aamc_id` VARCHAR(10) NOT NULL,
+    `address_city` VARCHAR(100) NOT NULL,
+    `address_state_or_province` VARCHAR(50) NOT NULL,
+    `address_country_code` CHAR(2) NOT NULL,
+    PRIMARY KEY (`school_id`),
+    CONSTRAINT `fkey_curriculum_inventory_institution_school_id`
+       FOREIGN KEY (`school_id`) REFERENCES `school` (`school_id`)
+       ON UPDATE CASCADE ON DELETE CASCADE
+)
+DEFAULT CHARSET='utf8'
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB
+EOL;
+        $this->db->query($sql);
+        $sql =<<<EOL
+CREATE TABLE `curriculum_inventory_program` (
+    `program_year_id` INT(10) UNSIGNED NOT NULL,
+    `aamc_id` VARCHAR(10) NULL,
+    `education_program_context_id` INT UNSIGNED NULL,
+    `profession_id` INT UNSIGNED NULL,
+    `specialty_id` INT UNSIGNED NULL,
+    PRIMARY KEY (`program_year_id`),
+    CONSTRAINT `fkey_curriculum_inventory_program_program_year_id`
+        FOREIGN KEY (`program_year_id`) REFERENCES `program_year` (`program_year_id`)
+        ON UPDATE CASCADE ON DELETE CASCADE
 )
 DEFAULT CHARSET='utf8'
 COLLATE='utf8_general_ci'
@@ -37,6 +56,7 @@ EOL;
     {
         $this->db->trans_start();
         $this->db->query("DROP TABLE `curriculum_inventory_institution`");
+        $this->db->query("DROP TABLE `curriculum_inventory_program`");
         $this->db->trans_complete();
     }
 }
