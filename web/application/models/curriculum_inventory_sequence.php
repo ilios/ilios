e8bfb6cabe8e2e05e814c0b4142c061afaab7489
Stefan Topfstedt <stefan.topfstedt@ucsf.edu>
2013-04-04 18:01:29 -0400

    implemented create and update methods.

--- web/application/models/curriculum_inventory_sequence.php
+++ web/application/models/curriculum_inventory_sequence.php
@@ -12,6 +12,34 @@ class Curriculum_Inventory_Sequence extends Ilios_Base_Model
      */
     public function __construct ()
     {
-        parent::__construct('curriculum_inventory_sequence', array('curriculum_inventory_sequence_id'));
+        parent::__construct('curriculum_inventory_sequence', array('program_year_id'));
+    }
+
+    /**
+     * Creates a new sequence.
+     * @param int $programYearId
+     * @param string $description
+     * @return boolean TRUE on success, FALSE otherwise
+     */
+    public function create ($programYearId, $description = '')
+    {
+        $data = array();
+        $data['program_year_id'] = $programYearId;
+        $data['description'] = $description;
+        return $this->db->insert($this->databaseTableName, $data);
+    }
+
+    /**
+     * Updates a sequence with the given data.
+     * @param int $programYearId the program year id
+     * @param string $description the sequence description
+     * @return boolean TRUE on success, FALSE otherwise
+     */
+    public function update ($programYearId, $description = '')
+    {
+        $data = array();
+        $data['description'] = $description;
+        $this->db->where('program_year_id', $programYearId);
+        return $this->db->update($this->databaseTableName, $data);
     }
 }
