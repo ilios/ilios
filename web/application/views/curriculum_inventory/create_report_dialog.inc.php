88398748a3cc4151cb84620750ba1ec82be86c24
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2013-05-01 16:23:34 -0400

    built out markup for report creation dialog. modded report search dialog markup slightly.

--- web/application/views/curriculum_inventory/create_report_dialog.inc.php
+++ web/application/views/curriculum_inventory/create_report_dialog.inc.php
@@ -6,18 +6,40 @@
  *
  * @see application/views/curriculum_inventory/index.php
  * @see application/views/curriculum_inventory/view.php
- * @see application/views/curriculum_inventory_curriculum_inventory_manager.js
+ * @see application/views/js/ilios.cim.js
  */
 ?>
 <div class="tabdialog" id="create_report_dialog">
     <div class="hd"><?php echo $this->languagemap->t('curriculum_inventory.create.title', $lang); ?></div>
     <div class="bd">
-        <span id="report_creation_status" class="search-status"></span>
         <div class="dialog_wrap">
+            <span id="report_creation_status" class="dialog-form-status"></span>
             <form method="POST" action="<?php echo $controllerURL; ?>/add">
-                <!-- todo implement -->
+                <div class="dialog-form-row" >
+                    <label for="new_report_name" class="entity_widget_title">
+                        <?php echo $this->languagemap->t('curriculum_inventory.create.report_name.label', $lang); ?>:
+                    </label><br />
+                    <input id="new_report_name" name="report_name" type="text"  value="" size="50"
+                           placeholder="<?php echo $this->languagemap->t('curriculum_inventory.create.report_name.hint', $lang, false); ?>" />
+                </div>
+                <div class="dialog-form-row">
+                    <label for="new_report_program" class="entity_widget_title">
+                        <?php echo$this->languagemap->t('curriculum_inventory.create.program_name.label', $lang); ?>:
+                    </label><br />
+                    <select name="program_id" id="new_report_program">
+                        <option value="1">Doctor of Medicine</option>
+                    </select>
+                </div>
+                <div class="dialog-form-row">
+                    <label for="new_report_year" class="entity_widget_title">
+                        <?php echo $this->languagemap->t('curriculum_inventory.create.report_year.label', $lang); ?>:
+                    </label><br />
+                    <input id="new_report_year" name="report_year" type="text" value=""
+                        maxlength="5" size="5" placeholder="yyyy">
+                </div>
+                <div class="clear"></div>
             </form>
         </div>
-    </div>
+     </div>
     <div class="ft"></div>
 </div>
--- web/application/views/curriculum_inventory/report_search_dialog.inc.php
+++ web/application/views/curriculum_inventory/report_search_dialog.inc.php
@@ -12,23 +12,25 @@
 <div class="tabdialog" id="report_search_picker">
     <div class="hd"><?php echo $this->languagemap->t('curriculum_inventory.search.title', $lang); ?></div>
     <div class="bd">
-        <span id="report_search_status" class="search-status"></span>
-        <form method="POST" action="<?php echo $controllerURL; ?>/searchReports">
-            <fieldset>
-                <legend><?php echo $this->languagemap->t('general.phrases.search_term', $lang); ?></legend>
-                <input type="text" id="report_search_term" name="report_search_term" size="50"
-                       placeholder="<?php echo $this->languagemap->t('general.phrases.search.hint', $lang, false); ?>">
-                <span class="search_icon_button" id="search_report_submit_btn"></span>
-            </fieldset>
-            <fieldset>
-                <legend><?php echo $this->languagemap->t('general.phrases.search_results', $lang); ?></legend>
-                <div class="scroll_list clearfix">
-                    <ul id="report_search_results_list" class="search-results">
-                        <li></li>
-                    </ul>
-                </div>
-            </fieldset>
-         </form>
+        <div class="dialog_wrap">
+            <span id="report_search_status" class="dialog-form-status"></span>
+            <form method="POST" action="<?php echo $controllerURL; ?>/searchReports">
+                <fieldset>
+                    <legend><?php echo $this->languagemap->t('general.phrases.search_term', $lang); ?></legend>
+                    <input type="text" id="report_search_term" name="report_search_term" size="50"
+                           placeholder="<?php echo $this->languagemap->t('general.phrases.search.hint', $lang, false); ?>">
+                    <span class="search_icon_button" id="search_report_submit_btn"></span>
+                </fieldset>
+                <fieldset>
+                    <legend><?php echo $this->languagemap->t('general.phrases.search_results', $lang); ?></legend>
+                    <div class="scroll_list clearfix">
+                        <ul id="report_search_results_list" class="search-results">
+                            <li></li>
+                        </ul>
+                    </div>
+                </fieldset>
+             </form>
+        </div>
     </div>
     <div class="ft"></div>
 </div>
