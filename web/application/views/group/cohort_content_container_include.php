dd815a06d90c8e8a6095f2671b6ba99e7a291558
Stefan Topfstedt <stefan.topfstedt@ucsf.edu>
2013-06-18 14:38:21 -0700

    cleanup: win-to-*nix linebreak conversion. tabs-to-whitespace conversion. ditched trailing PHP closing tag.

--- web/application/views/group/cohort_content_container_include.php
+++ web/application/views/group/cohort_content_container_include.php
@@ -2,72 +2,71 @@
 
 require_once(getServerFilePath('views') . 'common/content_container_generator.php');
 
-			$formPrefix = '<form id="cohort_form" method="POST" action="' . current_url()
-								. '/willNeverSubmit"
-						onsubmit="return false;">';
+            $formPrefix = '<form id="cohort_form" method="POST" action="' . current_url()
+                                . '/willNeverSubmit"
+                        onsubmit="return false;">';
 
 
-			$addNewEntityLink = '';
+            $addNewEntityLink = '';
 
-			$searchNewEntityLink = '<a id="find_cohort_and_program" href="" class="small radius button"
-							onClick="ilios.ui.onIliosEvent.fire({action: \'sac_dialog_open\',
-																	event: \'find_cohort_and_program\'});
-									 return false;">' . $select_program_link_string . '</a>';
+            $searchNewEntityLink = '<a id="find_cohort_and_program" href="" class="small radius button"
+                            onClick="ilios.ui.onIliosEvent.fire({action: \'sac_dialog_open\',
+                                                                    event: \'find_cohort_and_program\'});
+                                     return false;">' . $select_program_link_string . '</a>';
 
-			$entityContainerHeader = '
-            			<li class="title">
-							<span class="data-type">' . $program_title_full_string . '</span>
-							<span class="data" id="program_cohort_title"></span>
-						</li>
-						<li class="short-title">
-							<span class="data-type">' . $program_title_short_string . '</span>
-							<span class="data" id="program_title_short"></span>
-						</li>
-						<li class="enrollment">
-							<span class="data-type">' . $current_enrollment_string . '</span>
-							<span class="data" id="current_enrollment"></span>
-						</li>';
+            $entityContainerHeader = '
+                        <li class="title">
+                            <span class="data-type">' . $program_title_full_string . '</span>
+                            <span class="data" id="program_cohort_title"></span>
+                        </li>
+                        <li class="short-title">
+                            <span class="data-type">' . $program_title_short_string . '</span>
+                            <span class="data" id="program_title_short"></span>
+                        </li>
+                        <li class="enrollment">
+                            <span class="data-type">' . $current_enrollment_string . '</span>
+                            <span class="data" id="current_enrollment"></span>
+                        </li>';
 
-			$entityContainerContent = '';
+            $entityContainerContent = '';
 
-			$addNewSomethingId = 'all_edit_member_link';
-			$addNewSomethingAction = 'ilios.ui.onIliosEvent.fire({action: \'em_dialog_open\', event: \'add_new_members_picker_show_dialog\', container_number: \'-1\'});';
-			$addNewSomethingDisplayText = $add_to_all_string ;
-
-
-			$suffixingContent = '
-					<div class="collapse_children_toggle_link">
-						<button class="small secondary radius button"
-							onclick="ilios.gm.subgroup.changeBreadcrumbViewLevelOrSidestep(\'-1\'); return false;"
-								id="open_cohort" style="display: none;">' . $open_cohort_string . '</button>
-						<button class="small secondary radius button" onclick="ilios.gm.collapseOrExpandGroups(false, false); return false;"
-								class="groups_collapsed" id="expand_groups_link"
-								style="display: none;">' . $expand_groups_string . '</button>
-					</div>
-
-					<div style="clear: both;"></div>
-
-					<div id="breadcrumb_group_trail"></div>
-				    <div id="group_container"></div>
-				    <div id="breadcrumbed_suffixed_group_trail"></div>
+            $addNewSomethingId = 'all_edit_member_link';
+            $addNewSomethingAction = 'ilios.ui.onIliosEvent.fire({action: \'em_dialog_open\', event: \'add_new_members_picker_show_dialog\', container_number: \'-1\'});';
+            $addNewSomethingDisplayText = $add_to_all_string ;
 
 
-				    <div class="add_primary_child_link">
-						<button class="small secondary radius button" onclick="ilios.gm.transaction.handleManualGroupAdd();"
-								id="general_new_add_group_link" disabled="disabled">' . $add_group_string . '</button>
-					</div>';
+            $suffixingContent = '
+                    <div class="collapse_children_toggle_link">
+                        <button class="small secondary radius button"
+                            onclick="ilios.gm.subgroup.changeBreadcrumbViewLevelOrSidestep(\'-1\'); return false;"
+                                id="open_cohort" style="display: none;">' . $open_cohort_string . '</button>
+                        <button class="small secondary radius button" onclick="ilios.gm.collapseOrExpandGroups(false, false); return false;"
+                                class="groups_collapsed" id="expand_groups_link"
+                                style="display: none;">' . $expand_groups_string . '</button>
+                    </div>
+
+                    <div style="clear: both;"></div>
+
+                    <div id="breadcrumb_group_trail"></div>
+                    <div id="group_container"></div>
+                    <div id="breadcrumbed_suffixed_group_trail"></div>
 
 
-			$saveDraftAction = '';
-			$publishAction = '';
-			$revertAction = '';
+                    <div class="add_primary_child_link">
+                        <button class="small secondary radius button" onclick="ilios.gm.transaction.handleManualGroupAdd();"
+                                id="general_new_add_group_link" disabled="disabled">' . $add_group_string . '</button>
+                    </div>';
 
-			createContentContainerMarkup('&nbsp;', $formPrefix, $addNewEntityLink, $searchNewEntityLink,
-										 $entityContainerHeader, $entityContainerContent, $addNewSomethingId,
-										 $addNewSomethingAction, $addNewSomethingDisplayText,
-										 $suffixingContent, $saveDraftAction, $publishAction,
-										 $revertAction, false, false, false, true, $save_all_string, $save_all_draft_string,
-										 $save_draft_string, $publish_all_string,
-										 $publish_now_string, $reset_form_string);
 
-?>
+            $saveDraftAction = '';
+            $publishAction = '';
+            $revertAction = '';
+
+            createContentContainerMarkup('&nbsp;', $formPrefix, $addNewEntityLink, $searchNewEntityLink,
+                                         $entityContainerHeader, $entityContainerContent, $addNewSomethingId,
+                                         $addNewSomethingAction, $addNewSomethingDisplayText,
+                                         $suffixingContent, $saveDraftAction, $publishAction,
+                                         $revertAction, false, false, false, true, $save_all_string, $save_all_draft_string,
+                                         $save_draft_string, $publish_all_string,
+                                         $publish_now_string, $reset_form_string);
+
