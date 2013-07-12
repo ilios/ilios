46fb34773c21149bb37495fc62e6077598a2149b
Stefan Topfstedt <stefan.topfstedt@ucsf.edu>
2013-07-12 10:51:57 -0700

    tweaked code comments.

--- web/application/views/home/report_dialogs_include.js
+++ web/application/views/home/report_dialogs_include.js
@@ -1,6 +1,6 @@
 /**
- * This script entails the event handling and init routines
- * for the various report dialogs on the instructor dashboard page.
+ * This script entails the event handling and init routines for the various report dialogs
+ * on the educator dashboard page.
  *
  * DEPENDENCY: YUI lib
  * DEPENDENCY: scripts/ilios_ui.js
