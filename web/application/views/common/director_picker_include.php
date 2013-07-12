9e30a1b590257d43e44891c91d0619c9af8c991d
Stefan Topfstedt <stefan.topfstedt@ucsf.edu>
2013-07-12 14:48:31 -0700

    cleanup: line ending conversion to unix. tabs-to-blankspace conversion. removed closing PHP tags where applicable.

--- web/application/views/common/director_picker_include.php
+++ web/application/views/common/director_picker_include.php
@@ -5,133 +5,133 @@ require_once(getServerFilePath('views') . 'common/picker_dialog_generator.php');
 /**
  * The code relies upon the following notions:
  *  . the Javascript model which is produced by the PHP that includes this file must feature the
- *  		implementation of a method:
- *  					ilios.common.picker.director.getOwningModelForDirectorPicker(dialog)
- *  		which returns the underlying model (ProgramYearModel, for example) associated to a
- *  		given dialog
- * 	. the underlying model (ProgramYearModel, for example) implements the following methods:
- * 				addDirector(UserModel)
- * 				removeDirector(UserModel)
- * 				getDirectors()
- * 				replaceContentWithModel(<underlyingModelType>, bool:forceReplace)
- * 				compareTo(<underlyingModelType>)
- * 				clone()
+ *          implementation of a method:
+ *                      ilios.common.picker.director.getOwningModelForDirectorPicker(dialog)
+ *          which returns the underlying model (ProgramYearModel, for example) associated to a
+ *          given dialog
+ *     . the underlying model (ProgramYearModel, for example) implements the following methods:
+ *                 addDirector(UserModel)
+ *                 removeDirector(UserModel)
+ *                 getDirectors()
+ *                 replaceContentWithModel(<underlyingModelType>, bool:forceReplace)
+ *                 compareTo(<underlyingModelType>)
+ *                 clone()
  *
  * See program/director_include.php for a usage example
  *
  * TODO there is a lot of commonality between this an IGM's faculty picker
  */
 
-			$dialogDivId = 'director_picker_dialog';
+            $dialogDivId = 'director_picker_dialog';
 
 
-			$modelName = '';
-			$modelSpecification = '';
+            $modelName = '';
+            $modelSpecification = '';
 
 
-			$remoteDataSourceMethod = 'getDirectorList';
+            $remoteDataSourceMethod = 'getDirectorList';
 
-			$autoCompleteFilterer = 'ilios.common.picker.director. directorAutoCompleteFilterer';
+            $autoCompleteFilterer = 'ilios.common.picker.director. directorAutoCompleteFilterer';
 
-			$autoCompleteFormatter = 'ilios.common.picker.director.directorAutoCompleteFormatter';
+            $autoCompleteFormatter = 'ilios.common.picker.director.directorAutoCompleteFormatter';
 
-			$responseSchema = '{ resultNode: "Result", fields: ["last_name", "user_id", "first_name", '
-									. '"middle_name", "phone", "email", '
-									. '"uc_uid", "other_id"] }';
+            $responseSchema = '{ resultNode: "Result", fields: ["last_name", "user_id", "first_name", '
+                                    . '"middle_name", "phone", "email", '
+                                    . '"uc_uid", "other_id"] }';
 
-			$hiddenFormElementId = 'director_hidden_input';
-			$listingTextFieldId = 'director_picker_selected_text_list';
-			$selectedItemContainerId = 'director_picked';
-
-
-			$parentModelGetterName = '';
-			$parentModelGetter = '';
-			$localModelGetterInvocation = '';
-			$localModelSetterName = '';
-			$modelSettingCode = '';
+            $hiddenFormElementId = 'director_hidden_input';
+            $listingTextFieldId = 'director_picker_selected_text_list';
+            $selectedItemContainerId = 'director_picked';
 
 
-			$autolistContainerId = 'director_autolist';
-			$autoCompleteTextFieldId = 'director_name_input';
-			$i18nKeyForSelectedLabel = 'general.terms.directors';
-			$i18nKeyForInstructions = 'general.text.director_search_instructions';
-			$autoCompleteTabId = 'director_autocomplete_tab';
+            $parentModelGetterName = '';
+            $parentModelGetter = '';
+            $localModelGetterInvocation = '';
+            $localModelSetterName = '';
+            $modelSettingCode = '';
 
-			$dialogDisplayingEventTriggerName = 'director_picker_show_dialog';
 
-			$displayHandlerCode = '
-				var owningModel = ilios.common.picker.director.getOwningModelForDirectorPicker(dialog);
-				var users = owningModel.getDirectors();
-				var userModel = null;
+            $autolistContainerId = 'director_autolist';
+            $autoCompleteTextFieldId = 'director_name_input';
+            $i18nKeyForSelectedLabel = 'general.terms.directors';
+            $i18nKeyForInstructions = 'general.text.director_search_instructions';
+            $autoCompleteTabId = 'director_autocomplete_tab';
 
-				ilios.common.picker.director.idUserMap = new Array();
-				ilios.common.picker.director.directorPickerModel = owningModel.clone();
-				ilios.common.picker.director.directorPickerModel.setDBId(owningModel.getDBId());
+            $dialogDisplayingEventTriggerName = 'director_picker_show_dialog';
 
-				if (ilios.common.picker.director.directorSelectListElement == null) {
-					ilios.common.picker.director.directorSelectListElement
-													= document.getElementById(dialog.selectListId);
-				}
-				ilios.common.picker.director.directorSelectListElement.iliosModel = owningModel;
+            $displayHandlerCode = '
+                var owningModel = ilios.common.picker.director.getOwningModelForDirectorPicker(dialog);
+                var users = owningModel.getDirectors();
+                var userModel = null;
 
-				ilios.utilities.removeAllChildren(ilios.common.picker.director.directorSelectListElement);
+                ilios.common.picker.director.idUserMap = new Array();
+                ilios.common.picker.director.directorPickerModel = owningModel.clone();
+                ilios.common.picker.director.directorPickerModel.setDBId(owningModel.getDBId());
 
-				for (var key in users) {
-					userModel = users[key];
+                if (ilios.common.picker.director.directorSelectListElement == null) {
+                    ilios.common.picker.director.directorSelectListElement
+                                                    = document.getElementById(dialog.selectListId);
+                }
+                ilios.common.picker.director.directorSelectListElement.iliosModel = owningModel;
 
-					ilios.common.picker.director.directorSelectListElement.appendChild(
-													 ilios.common.picker.director
-													  	.createSelectedElementForUserModel(userModel));
-				}
+                ilios.utilities.removeAllChildren(ilios.common.picker.director.directorSelectListElement);
+
+                for (var key in users) {
+                    userModel = users[key];
+
+                    ilios.common.picker.director.directorSelectListElement.appendChild(
+                                                     ilios.common.picker.director
+                                                          .createSelectedElementForUserModel(userModel));
+                }
 
                 return true;
-			';
+            ';
 
-			$selectHandlerCode = '
-				var userModel = ilios.common.picker.director.idUserMap[rowSelection.user_id];
+            $selectHandlerCode = '
+                var userModel = ilios.common.picker.director.idUserMap[rowSelection.user_id];
 
-				ilios.common.picker.director.directorSelectListElement.appendChild(
-													  ilios.common.picker.director
-													  	.createSelectedElementForUserModel(userModel));
-				ilios.common.picker.director.directorPickerModel.addDirector(userModel);
-			';
+                ilios.common.picker.director.directorSelectListElement.appendChild(
+                                                      ilios.common.picker.director
+                                                          .createSelectedElementForUserModel(userModel));
+                ilios.common.picker.director.directorPickerModel.addDirector(userModel);
+            ';
 
-			$deselectHandlerCode = '
-				var target = ilios.utilities.getEventTarget(event);
+            $deselectHandlerCode = '
+                var target = ilios.utilities.getEventTarget(event);
 
-				if (target.tagName.toLowerCase() === "li") {
-					ilios.common.picker.director.directorPickerModel.removeDirector(target.iliosModel);
-				}
-			';
+                if (target.tagName.toLowerCase() === "li") {
+                    ilios.common.picker.director.directorPickerModel.removeDirector(target.iliosModel);
+                }
+            ';
 
-			$submitHandlerCode = '
-				var owningModel = ilios.common.picker.director.directorSelectListElement.iliosModel;
+            $submitHandlerCode = '
+                var owningModel = ilios.common.picker.director.directorSelectListElement.iliosModel;
 
-				if (owningModel.compareTo(ilios.common.picker.director.directorPickerModel) != 0) {
-					owningModel.replaceContentWithModel(ilios.common.picker.director.directorPickerModel,
-														true);
-				}
+                if (owningModel.compareTo(ilios.common.picker.director.directorPickerModel) != 0) {
+                    owningModel.replaceContentWithModel(ilios.common.picker.director.directorPickerModel,
+                                                        true);
+                }
 
-				ilios.common.picker.director.idUserMap = null;
-				ilios.common.picker.director.directorPickerModel = null;
-			';
+                ilios.common.picker.director.idUserMap = null;
+                ilios.common.picker.director.directorPickerModel = null;
+            ';
 
 
-			generatePickerMarkupAndScript("dire_", $dialogDivId, $modelName, $modelSpecification,
-										  $remoteDataSourceMethod, $responseSchema,
-										  $hiddenFormElementId, $listingTextFieldId,
-										  $selectedItemContainerId, $parentModelGetterName,
-										  $parentModelGetter, $localModelGetterInvocation,
-										  $localModelSetterName, $modelSettingCode,
-										  $autolistContainerId, $autoCompleteTextFieldId,
-										  $i18nKeyForSelectedLabel,
-										  $i18nKeyForInstructions,
-										  $autoCompleteTabId,
-										  $dialogDisplayingEventTriggerName,
-										  'YAHOO.util.XHRDataSource.TYPE_XML', null,
-										  $autoCompleteFilterer, $autoCompleteFormatter, 500,
-										  $displayHandlerCode, $selectHandlerCode,
-										  $deselectHandlerCode, $submitHandlerCode);
+            generatePickerMarkupAndScript("dire_", $dialogDivId, $modelName, $modelSpecification,
+                                          $remoteDataSourceMethod, $responseSchema,
+                                          $hiddenFormElementId, $listingTextFieldId,
+                                          $selectedItemContainerId, $parentModelGetterName,
+                                          $parentModelGetter, $localModelGetterInvocation,
+                                          $localModelSetterName, $modelSettingCode,
+                                          $autolistContainerId, $autoCompleteTextFieldId,
+                                          $i18nKeyForSelectedLabel,
+                                          $i18nKeyForInstructions,
+                                          $autoCompleteTabId,
+                                          $dialogDisplayingEventTriggerName,
+                                          'YAHOO.util.XHRDataSource.TYPE_XML', null,
+                                          $autoCompleteFilterer, $autoCompleteFormatter, 500,
+                                          $displayHandlerCode, $selectHandlerCode,
+                                          $deselectHandlerCode, $submitHandlerCode);
 
 ?>
 
@@ -139,69 +139,69 @@ require_once(getServerFilePath('views') . 'common/picker_dialog_generator.php');
 
 
 
-	ilios.namespace('common.picker.director');
+    ilios.namespace('common.picker.director');
 
 
-	ilios.common.picker.director.idUserMap = null;
-	ilios.common.picker.director.directorPickerModel = null;
-	ilios.common.picker.director.directorSelectListElement = null;
+    ilios.common.picker.director.idUserMap = null;
+    ilios.common.picker.director.directorPickerModel = null;
+    ilios.common.picker.director.directorSelectListElement = null;
 
-	ilios.common.picker.director.createSelectedElementForUserModel = function (userModel) {
-		var element = document.createElement('li');
-		var textNode = document.createTextNode(userModel.getFormattedName(ilios.utilities.USER_NAME_FORMAT_LAST_FIRST));
+    ilios.common.picker.director.createSelectedElementForUserModel = function (userModel) {
+        var element = document.createElement('li');
+        var textNode = document.createTextNode(userModel.getFormattedName(ilios.utilities.USER_NAME_FORMAT_LAST_FIRST));
 
-		element.iliosModel = userModel;
-		element.appendChild(textNode);
+        element.iliosModel = userModel;
+        element.appendChild(textNode);
 
-		ilios.utilities.setToolTipForElement(element, userModel.getEmailAddress());
+        ilios.utilities.setToolTipForElement(element, userModel.getEmailAddress());
 
-		return element;
-	};
+        return element;
+    };
 
-	ilios.common.picker.director.directorAutoCompleteFilterer = function (queryString, fullResponse,
-																		  parsedResponse, callback,
-																		  autoCompleter) {
-		var len = parsedResponse.results.length;
-		var selectedList = document.getElementById(autoCompleter.target);
-		var filteredResults = new Array();
-		var i = 0;
-		var userModel = null;
-		var populateUserMap = (ilios.common.picker.director.idUserMap.length == 0);
+    ilios.common.picker.director.directorAutoCompleteFilterer = function (queryString, fullResponse,
+                                                                          parsedResponse, callback,
+                                                                          autoCompleter) {
+        var len = parsedResponse.results.length;
+        var selectedList = document.getElementById(autoCompleter.target);
+        var filteredResults = new Array();
+        var i = 0;
+        var userModel = null;
+        var populateUserMap = (ilios.common.picker.director.idUserMap.length == 0);
 
-		for (; i < len; i++) {
-			userModel = new UserModel(parsedResponse.results[i]);
+        for (; i < len; i++) {
+            userModel = new UserModel(parsedResponse.results[i]);
 
-			 userModel.setLastName(parsedResponse.results[i].last_name);
-			 userModel.clearDirtyState();
+             userModel.setLastName(parsedResponse.results[i].last_name);
+             userModel.clearDirtyState();
 
-			if (populateUserMap) {
-				ilios.common.picker.director.idUserMap[userModel.getDBId()] = userModel;
-			}
+            if (populateUserMap) {
+                ilios.common.picker.director.idUserMap[userModel.getDBId()] = userModel;
+            }
 
-			if (! ilios.utilities.searchListElementForModel(selectedList, userModel)) {
-				filteredResults.push(parsedResponse.results[i]);
-			}
-		}
+            if (! ilios.utilities.searchListElementForModel(selectedList, userModel)) {
+                filteredResults.push(parsedResponse.results[i]);
+            }
+        }
 
-		parsedResponse.results = filteredResults;
+        parsedResponse.results = filteredResults;
 
-		return parsedResponse;
-	};
+        return parsedResponse;
+    };
 
-	ilios.common.picker.director.directorAutoCompleteFormatter = function (resultDataObject,
-																		   queryString, resultMatch,
-												  						   autoCompleter) {
-		var rhett = '<span uid="' + resultDataObject.user_id + '" title="'
-						+ resultDataObject.email + '">';
+    ilios.common.picker.director.directorAutoCompleteFormatter = function (resultDataObject,
+                                                                           queryString, resultMatch,
+                                                                             autoCompleter) {
+        var rhett = '<span uid="' + resultDataObject.user_id + '" title="'
+                        + resultDataObject.email + '">';
 
-		rhett += ilios.utilities.createFormattedUserName(resultDataObject.first_name,
-				 										 resultDataObject.middle_name,
-				 										 resultDataObject.last_name, 0);
+        rhett += ilios.utilities.createFormattedUserName(resultDataObject.first_name,
+                                                          resultDataObject.middle_name,
+                                                          resultDataObject.last_name, 0);
 
-		rhett += '</span>';
+        rhett += '</span>';
 
-		return rhett;
-	};
+        return rhett;
+    };
 
 
 </script>
