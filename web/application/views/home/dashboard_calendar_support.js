7996e9daa63fc64b7d5efbd2a12494bc98aaac1e
elliotkendallUCSF <elliot.kendall@ucsf.edu>
2013-09-16 11:38:57 -0700

    Correct API feed URL format

--- web/application/views/home/dashboard_calendar_support.js
+++ web/application/views/home/dashboard_calendar_support.js
@@ -410,7 +410,7 @@ ilios.home.calendar.selectAllCheckboxes = function (elContainer, checked) {
 ilios.home.calendar.getFeedURL = function (f, field) {
     var method = "GET";
     var ajaxURL = baseURL + "api_controller/" + f;
-    var feedURL = baseURL + "calendar_exporter/";
+    var feedURL = baseURL + "calendar_exporter/api/";
     var paramString = '';
     var message = 'Error fetching feed URL';
 
