8e403099b4a721a1c91e72df600c8d83ec5075d5
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2013-11-24 12:21:39 +0100

    moved generateProgressDivMarkup() function into helper script.

--- web/application/helpers/template_helper.php
+++ web/application/helpers/template_helper.php
@@ -37,3 +37,24 @@ if ( ! function_exists('ilios_print_daytime_options')) {
     }
 }
 
+if (! function_exists('generateProgressDivMarkup')) {
+    /**
+     * Generates markup for displaying status progress messages.
+     * @param string|null $divStyleDefinition Inline CSS styles.
+     * @param string $message A status message.
+     * @return string The generated markup.
+     */
+    function generateProgressDivMarkup ($divStyleDefinition = null, $message = 'XX') {
+        if (is_null($divStyleDefinition)) {
+            $divStyleDefinition= "float: right; margin-right: 36px; display: none;";
+        }
+
+        $rhett = '<div style="' . $divStyleDefinition . '" id="save_in_progress_div" class="indeterminate_progress_text">
+                <div class="indeterminate_progress" style="display: inline-block;"></div>
+                <span id="save_in_progress_text">' . htmlentities($message, ENT_COMPAT, 'UTF-8') . '</span>&hellip;
+            </div>';
+
+        return $rhett;
+    }
+}
+
