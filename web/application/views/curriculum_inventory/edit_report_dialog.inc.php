f07e5c3b4529e83a2bf02c6ff0a5af518e0027c4
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2013-11-27 11:15:14 +0100

    using translation helper function

--- web/application/views/curriculum_inventory/edit_report_dialog.inc.php
+++ web/application/views/curriculum_inventory/edit_report_dialog.inc.php
@@ -14,7 +14,7 @@
  */
 ?>
 <div class="tabdialog hidden" id="edit_report_dialog">
-    <div class="hd"><?php echo $this->languagemap->t('curriculum_inventory.edit.title'); ?></div>
+    <div class="hd"><?php echo t('curriculum_inventory.edit.title'); ?></div>
     <div class="bd">
         <div class="dialog_wrap">
             <span id="report_update_status" class="dialog-form-status"></span>
@@ -22,21 +22,21 @@
                 <input type="hidden" value="" id="edit_report_id" name="report_id" />
                 <div class="dialog-form-row" >
                     <label for="edit_report_name" class="entity_widget_title">
-                        <?php echo $this->languagemap->t('general.terms.name'); ?>:
+                        <?php echo t('general.terms.name'); ?>:
                     </label><br />
                     <input id="edit_report_name" name="report_name" type="text"  value="" size="50"
-                           placeholder="<?php echo $this->languagemap->t('curriculum_inventory.create.report_name.hint', false); ?>" />
+                           placeholder="<?php echo t('curriculum_inventory.create.report_name.hint', false); ?>" />
                 </div>
                 <div class="dialog-form-row" >
                     <label for="edit_report_description" class="entity_widget_title">
-                        <?php echo $this->languagemap->t('general.terms.description'); ?>:
+                        <?php echo t('general.terms.description'); ?>:
                     </label><br />
                     <textarea id="edit_report_description" name="report_description" type="text" cols="80" rows="10"
-                              placeholder="<?php echo $this->languagemap->t('curriculum_inventory.create.report_description.hint', false); ?>"></textarea>
+                              placeholder="<?php echo t('curriculum_inventory.create.report_description.hint', false); ?>"></textarea>
                 </div>
                 <div class="dialog-form-row" >
                     <label for="edit_report_description" class="entity_widget_title">
-                        <?php echo $this->languagemap->t('general.phrases.start_date'); ?>:
+                        <?php echo t('general.phrases.start_date'); ?>:
                     </label><br />
                     <input id="edit_report_start_date" name="start_date" type="text" size="10"
                            placeholder="YYYY-MM-DD" readonly="readonly"/>
@@ -46,7 +46,7 @@
                 <div class="clear"></div>
                 <div class="dialog-form-row">
                     <label for="edit_report_description" class="entity_widget_title">
-                        <?php echo $this->languagemap->t('general.phrases.end_date'); ?>:
+                        <?php echo t('general.phrases.end_date'); ?>:
                     </label><br />
                     <input id="edit_report_end_date" name="end_date" type="text" size="10"
                            placeholder="YYYY-MM-DD" readonly="readonly"/>
