b4dad9a7ddef16fd030dca66b36aa036589287d1
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2013-12-06 14:36:13 -0800

    fixed typo in code comment. minor indentation/whitespace corrections.

--- web/application/migrations/020_associate_learning_groups_with_owning_cohort.php
+++ web/application/migrations/020_associate_learning_groups_with_owning_cohort.php
@@ -1,8 +1,7 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 /**
- * Breaks up the `cohort_master_group` JOIN table and moves the `cohort_id` directly into the `group`
- * table
+ * Breaks up the `cohort_master_group` JOIN table and moves the `cohort_id` directly into the `group` table.
  */
 class Migration_Associate_learning_groups_with_owning_cohort extends CI_Migration
 {
@@ -12,35 +11,35 @@ class Migration_Associate_learning_groups_with_owning_cohort extends CI_Migratio
     public function up ()
     {
         $this->db->trans_start();
-        //add the `cohort_id` column to the `group` table, we'll add the foreign key constratin after the next step or
-        //it will error
+        // add the `cohort_id` column to the `group` table, we'll add the foreign key constraint after the next step or
+        // it will error
         $sql = <<<EOL
 ALTER TABLE `group`
- ADD COLUMN `cohort_id` INT(14) UNSIGNED NULL AFTER `parent_group_id`
+    ADD COLUMN `cohort_id` INT(14) UNSIGNED NULL AFTER `parent_group_id`
 EOL;
         $this->db->query($sql);
 
-        //then populate the `cohort_id` column with the correct cohort_id from the `cohort_master_group` table
+        // then populate the `cohort_id` column with the correct cohort_id from the `cohort_master_group` table
         $sql = <<<EOL
 UPDATE `group` `g` SET `g`.`cohort_id` = (
- SELECT `cmg`.`cohort_id` FROM `cohort_master_group` `cmg` WHERE `cmg`.`group_id` = root_group_of_group(`g`.`group_id`)
+    SELECT `cmg`.`cohort_id` FROM `cohort_master_group` `cmg` WHERE `cmg`.`group_id` = root_group_of_group(`g`.`group_id`)
 )
 EOL;
         $this->db->query($sql);
 
-        //get rid of the group_x_user entries where the cohort_id is null or non-existent
+        // get rid of the group_x_user entries where the cohort_id is null or non-existent
         $sql = <<<EOL
 DELETE FROM group_x_user WHERE group_id IN (
- SELECT group_id FROM `group` WHERE cohort_id IS NULL OR cohort_id NOT IN (SELECT `cohort_id` FROM `cohort`)
+    SELECT group_id FROM `group` WHERE cohort_id IS NULL OR cohort_id NOT IN (SELECT `cohort_id` FROM `cohort`)
 )
 EOL;
         $this->db->query($sql);
 
-        //clean up the group table where the cohort_id is null or non-existent
+        // clean up the group table where the cohort_id is null or non-existent
         $sql = "DELETE FROM `group` WHERE `cohort_id` IS NULL OR `cohort_id` NOT IN (SELECT `cohort_id` FROM `cohort`)";
         $this->db->query($sql);
 
-        //alter the `group` table again to add the foreign key constraint and make the `cohort_id` column not NULL
+        // alter the `group` table again to add the foreign key constraint and make the `cohort_id` column not NULL
         $sql = <<<EOL
 ALTER TABLE `group`
  MODIFY COLUMN `cohort_id` INT(14) UNSIGNED NOT NULL,
@@ -65,33 +64,33 @@ EOL;
     {
         $this->db->trans_start();
 
-        //create the `cohort_master_group` table
+        // create the `cohort_master_group` table
         $sql = <<<EOL
 CREATE TABLE `cohort_master_group` (
-	  `cohort_id` INT(14) UNSIGNED NOT NULL,
-	  `group_id` INT(14) UNSIGNED NOT NULL
-	) ENGINE=InnoDB DEFAULT CHARSET=utf8
+    `cohort_id` INT(14) UNSIGNED NOT NULL,
+    `group_id` INT(14) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
 EOL;
         $this->db->query($sql);
 
-        //then insert the rows from the group table
+        // then insert the rows from the group table
         $sql = <<<EOL
 INSERT INTO `cohort_master_group` (`cohort_id`,`group_id`)
- SELECT `cohort_id`, `group_id` FROM `group` WHERE `cohort_id` IS NOT NULL
- AND `parent_group_id` IS NULL
- ORDER BY `group_id`
+    SELECT `cohort_id`, `group_id` FROM `group` WHERE `cohort_id` IS NOT NULL
+    AND `parent_group_id` IS NULL
+    ORDER BY `group_id`
 EOL;
         $this->db->query($sql);
 
-        //then drop the foreign key on `cohort_id` and the column itself from group table
+        // then drop the foreign key on `cohort_id` and the column itself from group table
         $sql = <<<EOL
 ALTER TABLE `group`
- DROP FOREIGN KEY `fkey_group_cohort_id`,
- DROP COLUMN `cohort_id`
+    DROP FOREIGN KEY `fkey_group_cohort_id`,
+    DROP COLUMN `cohort_id`
 EOL;
         $this->db->query($sql);
 
-        //then create the root_group_of_group function
+        // then create the root_group_of_group function
         $sql =  <<<EOL
 CREATE FUNCTION root_group_of_group (in_gid INT)
     RETURNS INT
