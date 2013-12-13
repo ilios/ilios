63aa754fe5f27f85c247385217a2bc4b747da58c
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2013-12-12 17:51:52 -0800

    moved api key actions into web controller.

--- web/application/controllers/ilios_web_controller.php
+++ web/application/controllers/ilios_web_controller.php
@@ -405,4 +405,43 @@ abstract class Ilios_Web_Controller extends Ilios_Base_Controller
         header("Content-Type: text/plain");
         echo json_encode($results);
     }
+
+    /**
+     * Fetch the user's API key. If none exists, create one
+     */
+    public function get_api_key ()
+    {
+        $key = @$this->authentication->getByUserId($this->session->userdata('uid'))->api_key;
+        if ($key) {
+            header('Content-type: text/plain');
+            print json_encode(array('key' => $key));
+        } else {
+            $this->new_api_key();
+        }
+    }
+
+    /**
+     * Create and store a new API key for the user
+     */
+    public function new_api_key ()
+    {
+        if ($this->session->userdata('uid')) {
+            if (function_exists('openssl_random_pseudo_bytes')) {
+                $key = bin2hex(openssl_random_pseudo_bytes(32));
+            } else {
+                $key = '';
+                for ($i=0;$i<32;$i++) {
+                    $key = $key . str_pad(dechex(mt_rand(0, 255)), 2, '0', STR_PAD_LEFT);
+                }
+            }
+            header('Content-type: text/plain');
+            if ($this->authentication->changeAPIKey($this->session->userdata('uid'), $key)) {
+                print json_encode(array('key' => $key));
+            } else {
+                print json_encode(array('error' => 'Error'));
+            }
+        } else {
+            header('HTTP/1.1 403 Forbidden');
+        }
+    }
 }
