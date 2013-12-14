74b7827662c7af846b80a2389f1d781108696851
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2013-12-13 16:43:58 -0800

    gutted calendar export controller and moved all functionality into the calendar controller.

--- web/application/controllers/calendar_controller.php
+++ web/application/controllers/calendar_controller.php
@@ -22,6 +22,9 @@ class Calendar_Controller extends Ilios_Web_Controller
         $this->load->model('School', 'school', TRUE);
         $this->load->model('User', 'user', TRUE);
         $this->load->model('User_Made_Reminder', 'reminder', TRUE);
+
+        $this->load->library("ICalExporter");
+        $this->load->library('CalendarFeedDataProvider');
     }
 
     /**
@@ -613,6 +616,50 @@ class Calendar_Controller extends Ilios_Web_Controller
         } else {
             print json_encode(array('error' => 'Error'));
         }
+    }
+
+    /**
+     * @todo add code docs
+     * @param string $role
+     */
+    public function exportICalendar ($role='all')
+    {
+        $userRoles = array();
+
+        // authorization check and capture of user requested/available user roles
+        if ($this->session->userdata('is_learner') && ($role == 'all' || $role == 'student')) {
+            $userRoles[] = User_Role::STUDENT_ROLE_ID;
+        }
+        if ($this->session->userdata('has_instructor_access') && ($role == 'all' || $role == 'instructor')) {
+            $userRoles[] = User_Role::FACULTY_ROLE_ID;
+            $userRoles[] = User_Role::COURSE_DIRECTOR_ROLE_ID;
+        }
+        if (! count($userRoles)) {
+            header('HTTP/1.1 403 Forbidden'); // VERBOTEN!
+            return;
+        }
+
+        $userId = $this->session->userdata('uid');
+
+        $schoolId = $this->session->userdata('school_id');
+
+        $events = $this->calendarfeeddataprovider->getData($userId, $schoolId, $userRoles);
+
+        $calendar_title = 'Ilios Calendar';
+        $this->icalexporter->setTitle($calendar_title);
+        $ical = $this->icalexporter->toICal($events);
+
+        $filename="ilios_calendar.ics";
+        header('Last-Modified: '. gmdate('D, d M Y H:i:s', time()) .' GMT');
+        header('Cache-Control: private, must-revalidate, pre-check=0, post-check=0, max-age=0');
+        header('Expires: '. gmdate('D, d M Y H:i:s', 0) .'GMT');
+        header('Pragma: no-cache');
+        header('Accept-Ranges: none');
+        header('Content-disposition: attachment; filename='.$filename);
+        header('Content-length: '.strlen($ical));
+        header('Content-type: text/calendar');
+
+        echo $ical;
     }
 
     /**
--- web/application/controllers/calendar_exporter.php
+++ web/application/controllers/calendar_exporter.php
@@ -14,8 +14,7 @@ class Calendar_Exporter extends Ilios_Web_Controller
     public function __construct ()
     {
         parent::__construct();
-        $this->load->library("ICalExporter");
-        $this->load->library('CalendarFeedDataProvider');
+
     }
 
     /**
@@ -27,47 +26,5 @@ class Calendar_Exporter extends Ilios_Web_Controller
         $this->exportICalendar();
     }
 
-    /**
-     * @todo add code docs
-     * @param string $role
-     */
-    public function exportICalendar ($role='all')
-    {
-        $userRoles = array();
 
-        // authorization check and capture of user requested/available user roles
-        if ($this->session->userdata('is_learner') && ($role == 'all' || $role == 'student')) {
-            $userRoles[] = User_Role::STUDENT_ROLE_ID;
-        }
-        if ($this->session->userdata('has_instructor_access') && ($role == 'all' || $role == 'instructor')) {
-            $userRoles[] = User_Role::FACULTY_ROLE_ID;
-            $userRoles[] = User_Role::COURSE_DIRECTOR_ROLE_ID;
-        }
-        if (! count($userRoles)) {
-            header('HTTP/1.1 403 Forbidden'); // VERBOTEN!
-            return;
-        }
-
-        $userId = $this->session->userdata('uid');
-
-        $schoolId = $this->session->userdata('school_id');
-
-        $events = $this->calendarfeeddataprovider->getData($userId, $schoolId, $userRoles);
-
-        $calendar_title = 'Ilios Calendar';
-        $this->icalexporter->setTitle($calendar_title);
-        $ical = $this->icalexporter->toICal($events);
-
-        $filename="ilios_calendar.ics";
-        header('Last-Modified: '. gmdate('D, d M Y H:i:s', time()) .' GMT');
-        header('Cache-Control: private, must-revalidate, pre-check=0, post-check=0, max-age=0');
-        header('Expires: '. gmdate('D, d M Y H:i:s', 0) .'GMT');
-        header('Pragma: no-cache');
-        header('Accept-Ranges: none');
-        header('Content-disposition: attachment; filename='.$filename);
-        header('Content-length: '.strlen($ical));
-        header('Content-type: text/calendar');
-
-        echo $ical;
-    }
 }
