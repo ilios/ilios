c48707a660aea92426ba8f281340b7c650ac9961
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2014-01-08 14:09:43 -0800

    refactored code for flattening event details out into its own method. added todo item to class docblock. augmented event details for ILM based events.

--- web/application/libraries/CalendarFeedDataProvider.php
+++ web/application/libraries/CalendarFeedDataProvider.php
@@ -4,6 +4,10 @@
  * Data provider for the iCal export feature in the user dashboard and the calendar feed API.
  *
  * Architecturally, this class provides a layer of business logic/data processing that sits between controller and model.
+ *
+ * @todo Calendar events returned from the Model layer should be accessed via some sort of DTO/unified interface,
+ * @todo regardless whether they are are backed by offerings or ILMs. Juggling arrays is getting cumbersome.
+ * @todo [ST 2014/01/07]
  */
 class CalendarFeedDataProvider
 {
@@ -84,80 +88,11 @@ class CalendarFeedDataProvider
                 $event['event_details'] = $this->_ci->languagemap->t('general.acronyms.to_be_decided');
             } else {
                 $event['location'] = array_key_exists('room', $offering) ? $offering['room'] : null;  // LOCATION
-                $event['event_details'] = '';
-
-                if ($offering['description']) {
-                    $event['event_details'] .= $this->_unHTML($offering['description']) . "\n";
-                }
-
-                // Taught by
-                if (is_array($offering['instructors'])) {
-                    $event['event_details'] .= $this->_ci->languagemap->t('general.phrases.taught_by') . ' '
-                        . implode(', ', $offering['instructors']) . "\n";
-                }
-
-                // This offering is a(n)
-                $event['event_details'] .= $this->_ci->languagemap->t('dashboard.offering_description.offering_type')
-                    . ' ' . $offering['session_type'];
-
-                if ($offering['supplemental']) {
-                    $event['event_details'] .= $this->_ci->languagemap->t('dashboard.offering_description.offering_supplemental_suffix') . "\n";
-                } else {
-                    $event['event_details'] .= "\n";
-                }
-                if ($offering['attire_required']) {
-                    $event['event_details'] .= $this->_ci->languagemap->t('dashboard.offering_description.special_attire'). "\n";
-                }
-                if ($offering['equipment_required']) {
-                    $event['event_details'] .= $this->_ci->languagemap->t('dashboard.offering_description.special_equipment'). "\n";
-                }
-                if (count($offering['session_objectives']) > 0) {
-                    $event['event_details'] .= "\n";
-                    $event['event_details'] .= $this->_ci->languagemap->t('general.terms.session') . ' ';
-                    $event['event_details'] .= $this->_ci->languagemap->t('general.terms.objectives') . "\n";
-                    foreach ($offering['session_objectives'] as $objective)
-                        $event['event_details'] .= $this->_unHTML($objective) . "\n";
-                }
-                if (count($offering['session_materials']) > 0) {
-                    $event['event_details'] .= "\n";
-                    $event['event_details'] .= $this->_ci->languagemap->t('general.terms.session') . ' ';
-                    $event['event_details'] .= $this->_ci->languagemap->t('general.phrases.learning_materials') . "\n";
-                    foreach ($offering['session_materials'] as $material) {
-                        $event['event_details'] .= $this->_unHTML($material['title']);
-                        if ($material['required']) {
-                            $event['event_details'] .= ' (' . $this->_ci->languagemap->t('general.terms.required'). ')';
-                        }
-                        $$event['event_details'] .= ' (' . base_url()
-                            . 'ilios.php/learning_materials/getLearningMaterialWithId?learning_material_id='
-                            . $material['learning_material_id']
-                            . ')';
-                        $event['event_details'] .= ': ' . $this->_unHTML($material['description']) . "\n";
-                    }
-                }
-                if (count($offering['course_objectives']) > 0) {
-                    $event['event_details'] .= "\n";
-                    $event['event_details'] .= $this->_ci->languagemap->t('general.terms.course') . ' ';
-                    $event['event_details'] .= $this->_ci->languagemap->t('general.terms.objectives') . "\n";
-                    foreach ($offering['course_objectives'] as $objective) {
-                        $event['event_details'] .= $this->_unHTML($objective) . "\n";
-                    }
-                }
-                if (count($offering['course_materials']) > 0) {
-                    $event['event_details'] .= "\n";
-                    $event['event_details'] .= $this->_ci->languagemap->t('general.terms.course') . ' ';
-                    $event['event_details'] .= $this->_ci->languagemap->t('general.phrases.learning_materials') . "\n";
-                    foreach ($offering['course_materials'] as $material) {
-                        $event['event_details'] .= $this->_unHTML($material['title']);
-                        $event['event_details'] .= ' (' . base_url()
-                            . 'ilios.php/learning_materials/getLearningMaterialWithId?learning_material_id='
-                            . $material['learning_material_id']
-                            . ')';
-                        if ($material['required']) {
-                            $event['event_details'] .= ' (' . $this->_ci->languagemap->t('general.phrases.learning_materials') . ')';
-                        }
-                        $event['event_details'] .= ': ' . $this->_unHTML($material['description']) . "\n";
-                    }
-                }
+                $event['event_details'] = $this->_eventDetailsToText($offering['description'], $offering['session_type'],
+                    $offering['supplemental'], $offering['attire_required'], $offering['equipment_required'],
+                    $offering['session_objectives'], $offering['session_materials'], $offering['course_objectives'],
+                    $offerings['course_materials']
+                );
             }
             $events[$id] = $event;
         }
@@ -175,7 +110,7 @@ class CalendarFeedDataProvider
             $event['text'] .= strtolower($this->_ci->languagemap->t('general.terms.hours')) . ' ';
             $event['text'] .= strtolower($this->_ci->languagemap->t('general.phrases.due_by')) . ' ';
             $event['text'] .= strftime('%a, %b %d', strtotime($session['due_date'])) . ' - ';
-            $event['text'] .= $session['course_title'].' - '.$session['session_title']; // SUMMARY
+            $event['text'] .= $session['course_title'].' - ' . $session['session_title']; // SUMMARY
             $event['start_date'] = $session['due_date'] . ' 17:00:00';
             $event['end_date'] = $session['due_date'] . ' 17:30:00';
             $event['event_pid'] = null;
@@ -185,7 +120,11 @@ class CalendarFeedDataProvider
             if ($session['published_as_tbd'] || $session['course_published_as_tbd']) {
                 $event['event_details'] = $this->_ci->languagemap->t('general.acronyms.to_be_decided');
             } else {
-                $event['event_details'] = $session['event_details'];
+                $event['event_details'] = $event['event_details'] = $this->_eventDetailsToText($session['description'], $session['session_type'],
+                    $session['supplemental'], $session['attire_required'], $session['equipment_required'],
+                    $session['session_objectives'], $session['session_materials'], $session['course_objectives'],
+                    $session['course_materials']
+                );
             }
             $events[$id] = $event;
 
@@ -202,5 +141,114 @@ class CalendarFeedDataProvider
     protected function _unHTML ($s)
     {
         return str_replace("\n", ' ', trim(strip_tags($s)));
+    }
+
+    /** Flattens out given event details to a string.
+     * Any markup is removed from the content in the process.
+     *
+     * @param string $description The event description.
+     * @param string $sessionType The type of the event-owning session.
+     * @param bool $isSupplemental Flag indicating whether the owning session is supplemental.
+     * @param bool $requiresSpecialAttire Flag indicating whether the owning session requires special attire.
+     * @param bool $requiresSpecialEquipment Flag indicating whether the owning session requires special equipment.
+     * @param array $instructors A list of instructors teaching the event.
+     * @param array $sessionObjectives A list of session objectives for this event.
+     * @param array $sessionMaterials A list of session learning materials for this event.
+     * @param array $courseObjectives A list of course objectives for this event.
+     * @param array $courseMaterials A list of course learning materials for this event.
+     * @return string The aggregated given event details as text.
+     */
+    protected function _eventDetailsToText ($description = '', $sessionType = '', $isSupplemental = false,
+                                                   $requiresSpecialAttire = false, $requiresSpecialEquipment = false,
+                                                   array $instructors = array(), array $sessionObjectives = array(),
+                                                   array $sessionMaterials = array(), array $courseObjectives = array(),
+                                                   array $courseMaterials = array())
+    {
+        $rhett = '';
+
+        if ($description) {
+            $rhett = $this->_unHTML($description) . "\n";
+        }
+
+        // Taught by
+        if (! empty($instructors)) {
+            $rhett .= $this->_ci->languagemap->t('general.phrases.taught_by') . ' ' . implode(', ', $instructors) . "\n";
+        }
+
+        // This offering is a(n)
+        $rhett .= $this->_ci->languagemap->t('dashboard.offering_description.offering_type') . ' ' . $sessionType;
+
+        // is this event supplemental?
+        if ($isSupplemental) {
+            $rhett .= $this->_ci->languagemap->t('dashboard.offering_description.offering_supplemental_suffix') . "\n";
+        } else {
+            $rhett .= "\n";
+        }
+        // does this event require special attire?
+        if ($requiresSpecialAttire) {
+            $rhett .= $this->_ci->languagemap->t('dashboard.offering_description.special_attire'). "\n";
+        }
+
+
+        // does this event require special equipment?
+        if ($requiresSpecialEquipment) {
+            $rhett .= $this->_ci->languagemap->t('dashboard.offering_description.special_equipment'). "\n";
+        }
+
+        // flatten out session objectives
+        if (! empty($sessionObjectives)) {
+            $rhett .= "\n";
+            $rhett .= $this->_ci->languagemap->t('general.terms.session') . ' ';
+            $rhett .= $this->_ci->languagemap->t('general.terms.objectives') . "\n";
+            foreach ($sessionObjectives as $objective)
+                $rhett .= $this->_unHTML($objective) . "\n";
+        }
+
+        // flatten out session LMs
+        if (! empty($sessionMaterials)) {
+            $rhett .= "\n";
+            $rhett .= $this->_ci->languagemap->t('general.terms.session') . ' ';
+            $rhett .= $this->_ci->languagemap->t('general.phrases.learning_materials') . "\n";
+            foreach ($sessionMaterials as $material) {
+                $rhett .= $this->_unHTML($material['title']);
+                if ($material['required']) {
+                    $rhett .= ' (' . $this->_ci->languagemap->t('general.terms.required'). ')';
+                }
+                $rhett .= ' (' . base_url()
+                    . 'ilios.php/learning_materials/getLearningMaterialWithId?learning_material_id='
+                    . $material['learning_material_id']
+                    . ')';
+                $rhett .= ': ' . $this->_unHTML($material['description']) . "\n";
+            }
+        }
+
+        // flatten out course objectives
+        if (! empty($courseObjectives)) {
+            $rhett .= "\n";
+            $rhett .= $this->_ci->languagemap->t('general.terms.course') . ' ';
+            $rhett .= $this->_ci->languagemap->t('general.terms.objectives') . "\n";
+            foreach ($courseObjectives as $objective) {
+                $rhett .= $this->_unHTML($objective) . "\n";
+            }
+        }
+
+        // flatten out course LMs
+        if (! empty($courseMaterials)) {
+            $rhett .= "\n";
+            $rhett .= $this->_ci->languagemap->t('general.terms.course') . ' ';
+            $rhett .= $this->_ci->languagemap->t('general.phrases.learning_materials') . "\n";
+            foreach ($courseMaterials as $material) {
+                $rhett .= $this->_unHTML($material['title']);
+                $rhett .= ' (' . base_url()
+                    . 'ilios.php/learning_materials/getLearningMaterialWithId?learning_material_id='
+                    . $material['learning_material_id']
+                    . ')';
+                if ($material['required']) {
+                    $rhett.= ' (' . $this->_ci->languagemap->t('general.phrases.learning_materials') . ')';
+                }
+                $rhett .= ': ' . $this->_unHTML($material['description']) . "\n";
+            }
+        }
+        return $rhett;
     }
 }
