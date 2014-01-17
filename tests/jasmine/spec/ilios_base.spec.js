61df6c2c0e19d20e502a9d3d2a7354fc0782ccc9
Rich Trott <rtrott@gmail.com>
2014-01-16 22:40:43 -0800

    rm redundant matchers, add minimal test for namespace()

--- tests/jasmine/spec/ilios_base.spec.js
+++ tests/jasmine/spec/ilios_base.spec.js
@@ -7,23 +7,27 @@ describe("ilios_base", function() {
   describe("ilios", function () {
 
     it("should have a namespace method", function () {
-      expect(ilios.namespace).toBeDefined();
       expect(typeof ilios.namespace).toBe("function");
     });
 
     it("should have a lang namespace", function () {
-      expect(ilios.lang).toBeDefined();
       expect(typeof ilios.lang).toBe("object");
     });
 
     it("should have an alert namespace", function () {
-      expect(ilios.alert).toBeDefined();
       expect(typeof ilios.alert).toBe("object");
     });
 
     it("should have a global namespace", function () {
-      expect(ilios.global).toBeDefined();
       expect(typeof ilios.global).toBe("object");
+    });
+
+    describe("namespace()", function () {
+      it("should create the supplied namespace", function () {
+        expect(typeof ilios.foo).toBe("undefined");
+        ilios.namespace('foo');
+        expect(typeof ilios.foo).toBe("object");
+      });
     });
   });
 });
