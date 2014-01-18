f3b4516d22ad86d08d3fc7e93c1ee7666e05b2ba
Rich Trott <rtrott@gmail.com>
2014-01-18 12:04:42 -0800

    specs for USER_NAME_FORMAT_*

--- tests/jasmine/spec/ilios_utilities.spec.js
+++ tests/jasmine/spec/ilios_utilities.spec.js
@@ -2,4 +2,18 @@ describe("ilios_utilities", function() {
   it("should create a utilities namespace on the ilios global object", function () {
     expect(typeof ilios.utilities).toBe("object");
   });
+
+  it("should define 3 different user name formats", function () {
+    var formats = [
+      ilios.utilities.USER_NAME_FORMAT_LAST_FIRST,
+      ilios.utilities.USER_NAME_FORMAT_FIRST_FIRST,
+      ilios.utilities.USER_NAME_FORMAT_FIRST_INITIAL_FIRST
+    ];
+    expect(formats[0]).toBeDefined();
+    expect(formats[1]).toBeDefined();
+    expect(formats[2]).toBeDefined();
+    expect(formats[0]).not.toEqual(formats[1]);
+    expect(formats[0]).not.toEqual(formats[2]);
+    expect(formats[1]).not.toEqual(formats[2]);
+  });
 });
