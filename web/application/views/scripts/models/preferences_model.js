b8abaed0291fa9f7bb4b9caa64fdd4eefec63bdf
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2014-01-31 14:16:31 -0800

    annotated methods and properties. removed obsolete property and method.

--- web/application/views/scripts/models/preferences_model.js
+++ web/application/views/scripts/models/preferences_model.js
@@ -1,43 +1,67 @@
 /**
- * This class serves as a small model containing the currently logged in user's preferences state.
+ * This object serves as a small model containing the currently logged-in user's preferences state.
+ *
+ * @class PreferencesModel
+ * @param {Object} [serverDispatchedObject]
+ * @constructor
  */
-
 function PreferencesModel (serverDispatchedObject) {
 
-    this.langLocale = 'en_US';
-
+    /**
+     * @property canArchiveProgramYears
+     * @type {Boolean}
+     */
     this.canArchiveProgramYears = false;
+
+    /**
+     * @property canArchiveCourses
+     * @type {Boolean}
+     */
     this.canArchiveCourses = false;
 
+    /**
+     * @property canRolloverCourses
+     * @type {Boolean}
+     */
     this.canRolloverCourses = false;
 
-    if (typeof serverDispatchedObject != 'undefined') {
+    if (typeof serverDispatchedObject !== 'undefined') {
         this.updateWithServerDispatchedObject(serverDispatchedObject);
     }
-
 }
-
-PreferencesModel.prototype.getLangLocale = function () {
-    return this.langLocale;
-};
-
+/**
+ * @method showCourseArchiving
+ * @return {Boolean}
+ */
 PreferencesModel.prototype.showCourseArchiving = function () {
     return this.canArchiveCourses;
 };
 
+/**
+ * @method showProgramYearArchiving
+ * @return {Boolean}
+ */
 PreferencesModel.prototype.showProgramYearArchiving = function () {
     return this.canArchiveProgramYears;
 };
 
+/**
+ * @method showCourseRollover
+ * @return {Boolean}
+ */
 PreferencesModel.prototype.showCourseRollover = function () {
     return this.canRolloverCourses;
 };
 
+/**
+ * @method updateWithServerDispatchedObject
+ * @param {Object} serverDispatchedObject
+ *   @param {String} serverDispatchedObject.py_archiving
+ *   @param {String} serverDispatchedObject.course_archiving
+ *   @param {String} serverDispatchedObject.course_rollover
+ */
 PreferencesModel.prototype.updateWithServerDispatchedObject = function (serverDispatchedObject) {
-    this.langLocale = serverDispatchedObject.lang;
-
-    this.canArchiveProgramYears = (serverDispatchedObject.py_archiving == 'true');
-    this.canArchiveCourses = (serverDispatchedObject.course_archiving == 'true');
-
-    this.canRolloverCourses = (serverDispatchedObject.course_rollover == 'true');
+    this.canArchiveProgramYears = (serverDispatchedObject.py_archiving === 'true');
+    this.canArchiveCourses = (serverDispatchedObject.course_archiving === 'true');
+    this.canRolloverCourses = (serverDispatchedObject.course_rollover === 'true');
 };
