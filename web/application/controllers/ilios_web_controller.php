3032b637b3159e7f4677f20ee13867eeb94bb335
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2014-02-01 12:29:20 -0800

    added proper doc block.

--- web/application/controllers/ilios_web_controller.php
+++ web/application/controllers/ilios_web_controller.php
@@ -408,7 +408,12 @@ abstract class Ilios_Web_Controller extends Ilios_Base_Controller
 
 
     /**
-     * @todo add code docs
+     * Retrieves various application preferences from the user session.
+     *
+     * @return array An associative array containing user-set preferences under the following keys:
+     *     'py_archiving'     ... A boolean indicating whether program-year archiving has been enabled or not.
+     *     'course_archiving' ... A boolean indicating whether course archiving has been enabled or not.
+     *     'course_rollover'  ... A boolean indicating whether course rollover has been turned on or not.
      */
     protected function _getUserPreferences ()
     {
