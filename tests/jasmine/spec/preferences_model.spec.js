b619bfc86de0280ace9503a8426e43f44276d06d
Rich Trott <rtrott@gmail.com>
2014-02-09 12:13:54 -0800

    use getters

--- tests/jasmine/spec/preferences_model.spec.js
+++ tests/jasmine/spec/preferences_model.spec.js
@@ -2,9 +2,9 @@ describe("PreferencesModel", function () {
   describe("constructor", function () {
     it("should default all properties to false", function () {
       var prefs = new PreferencesModel();
-      expect(prefs.canArchiveProgramYears).toBe(false);
-      expect(prefs.canArchiveCourses).toBe(false);
-      expect(prefs.canRolloverCourses).toBe(false);
+      expect(prefs.showCourseArchiving()).toBe(false);
+      expect(prefs.showProgramYearArchiving()).toBe(false);
+      expect(prefs.showCourseRollover()).toBe(false);
     });
   });
 });
