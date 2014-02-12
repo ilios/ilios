1317b95754ff7d4f8ceaa1c93977622f640f9494
Rich Trott <rtrott@gmail.com>
2014-02-11 22:19:24 -0800

    move specs from pref model to ilios base

--- tests/jasmine/spec/ilios_base.spec.js
+++ tests/jasmine/spec/ilios_base.spec.js
@@ -72,6 +72,28 @@ describe("ilios_base", function() {
           expect(typeof ilios.global.preferencesModel.updateWithServerDispatchedObject).toBe("function");
         });
 
+        it("should default all properties to false", function () {
+          ilios.global.installPreferencesModel();
+          expect(ilios.global.preferencesModel.courseArchiving).toBe(false);
+          expect(ilios.global.preferencesModel.programYearArchiving).toBe(false);
+          expect(ilios.global.preferencesModel.courseRollover).toBe(false);
+        });
+
+        describe("updateWithServerDispatchedObject()", function () {
+          it("should update from provided object", function () {
+            ilios.global.installPreferencesModel();
+            var updateObject = {
+              py_archiving: true,
+              course_archiving: true,
+              course_rollover: true
+            };
+            ilios.global.preferencesModel.updateWithServerDispatchedObject(updateObject);
+            expect(ilios.global.preferencesModel.courseArchiving).toBe(true);
+            expect(ilios.global.preferencesModel.programYearArchiving).toBe(true);
+            expect(ilios.global.preferencesModel.courseRollover).toBe(true);
+          });
+        });
+
         it("should intialize preferencesModel with prefs data in DOM", function () {
           container.innerHTML = "{\"py_archiving\":true,\"course_archiving\":true,\"course_rollover\":true}";
           document.body.appendChild(container);
@@ -93,85 +115,85 @@ describe("ilios_base", function() {
         });
       });
 
-      describe("startIdleTimer()", function () {
-        beforeEach(function () {
+describe("startIdleTimer()", function () {
+  beforeEach(function () {
           // test double
           YAHOO.util.IdleTimer = {subscribe: function () {}, start: function () {}};
           spyOn(YAHOO.util.IdleTimer, "subscribe");
           spyOn(YAHOO.util.IdleTimer, "start");
         });
 
-        afterEach(function () {
+  afterEach(function () {
           // clean up test double
           delete YAHOO.util.IdleTimer;
         });
 
-        it("should use the supplied timeout", function () {
-          var timeout = 9999999;
-          ilios.global.startIdleTimer(timeout);
-          expect(YAHOO.util.IdleTimer.start).toHaveBeenCalledWith(timeout, document);
-        });
+  it("should use the supplied timeout", function () {
+    var timeout = 9999999;
+    ilios.global.startIdleTimer(timeout);
+    expect(YAHOO.util.IdleTimer.start).toHaveBeenCalledWith(timeout, document);
+  });
 
-        it("should use default timeout if supplied timeout is not a number", function () {
-          var timeout = "totally not a number";
-          ilios.global.startIdleTimer(timeout);
-          expect(YAHOO.util.IdleTimer.start).not.toHaveBeenCalledWith(timeout, document);
-          expect(YAHOO.util.IdleTimer.start).toHaveBeenCalledWith(2700000, document);
-        });
+  it("should use default timeout if supplied timeout is not a number", function () {
+    var timeout = "totally not a number";
+    ilios.global.startIdleTimer(timeout);
+    expect(YAHOO.util.IdleTimer.start).not.toHaveBeenCalledWith(timeout, document);
+    expect(YAHOO.util.IdleTimer.start).toHaveBeenCalledWith(2700000, document);
+  });
 
-        it("should call subscribe with the hardcoded callback", function () {
-          ilios.global.startIdleTimer();
-          expect(YAHOO.util.IdleTimer.subscribe).toHaveBeenCalledWith("idle", jasmine.any(Function));
-        });
-      });
+  it("should call subscribe with the hardcoded callback", function () {
+    ilios.global.startIdleTimer();
+    expect(YAHOO.util.IdleTimer.subscribe).toHaveBeenCalledWith("idle", jasmine.any(Function));
+  });
+});
 
-      describe("defaultAJAXFailureHandler()", function () {
-        beforeEach(function () {
-          spyOn(ilios.alert, "alert");
+describe("defaultAJAXFailureHandler()", function () {
+  beforeEach(function () {
+    spyOn(ilios.alert, "alert");
           // test double
           window.ilios_i18nVendor = {getI18NString: function (string) { return string; }};
         });
 
-        afterEach(function () {
+  afterEach(function () {
           // clean up test double
           delete window.ilios_i18nVendor;
         });
 
-        it("should call ilios.alert.alert()", function () {
-          ilios.global.defaultAJAXFailureHandler({responseText: "foo"}, {description: "bar"});
-          expect(ilios.alert.alert).toHaveBeenCalled();
-        });
+  it("should call ilios.alert.alert()", function () {
+    ilios.global.defaultAJAXFailureHandler({responseText: "foo"}, {description: "bar"});
+    expect(ilios.alert.alert).toHaveBeenCalled();
+  });
 
-        it("should use the rootException.description if root expection is defined", function () {
-          ilios.global.defaultAJAXFailureHandler({responseText: "foo"}, {description: "bar"});
-          expect(ilios.alert.alert).toHaveBeenCalledWith("general.error.fatal (bar)");
-        });
+  it("should use the rootException.description if root expection is defined", function () {
+    ilios.global.defaultAJAXFailureHandler({responseText: "foo"}, {description: "bar"});
+    expect(ilios.alert.alert).toHaveBeenCalledWith("general.error.fatal (bar)");
+  });
 
-        it("should use the resultObject.responseText if rootException is undefined", function () {
-          ilios.global.defaultAJAXFailureHandler({responseText: "foo"});
-          expect(ilios.alert.alert).toHaveBeenCalledWith("general.error.fatal (foo)");
-        });
-      });
+  it("should use the resultObject.responseText if rootException is undefined", function () {
+    ilios.global.defaultAJAXFailureHandler({responseText: "foo"});
+    expect(ilios.alert.alert).toHaveBeenCalledWith("general.error.fatal (foo)");
+  });
+});
 
-      describe("longDayOfWeekI18NStrings", function () {
-        it("should be initialized to null", function () {
-          expect(ilios.global.longDayOfWeekI18NStrings).toBe(null);
-        });
-      });
+describe("longDayOfWeekI18NStrings", function () {
+  it("should be initialized to null", function () {
+    expect(ilios.global.longDayOfWeekI18NStrings).toBe(null);
+  });
+});
 
-      describe("shortDayOfWeekI18NStrings", function () {
-        it("should be initialized to null", function () {
-          expect(ilios.global.shortDayOfWeekI18NStrings).toBe(null);
-        });
-      });
+describe("shortDayOfWeekI18NStrings", function () {
+  it("should be initialized to null", function () {
+    expect(ilios.global.shortDayOfWeekI18NStrings).toBe(null);
+  });
+});
 
-      describe("getI18NStringForDayOfWeek()", function () {
-        beforeEach(function () {
+describe("getI18NStringForDayOfWeek()", function () {
+  beforeEach(function () {
           // test double
           window.ilios_i18nVendor = {getI18NString: function (string) { return string; }};
         });
 
-        afterEach(function () {
+  afterEach(function () {
           // clean up test double
           delete window.ilios_i18nVendor;
 
@@ -180,38 +202,38 @@ describe("ilios_base", function() {
           ilios.global.shortDayOfWeekI18NStrings = null;
         });
 
-        it("should return empty string if day is less than 0", function () {
-          expect(ilios.global.getI18NStringForDayOfWeek(-1)).toBe("");
-        });
-
-        it("should return empty string if day is greatern than 6", function () {
-          expect(ilios.global.getI18NStringForDayOfWeek(7)).toBe("");
-        });
-
-        it("should load shortDayOfWeekI18NStrings and not longDayOfWeekI18NStrings if shortString is true", function () {
-          expect(ilios.global.shortDayOfWeekI18NStrings).toBe(null);
-          expect(ilios.global.longDayOfWeekI18NStrings).toBe(null);
-          ilios.global.getI18NStringForDayOfWeek(0, true);
-          expect(ilios.global.shortDayOfWeekI18NStrings).not.toBe(null);
-          expect(ilios.global.longDayOfWeekI18NStrings).toBe(null);
-        });
-
-        it("should not load shortDayOfWeekI18NStrings and load longDayOfWeekI18NStrings if shortString is false", function () {
-          expect(ilios.global.shortDayOfWeekI18NStrings).toBe(null);
-          expect(ilios.global.longDayOfWeekI18NStrings).toBe(null);
-          ilios.global.getI18NStringForDayOfWeek(0, false);
-          expect(ilios.global.shortDayOfWeekI18NStrings).toBe(null);
-          expect(ilios.global.longDayOfWeekI18NStrings).not.toBe(null);
-        });
-
-        it("should return short string if requested", function () {
-          expect(ilios.global.getI18NStringForDayOfWeek(0, true)).toBe("general.calendar.sunday_short");
-        });
-
-        it("should return long string if requested", function () {
-          expect(ilios.global.getI18NStringForDayOfWeek(0, false)).toBe("general.calendar.sunday_long");
-        });
-      });
-    });
+  it("should return empty string if day is less than 0", function () {
+    expect(ilios.global.getI18NStringForDayOfWeek(-1)).toBe("");
   });
+
+  it("should return empty string if day is greatern than 6", function () {
+    expect(ilios.global.getI18NStringForDayOfWeek(7)).toBe("");
+  });
+
+  it("should load shortDayOfWeekI18NStrings and not longDayOfWeekI18NStrings if shortString is true", function () {
+    expect(ilios.global.shortDayOfWeekI18NStrings).toBe(null);
+    expect(ilios.global.longDayOfWeekI18NStrings).toBe(null);
+    ilios.global.getI18NStringForDayOfWeek(0, true);
+    expect(ilios.global.shortDayOfWeekI18NStrings).not.toBe(null);
+    expect(ilios.global.longDayOfWeekI18NStrings).toBe(null);
+  });
+
+  it("should not load shortDayOfWeekI18NStrings and load longDayOfWeekI18NStrings if shortString is false", function () {
+    expect(ilios.global.shortDayOfWeekI18NStrings).toBe(null);
+    expect(ilios.global.longDayOfWeekI18NStrings).toBe(null);
+    ilios.global.getI18NStringForDayOfWeek(0, false);
+    expect(ilios.global.shortDayOfWeekI18NStrings).toBe(null);
+    expect(ilios.global.longDayOfWeekI18NStrings).not.toBe(null);
+  });
+
+  it("should return short string if requested", function () {
+    expect(ilios.global.getI18NStringForDayOfWeek(0, true)).toBe("general.calendar.sunday_short");
+  });
+
+  it("should return long string if requested", function () {
+    expect(ilios.global.getI18NStringForDayOfWeek(0, false)).toBe("general.calendar.sunday_long");
+  });
+});
+});
+});
 });
--- tests/jasmine/spec/preferences_model.spec.js
+++ tests/jasmine/spec/preferences_model.spec.js
@@ -1,25 +1,3 @@
 describe("PreferencesModel", function () {
-  describe("constructor", function () {
-    it("should default all properties to false", function () {
-      var prefs = new PreferencesModel();
-      expect(prefs.courseArchiving).toBe(false);
-      expect(prefs.programYearArchiving).toBe(false);
-      expect(prefs.courseRollover).toBe(false);
-    });
-  });
 
-  describe("updateWithServerDispatchedObject()", function () {
-    it("should update from provided object", function () {
-      var prefs = new PreferencesModel();
-      var updateObject = {
-        py_archiving: true,
-        course_archiving: true,
-        course_rollover: true
-      };
-      prefs.updateWithServerDispatchedObject(updateObject);
-      expect(prefs.courseArchiving).toBe(true);
-      expect(prefs.programYearArchiving).toBe(true);
-      expect(prefs.courseRollover).toBe(true);
-    });
-  });
 });
