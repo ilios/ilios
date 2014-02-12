6a059b4fe7b51e64f8ad9a643ad87135f5064f1d
Rich Trott <rtrott@gmail.com>
2014-02-11 22:58:32 -0800

    move prefs to its own file, nothing for it in base please

--- web/application/views/scripts/ilios_base.js
+++ web/application/views/scripts/ilios_base.js
@@ -73,48 +73,6 @@ if (YAHOO.lang.JSON) {
 ilios.namespace('global');
 
 /**
- * The page's preferences model.
- * Should be instantiated by calling <code>ilios.global.installPreferencesModel()</code>.
- * @property {PreferencesModel} preferencesModel
- */
-ilios.global.preferencesModel = null;
-
-/**
- * Instantiates <code>ilios.global.preferencesModel</code>.
- * @method installPreferencesModel
- */
-ilios.global.installPreferencesModel = function () {
-    ilios.global.preferencesModel = {
-        programYearArchiving: false,
-        courseArchiving: false,
-        courseRollover: false,
-
-        /**
-         * @method updateWithServerDispatchedObject
-         * @param {Object} serverDispatchedObject
-         *   @param {Boolean} serverDispatchedObject.py_archiving
-         *   @param {Boolean} serverDispatchedObject.course_archiving
-         *   @param {Boolean} serverDispatchedObject.course_rollover
-         */
-        updateWithServerDispatchedObject: function (serverDispatchedObject) {
-            this.programYearArchiving = serverDispatchedObject.py_archiving;
-            this.courseArchiving = serverDispatchedObject.course_archiving;
-            this.courseRollover = serverDispatchedObject.course_rollover;
-        }
-    };
-
-    var domData = document.getElementById("iliosPreferencesModel");
-    if (domData) {
-        try {
-            ilios.global.preferencesModel.updateWithServerDispatchedObject(JSON.parse(domData.innerHTML));
-        } catch (e) {
-            // SOL
-            ilios.global.defaultAJAXFailureHandler(null, e);
-        }
-    }
-};
-
-/**
  * Instantiates and starts the idle timer, subscribes a timeout-handler function to it.
  * @method startIdleTimer
  * @param {Number} Timeout period in milliseconds
