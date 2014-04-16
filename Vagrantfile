e5f9c6be26109fb65992465856f6c9b1a11b0049
Jonathan Johnson <jon.johnson@ucsf.edu>
2014-04-16 15:06:51 -0700

    Use puppet labs apache
    Using supported puppet module to configure apache.

--- Vagrantfile
+++ Vagrantfile
@@ -4,7 +4,7 @@
 Vagrant.configure("2") do |config|
   config.vm.box = "precise32"
   config.vm.box_url = "http://files.vagrantup.com/precise32.box"
-  config.vm.hostname = "iliosdev.localdomain"
+  config.vm.hostname = "iliosdev"
   config.vm.provision "puppet" do |puppet|
     puppet.manifests_path = "puppet/manifests"
     puppet.module_path = "puppet/modules"
--- puppet/manifests/ilios.pp
+++ puppet/manifests/ilios.pp
@@ -45,20 +45,33 @@ class ilios (
   class { '::mysql::bindings': 
     php_enable    => true
   }
+
+  class { 'apache':
+    default_mods        => false,
+    default_vhost       => false,
+    default_confd_files => false,
+    mpm_module => 'prefork'
+  }
+  class {'apache::mod::php': }
+  class {'apache::mod::rewrite': }
+  class {'apache::mod::headers': }
+  class {'apache::mod::setenvif': }
   
-  package { "apache2":
-    ensure => present,
+
+   package { "php5-ldap":
+    ensure  => present,
+    require => Class['apache::mod::php']
   }
 
-  package { "libapache2-mod-php5":
-    ensure => present,
-    notify => Service["apache2"],
-  }
-
-  package { "php5-ldap":
-    ensure => present,
-    require => Package["libapache2-mod-php5"],
-    notify => Service["apache2"],
+  apache::vhost { 'iliosdev':
+    default_vhost   => true,
+    docroot         => $docroot,
+    docroot_owner   => 'vagrant',
+    docroot_group   => 'vagrant',
+    override        => ['all'],
+    ssl             => true,
+    port            => '443',
+    ip              => '*'
   }
 
   package { "sendmail":
@@ -69,24 +82,6 @@ class ilios (
     ensure => present,
   }
 
-  service { "apache2":
-    ensure => running,
-    enable => true,
-    require => Package["apache2"],
-  }
-
-   exec {"add-mod-rewrite":
-    command => "/usr/sbin/a2enmod rewrite",
-    notify => Service["apache2"],
-    require => Package["apache2"],
-  }
-
-  exec {"add-mod-headers":
-    command => "/usr/sbin/a2enmod headers",
-    notify => Service["apache2"],
-    require => Package["apache2"],
-  }
-
   exec {"create-db":
     cwd => "${repodir}/database/install",
     unless => "/usr/bin/sudo /bin/ls /var/lib/mysql/${dbname}/mesh_concept_x_term.MYI",
@@ -94,40 +89,8 @@ class ilios (
     require => [Class["::mysql::server"],Package["expect"]],
   }
 
-  exec {"allow-override":
-    cwd => "/etc/apache2/sites-enabled",
-    command => "/usr/bin/find . -! -type d -exec /bin/sed -i '/<Directory \\/var\\/www\\/>/,/<\\/Directory>/ { s#AllowOverride None#AllowOverride All#}' {} \\;",
-    notify => Service["apache2"],
-    require => Exec["link-ssl-conf-file"],
-  }
-
-  exec {"enable-mod-ssl":
-    command => "/usr/sbin/a2enmod ssl",
-    require => Package["apache2"],
-  }
-
-  exec {"link-ssl-conf-file":
-    unless => "/bin/ls /etc/apache2/sites-enabled/000-default-ssl",
-    command => "/bin/ln -s /etc/apache2/sites-available/default-ssl /etc/apache2/sites-enabled/000-default-ssl",
-    notify => Service["apache2"],
-    require => Exec["enable-mod-ssl"],
-  }
-
   file {"${docroot}/application/config/config.php":
     source => "${docroot}/application/config/default.config.php",
-  }
-
-  file {"${docroot}/application/config/ilios.php":
-    source => "${docroot}/application/config/default.ilios.php"
-  }
-
-  file {"${docroot}/application/config/database.php":
-    source => "${docroot}/application/config/default.database.php"
-  }
-
-  file {"${docroot}/index.html":
-    ensure => "absent",
-    require => Service["apache2"],
   }
 
   exec {"set-version":
@@ -154,4 +117,4 @@ class ilios (
 
 }
 
-include ilios
+class{'ilios': }
