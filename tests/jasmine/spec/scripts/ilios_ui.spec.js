624522198d98e603a0e17b9d98988250d722f730
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2014-04-24 16:12:59 -0700

    fixed namespace test. added type check test for ilios custom event.

--- tests/jasmine/spec/scripts/ilios_ui.spec.js
+++ tests/jasmine/spec/scripts/ilios_ui.spec.js
@@ -1,5 +1,10 @@
 describe("ilios_ui", function() {
     it("should create a ui namespace on the ilios global object", function () {
-        expect(typeof ilios.utilities).toBe("object");
+        expect(typeof ilios.ui).toBe("object");
+    });
+    describe("onIliosEvent", function () {
+        it("should be a YUI custom event", function () {
+            expect(ilios.ui.onIliosEvent instanceof YAHOO.util.CustomEvent).toBeTruthy();
+        });
     });
 });
