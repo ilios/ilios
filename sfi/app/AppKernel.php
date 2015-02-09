72cdf4053d2cf538e12498ecc18f364f601acd59
Jonathan Johnson <jon.johnson@ucsf.edu>
2015-02-09 00:59:33 -0800

    Re-enable testing dependancies

--- sfi/app/AppKernel.php
+++ sfi/app/AppKernel.php
@@ -31,6 +31,7 @@ class AppKernel extends Kernel
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+            $bundles[] = new IC\Bundle\Base\TestBundle\ICBaseTestBundle();
         }
 
         return $bundles;
