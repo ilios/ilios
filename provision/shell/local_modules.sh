e9e7606928e9bdfb0e7cfda33449dddc7ac95967
Victor Passapera <vpassapera@gmail.com>
2015-03-09 11:33:32 -0700

    Move php to Puppetfile

--- provision/shell/local_modules.sh
+++ provision/shell/local_modules.sh
@@ -8,7 +8,3 @@ fi
 #Copy local modules
 echo "Copying local modules to puppet dir..."
 cp -r /vagrant/provision/puppet/modules/* "$PUPPET_MDOULE_DIR"
-
-##Puppet modules that librarian won't install...PRs in some of them to address this
-echo "Installing modules uninstallable by Librarian Puppet"
-puppet module install nodes/php --ignore-dependencies --modulepath "$PUPPET_MDOULE_DIR" #Deps handled in Puppetfile
