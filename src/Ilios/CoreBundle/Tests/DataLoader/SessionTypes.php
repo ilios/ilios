faba99fd64448ab6e3856744b9a5be49e79a1c94
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2015-03-15 15:06:17 -0700

    stubbed Session Types dataloader.

---
+++ src/Ilios/CoreBundle/Tests/DataLoader/SessionTypes.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Ilios\CoreBundle\Tests\DataLoader;
+
+class SessionTypes
+{
+    public function get()
+    {
+        // @todo implement.
+        $arr = array();
+        return $arr;
+    }
+}
