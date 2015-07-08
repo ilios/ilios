3269953c8fd1c571e79bac0a5bb3ab432f5c5e13
Jonathan Johnson <jon.johnson@ucsf.edu>
2015-07-08 09:24:03 -0700

    set deleted flag for sessions

--- src/Ilios/CoreBundle/Entity/Manager/SessionManager.php
+++ src/Ilios/CoreBundle/Entity/Manager/SessionManager.php
@@ -22,6 +22,7 @@ class SessionManager extends AbstractManager implements SessionManagerInterface
         array $criteria,
         array $orderBy = null
     ) {
+        $criteria['deleted'] = false;
         return $this->repository->findOneBy($criteria, $orderBy);
     }
 
@@ -39,6 +40,7 @@ class SessionManager extends AbstractManager implements SessionManagerInterface
         $limit = null,
         $offset = null
     ) {
+        $criteria['deleted'] = false;
         return $this->repository->findBy($criteria, $orderBy, $limit, $offset);
     }
 
@@ -70,8 +72,8 @@ class SessionManager extends AbstractManager implements SessionManagerInterface
     public function deleteSession(
         SessionInterface $session
     ) {
-        $this->em->remove($session);
-        $this->em->flush();
+        $session->setDeleted(true);
+        $this->updateSession($session);
     }
 
     /**
