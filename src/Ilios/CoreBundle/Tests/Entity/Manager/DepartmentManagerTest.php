8eb8887eedadb46c371944ca77c33af3f7c77c45
Jonathan Johnson <jon.johnson@ucsf.edu>
2015-07-08 01:01:22 -0700

    Set deleted flag for departments

---
+++ src/Ilios/CoreBundle/Tests/Entity/Manager/DepartmentManagerTest.php
@@ -0,0 +1,84 @@
+<?php 
+namespace Ilios\CoreBundle\Tests\Entity\Manager;
+
+use Ilios\CoreBundle\Entity\Manager\DepartmentManager;
+use IC\Bundle\Base\TestBundle\Test\TestCase;
+use Mockery as m;
+
+/**
+ * Tests for Entity AamcMethod
+ */
+class DepartmentManagerTest  extends TestCase
+{
+    /**
+     * Remove all mock objects
+     */
+    public function tearDown()
+    {
+        m::close();
+    }
+    
+    /**
+     * @covers Ilios\CoreBundle\Entity\Manager\DepartmentManager::deleteDepartment
+     */
+    public function testDeleteDepartment()
+    {
+        $class = 'Ilios\CoreBundle\Entity\Department';
+        $em = m::mock('Doctrine\ORM\EntityManager')
+            ->shouldReceive('persist')->shouldReceive('flush')->mock();
+        $repository = m::mock('Doctrine\ORM\Repository');
+        $registry = m::mock('Doctrine\Bundle\DoctrineBundle\Registry')
+            ->shouldReceive('getManagerForClass')
+            ->andReturn($em)
+            ->shouldReceive('getRepository')
+            ->andReturn($repository)
+            ->mock();
+        
+        $entity = m::mock($class)
+            ->shouldReceive('setDeleted')->with(true)->mock();
+        $manager = new DepartmentManager($registry, $class);
+        $manager->deleteDepartment($entity);
+    }
+    
+    /**
+     * @covers Ilios\CoreBundle\Entity\Manager\DepartmentManager::findDepartmentBy
+     */
+    public function testFindDepartmentDoesNotIncludeDeleted()
+    {
+        $class = 'Ilios\CoreBundle\Entity\Department';
+        $em = m::mock('Doctrine\ORM\EntityManager');
+        $repository = m::mock('Doctrine\ORM\Repository')
+            ->shouldReceive('findOneBy')
+            ->with(array('foo' => 'bar', 'deleted' => false), null)
+            ->mock();
+        $registry = m::mock('Doctrine\Bundle\DoctrineBundle\Registry')
+            ->shouldReceive('getManagerForClass')
+            ->andReturn($em)
+            ->shouldReceive('getRepository')
+            ->andReturn($repository)
+            ->mock();
+        $manager = new DepartmentManager($registry, $class);
+        $manager->findDepartmentBy(array('foo' => 'bar'));
+    }
+    
+    /**
+     * @covers Ilios\CoreBundle\Entity\Manager\DepartmentManager::findDepartmentsBy
+     */
+    public function testFindDepartmentsDoNotIncludeDeleted()
+    {
+        $class = 'Ilios\CoreBundle\Entity\Department';
+        $em = m::mock('Doctrine\ORM\EntityManager');
+        $repository = m::mock('Doctrine\ORM\Repository')
+            ->shouldReceive('findBy')
+            ->with(array('foo' => 'bar', 'deleted' => false), null, null, null)
+            ->mock();
+        $registry = m::mock('Doctrine\Bundle\DoctrineBundle\Registry')
+            ->shouldReceive('getManagerForClass')
+            ->andReturn($em)
+            ->shouldReceive('getRepository')
+            ->andReturn($repository)
+            ->mock();
+        $manager = new DepartmentManager($registry, $class);
+        $manager->findDepartmentsBy(array('foo' => 'bar'));
+    }
+}
