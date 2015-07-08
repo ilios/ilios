054b39a988da59f978fbb8bf64c2dacdff539bf4
Jonathan Johnson <jon.johnson@ucsf.edu>
2015-07-08 09:16:57 -0700

    set deleted flag for program year

---
+++ src/Ilios/CoreBundle/Tests/Entity/Manager/ProgramYearManagerTest.php
@@ -0,0 +1,84 @@
+<?php 
+namespace Ilios\CoreBundle\Tests\Entity\Manager;
+
+use Ilios\CoreBundle\Entity\Manager\ProgramYearManager;
+use IC\Bundle\Base\TestBundle\Test\TestCase;
+use Mockery as m;
+
+/**
+ * Tests for Entity AamcMethod
+ */
+class ProgramYearManagerTest  extends TestCase
+{
+    /**
+     * Remove all mock objects
+     */
+    public function tearDown()
+    {
+        m::close();
+    }
+    
+    /**
+     * @covers Ilios\CoreBundle\Entity\Manager\ProgramYearManager::deleteProgramYear
+     */
+    public function testDeleteProgramYear()
+    {
+        $class = 'Ilios\CoreBundle\Entity\ProgramYear';
+        $em = m::mock('Doctrine\ORM\EntityManager')
+            ->shouldReceive('persist')->shouldReceive('flush')->mock();
+        $repository = m::mock('Doctrine\ORM\Repository');
+        $registry = m::mock('Doctrine\Bundle\DoctrineBundle\Registry')
+            ->shouldReceive('getManagerForClass')
+            ->andReturn($em)
+            ->shouldReceive('getRepository')
+            ->andReturn($repository)
+            ->mock();
+        
+        $entity = m::mock($class)
+            ->shouldReceive('setDeleted')->with(true)->mock();
+        $manager = new ProgramYearManager($registry, $class);
+        $manager->deleteProgramYear($entity);
+    }
+    
+    /**
+     * @covers Ilios\CoreBundle\Entity\Manager\ProgramYearManager::findProgramYearBy
+     */
+    public function testFindProgramYearDoesNotIncludeDeleted()
+    {
+        $class = 'Ilios\CoreBundle\Entity\ProgramYear';
+        $em = m::mock('Doctrine\ORM\EntityManager');
+        $repository = m::mock('Doctrine\ORM\Repository')
+            ->shouldReceive('findOneBy')
+            ->with(array('foo' => 'bar', 'deleted' => false), null)
+            ->mock();
+        $registry = m::mock('Doctrine\Bundle\DoctrineBundle\Registry')
+            ->shouldReceive('getManagerForClass')
+            ->andReturn($em)
+            ->shouldReceive('getRepository')
+            ->andReturn($repository)
+            ->mock();
+        $manager = new ProgramYearManager($registry, $class);
+        $manager->findProgramYearBy(array('foo' => 'bar'));
+    }
+    
+    /**
+     * @covers Ilios\CoreBundle\Entity\Manager\ProgramYearManager::findProgramYearsBy
+     */
+    public function testFindProgramYearsDoNotIncludeDeleted()
+    {
+        $class = 'Ilios\CoreBundle\Entity\ProgramYear';
+        $em = m::mock('Doctrine\ORM\EntityManager');
+        $repository = m::mock('Doctrine\ORM\Repository')
+            ->shouldReceive('findBy')
+            ->with(array('foo' => 'bar', 'deleted' => false), null, null, null)
+            ->mock();
+        $registry = m::mock('Doctrine\Bundle\DoctrineBundle\Registry')
+            ->shouldReceive('getManagerForClass')
+            ->andReturn($em)
+            ->shouldReceive('getRepository')
+            ->andReturn($repository)
+            ->mock();
+        $manager = new ProgramYearManager($registry, $class);
+        $manager->findProgramYearsBy(array('foo' => 'bar'));
+    }
+}
