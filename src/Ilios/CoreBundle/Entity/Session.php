cf9e9c8b28225d6531ff0503c54d35c3b1dc212e
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2015-07-08 12:01:29 -0700

    Issue 868: re-defined session/ilmSessionFacet relationship as one-to-one.

--- src/Ilios/CoreBundle/Entity/Session.php
+++ src/Ilios/CoreBundle/Entity/Session.php
@@ -179,7 +179,7 @@ class Session implements SessionInterface
     /**
      * @var IlmSessionFacetInterface
      *
-     * @ORM\ManyToOne(targetEntity="IlmSessionFacet", inversedBy="sessions")
+     * @ORM\OneToOne(targetEntity="IlmSessionFacet", inversedBy="sessions")
      * @ORM\JoinColumns({
      *   @ORM\JoinColumn(name="ilm_session_facet_id", referencedColumnName="ilm_session_facet_id")
      * })
