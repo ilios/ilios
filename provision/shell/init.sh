97defb10660b204c78711a25947c9f1c639da512
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2015-07-08 19:54:08 -0700

    added instructions to write export statements to .bashrc

--- provision/shell/init.sh
+++ provision/shell/init.sh
@@ -6,6 +6,7 @@ locale-gen en_US.UTF-8
 dpkg-reconfigure locales
 
 path="/vagrant/provision/shell"
+"$path/bashrc.sh" #writes to the vagrant user's .bashrc
 "$path/librarian.sh" #Installs librarian
 "$path/local_modules.sh" #For modules that librarian can't install yet
 "$path/puppet_apply.sh" #Tmp fix for https://github.com/mitchellh/vagrant/issues/3740
