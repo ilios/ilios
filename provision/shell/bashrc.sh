00b29736910cfbf7c3bb8e3df8be38e5794ab64d
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2015-07-09 06:57:34 -0700

    changed servername to 'ilios.vagrant' in IDE config.

--- provision/shell/bashrc.sh
+++ provision/shell/bashrc.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-echo "export PHP_IDE_CONFIG='serverName=vagrant'" >> /home/vagrant/.bashrc
+echo "export PHP_IDE_CONFIG='serverName=ilios.vagrant'" >> /home/vagrant/.bashrc
 echo "export XDEBUG_CONFIG='idekey=PHPSTORM'" >> /home/vagrant/.bashrc
