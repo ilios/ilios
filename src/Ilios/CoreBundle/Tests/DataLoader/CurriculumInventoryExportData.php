16c9fa650a8efed5dba170e5b935ca513a6f6e99
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2015-07-09 11:15:54 -0700

    fleshed out curr inv export data loader.

--- src/Ilios/CoreBundle/Tests/DataLoader/CurriculumInventoryExportData.php
+++ src/Ilios/CoreBundle/Tests/DataLoader/CurriculumInventoryExportData.php
@@ -6,17 +6,30 @@ class CurriculumInventoryExportData extends AbstractDataLoader
 {
     protected function getData()
     {
-        $arr = array(
-            'report' => 1
+        $arr = array();
+        $arr[] = array(
+            'report_id' => 1,
+            'program_id' => '1',
+            'year' => $this->faker->date('Y'),
+            'name' => $this->faker->text(100),
+            'description' => $this->faker->text(200),
+            'start_date' => $this->faker->date('Y-m-d'),
+            'end_date' => $this->faker->date('Y-m-d'),
         );
-
 
         return $arr;
     }
 
     public function create()
     {
-        return [];
+        return array(
+            'program_id' => '1',
+            'year' => $this->faker->date('Y'),
+            'name' => $this->faker->text(100),
+            'description' => $this->faker->text(200),
+            'start_date' => $this->faker->date('Y-m-d'),
+            'end_date' => $this->faker->date('Y-m-d')
+        );
     }
 
     public function createInvalid()
