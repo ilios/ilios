ffd5912fa3772a9cedfb837b529b68ea03b4b7ae
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2015-07-09 14:27:41 -0700

    ditched id setter/getter & identifiable interface.

--- src/Ilios/CoreBundle/Entity/CurriculumInventorySequenceInterface.php
+++ src/Ilios/CoreBundle/Entity/CurriculumInventorySequenceInterface.php
@@ -5,13 +5,11 @@ namespace Ilios\CoreBundle\Entity;
 use Doctrine\ORM\Mapping as ORM;
 
 use Ilios\CoreBundle\Traits\DescribableEntityInterface;
-use Ilios\CoreBundle\Traits\IdentifiableEntityInterface;
 
 /**
  * Interface CurriculumInventorySequenceInterface
  */
 interface CurriculumInventorySequenceInterface extends
-    IdentifiableEntityInterface,
     DescribableEntityInterface
 {
     /**
