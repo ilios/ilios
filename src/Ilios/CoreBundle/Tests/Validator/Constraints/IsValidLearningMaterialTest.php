1b9264f5370090b73107dc81f0b88446da292e63
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2015-07-14 16:49:52 -0700

    Issue 897: added skeleton test for LM validator.

---
+++ src/Ilios/CoreBundle/Tests/Validator/Constraints/IsValidLearningMaterialTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Ilios\CoreBundle\Tests\Validator\Constraints;
+
+class IsValidLearningMaterialTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @inheritdoc
+     */
+    public function setUp()
+    {
+        // @todo implement
+    }
+
+    /**
+     *
+     */
+    public function testValidate()
+    {
+        $this->markTestIncomplete(
+            'This test has not been implemented yet.'
+        );
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function tearDown()
+    {
+        // @todo implement
+    }
+}
