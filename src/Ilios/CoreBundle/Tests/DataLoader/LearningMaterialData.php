4e5f5054ab202eda36891bd89093449598352233
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2015-07-15 17:36:20 -0700

    augmented LM data loader with addl. dummy data and methods.

--- src/Ilios/CoreBundle/Tests/DataLoader/LearningMaterialData.php
+++ src/Ilios/CoreBundle/Tests/DataLoader/LearningMaterialData.php
@@ -4,6 +4,9 @@ namespace Ilios\CoreBundle\Tests\DataLoader;
 
 class LearningMaterialData extends AbstractDataLoader
 {
+    /**
+     * {@inheritdoc}
+     */
     protected function getData()
     {
         $arr = array();
@@ -18,7 +21,8 @@ class LearningMaterialData extends AbstractDataLoader
             'owningUser' => "1",
             'sessionLearningMaterials' => [1],
             'courseLearningMaterials' => [1],
-            'citation' => $this->faker->text
+            'citation' => $this->faker->text,
+            'link' => '',
         );
 
         $arr[] = array(
@@ -31,13 +35,16 @@ class LearningMaterialData extends AbstractDataLoader
             'owningUser' => "1",
             'sessionLearningMaterials' => [],
             'courseLearningMaterials' => [2],
-            'citation' => $this->faker->text
+            'citation' => '',
+            'link' => $this->faker->url,
         );
-
 
         return $arr;
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function create()
     {
         return array(
@@ -47,15 +54,108 @@ class LearningMaterialData extends AbstractDataLoader
             'originalAuthor' => $this->faker->name,
             'sessionLearningMaterials' => [],
             'courseLearningMaterials' => [],
-            'citation' => $this->faker->text,
             'userRole' => "2",
             'status' => "1",
             'owningUser' => "1",
+            'citation' => $this->faker->text,
+            'link' => '',
         );
     }
 
+    /**
+     * @return array
+     */
+    public function createCitation() {
+        return array(
+            'id' => 4,
+            'title' => $this->faker->text(30),
+            'description' => $this->faker->text,
+            'originalAuthor' => $this->faker->name,
+            'sessionLearningMaterials' => [],
+            'courseLearningMaterials' => [],
+            'userRole' => "2",
+            'status' => "1",
+            'owningUser' => "1",
+            'citation' => $this->faker->text,
+            'link' => '',
+        );
+    }
+
+    /**
+     * @return array
+     */
+    public function createLink() {
+        return array(
+            'id' => 5,
+            'title' => $this->faker->text(30),
+            'description' => $this->faker->text,
+            'originalAuthor' => $this->faker->name,
+            'sessionLearningMaterials' => [],
+            'courseLearningMaterials' => [],
+            'userRole' => "2",
+            'status' => "1",
+            'owningUser' => "1",
+            'citation' => null,
+            'link' => $this->faker->url,
+        );
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function createFile()
+    {
+        throw new \Exception('Not implemented yet');
+    }
+
+    /**
+     * @return array
+     */
     public function createInvalid()
     {
         return [];
+    }
+
+    /**
+     * @return array
+     */
+    public function createInvalidCitation() {
+        return array(
+            'id' => 6,
+            'title' => $this->faker->text(30),
+            'description' => $this->faker->text,
+            'originalAuthor' => $this->faker->name,
+            'sessionLearningMaterials' => [],
+            'courseLearningMaterials' => [],
+            'userRole' => "2",
+            'status' => "1",
+            'owningUser' => "1",
+            'citation' => $this->faker->text(600), // too long
+            'link' => '',
+        );
+    }
+
+    /**
+     * @return array
+     */
+    public function createInvalidLink() {
+        return array(
+            'id' => 7,
+            'title' => $this->faker->text(30),
+            'description' => $this->faker->text,
+            'originalAuthor' => $this->faker->name,
+            'sessionLearningMaterials' => [],
+            'courseLearningMaterials' => [],
+            'userRole' => "2",
+            'status' => "1",
+            'owningUser' => "1",
+            'citation' => '',
+            'link' => 'this-is-not-an-url',
+        );
+    }
+
+    public function createInvalidFile()
+    {
+        throw new \Exception('Not implemented yet');
     }
 }
