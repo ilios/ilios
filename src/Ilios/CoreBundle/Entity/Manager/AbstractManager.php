bf46a5fc16be080a4c41dfb1f53dc7e5aad9910c
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2015-07-30 17:42:15 -0700

    created abstract entity manager base class.

---
+++ src/Ilios/CoreBundle/Entity/Manager/AbstractManager.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Ilios\CoreBundle\Entity\Manager;
+
+use Doctrine\Bundle\DoctrineBundle\Registry;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * Class AbstractManager
+ * @package Ilios\CoreBundle\Entity\Manager
+ */
+abstract class AbstractManager
+{
+    /**
+     * @var EntityManager
+     */
+    protected $em;
+
+    /**
+     * @var EntityRepository
+     */
+    protected $repository;
+
+    /**
+     * @var string
+     */
+    protected $class;
+
+    /**
+     * @param Registry $em
+     * @param string $class
+     */
+    public function __construct(Registry $em, $class)
+    {
+        $this->em         = $em->getManagerForClass($class);
+        $this->class      = $class;
+        $this->repository = $em->getRepository($class);
+    }
+}
