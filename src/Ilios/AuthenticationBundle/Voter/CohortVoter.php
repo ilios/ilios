2a215e1ac23690fcc6e16c27aa41d9f91f3c1659
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2015-08-03 15:46:12 -0700

    re-implemented cohort voter's isGranted() method.

--- src/Ilios/AuthenticationBundle/Voter/CohortVoter.php
+++ src/Ilios/AuthenticationBundle/Voter/CohortVoter.php
@@ -4,6 +4,7 @@ namespace Ilios\AuthenticationBundle\Voter;
 
 use Ilios\CoreBundle\Entity\CohortInterface;
 use Ilios\CoreBundle\Entity\Manager\ProgramYearStewardManagerInterface;
+use Ilios\CoreBundle\Entity\ProgramYearStewardInterface;
 use Ilios\CoreBundle\Entity\UserInterface;
 use Ilios\CoreBundle\Entity\Manager\PermissionManagerInterface;
 
@@ -56,170 +57,47 @@ class CohortVoter extends AbstractVoter
 
         switch ($attribute) {
             case self::VIEW:
-                return $this->hasViewPermissions($user, $cohort);
+                // the given user is granted VIEW permissions on the given cohort
+                // when at least one of the following statements is true
+                // 1. The user's primary school is the same as the parent program's owning school
+                //    and the user has at least one of 'Course Director', 'Faculty' and 'Developer' role.
+                // 2. The user has READ permissions on the parent program's owning school
+                //    and the user has at least one of 'Course Director', 'Faculty' and 'Developer' role.
+                // 3. The user's primary school matches at least one of the schools owning the associated
+                //    program years' stewarding department
+                //    and the user has at least one of 'Course Director', 'Faculty' and 'Developer' role.
+                // 4. The user has READ permissions on the program.
+                return (
+                    ($this->userHasRole($user, ['Course Director', 'Developer', 'Faculty'])
+                        && ($cohort->getProgramYear()->getProgram()->getOwningSchool()->getId() === $user->getPrimarySchool()->getId()
+                            || $this->permissionManager->userHasReadPermissionToSchool($user, $cohort->getProgramYear()->getProgram()->getOwningSchool()))
+                        || $this->stewardManager->schoolIsStewardingProgramYear($user->getPrimarySchool(), $cohort->getProgramYear()))
+                    || $this->permissionManager->userHasReadPermissionToProgram($user, $cohort->getProgramYear()->getProgram())
+                );
                 break;
             case self::EDIT:
-                return $this->hasEditPermissions($user, $cohort);
-                break;
             case self::DELETE:
-                // can't do that at the moment
-                return false;
+                // the given user is granted EDIT and DELETE permissions on the given cohort
+                // when at least one of the following statements is true
+                // 1. The user's primary school is the same as the parent program's owning school
+                //    and the user has at least one of 'Course Director' and 'Developer' role.
+                // 2. The user has WRITE permissions on the parent program's owning school
+                //    and the user has at least one of 'Course Director' and 'Developer' role.
+                // 3. The user's primary school matches at least one of the schools owning the associated
+                //    program years' stewarding department,
+                //    and the user has at least one of 'Course Director' and 'Developer' role.
+                // 4. The user has WRITE permissions on the parent program.
+                return (
+                    ($this->userHasRole($user, ['Course Director', 'Developer'])
+                        && ($cohort->getProgramYear()->getProgram()->getOwningSchool()->getId() === $user->getPrimarySchool()->getId()
+                            || $this->permissionManager->userHasWritePermissionToSchool($user, $cohort->getProgramYear()->getProgram()->getOwningSchool()))
+                        || $this->stewardManager->schoolIsStewardingProgramYear($user->getPrimarySchool(), $programYear))
+                    || $this->permissionManager->userHasWritePermissionToProgram($user, $programYear->getProgram())
+                );
+                break;
                 break;
         }
 
         return false;
-    }
-
-    /**
-     * @param UserInterface $user
-     * @param CohortInterface $cohort
-     * @return bool
-     */
-    protected function hasViewPermissions(UserInterface $user, CohortInterface $cohort)
-    {
-        //
-        // View permissions to the given cohort by the given user are granted if at least one of the following
-        // statements are true:
-        //
-        // 1. The user's primary school is the same as the cohort's owning school,
-        //    and the user holds at least one of 'Developer', 'Course Director' or 'Faculty' roles.
-        // 2. The user's primary school is the same as an owning school of at least one stewarding department,
-        //    and the user holds at least one of 'Developer', 'Course Director' or 'Faculty' roles.
-        // 3. The user has been granted 'read' rights to the cohort's owning program via the 'permissions' system.
-        //    and the user holds at least one of 'Developer', 'Course Director' or 'Faculty' roles.
-        // 4. The user has been granted 'read' rights to the cohort's owning school via the 'permissions' system.
-        //
-        $cohortOwningSchoolId = $cohort->getProgramYear()->getProgram()->getOwningSchool()->getId();
-        $cohortOwningProgramId = $cohort->getProgramYear()->getProgram()->getId();
-        $userPrimarySchoolId = $user->getPrimarySchool()->getId();
-        $hasRole = $this->userHasRole($user, ['Cohort Director', 'Developer', 'Faculty']);
-        $permissionGranted = false;
-
-        // 1.
-        if ($cohortOwningSchoolId === $userPrimarySchoolId && $hasRole) {
-            $permissionGranted =  true;
-        }
-        if ($permissionGranted) {
-            return true;
-        }
-
-        // 2.
-        $departments = $this->stewardHandler->findProgramYearStewardsBy([
-            'programYear' => $cohort->getProgramYear()->getId(),
-        ]);
-        foreach ($departments as $department) {
-            if ($department->getSchool() === $userPrimarySchoolId && $hasRole) {
-                $permissionGranted = true;
-                break;
-            }
-        }
-        if ($permissionGranted) {
-            return true;
-        }
-
-        // 3.
-        $schoolPermissions = $this->permissionHandler->findPermissionsBy([
-            'user' => $user->getId(),
-            'tableName' => 'school',
-            'canRead' => true,
-        ]);
-        foreach ($schoolPermissions as $permission) {
-            if ($permission->getTableRowId() === $cohortOwningSchoolId && $hasRole) {
-                $permissionGranted = true;
-                break;
-            }
-        }
-        if ($permissionGranted) {
-            return true;
-        }
-
-        // 4.
-        $programPermissions =  $this->permissionHandler->findPermissionsBy([
-            'user' => $user->getId(),
-            'tableName' => 'program',
-            'canRead' => true,
-        ]);
-        foreach ($programPermissions as $permission) {
-            if ($cohortOwningProgramId === $permission->getTableRowId()) {
-                $permissionGranted = true;
-                break;
-            }
-        }
-
-        return $permissionGranted;
-    }
-
-    protected function hasEditPermissions(UserInterface $user, CohortInterface $cohort)
-    {
-        //
-        // View permissions to the given cohort by the given user are granted if at least one of the following
-        // statements are true:
-        //
-        // 1. The user's primary school is the same as the cohort's owning school,
-        //    and the user holds 'Developer' and/or 'Course Director' roles.
-        // 2. The user's primary school is the same as an owning school of at least one stewarding department,
-        //    and the user holds 'Developer' and/or 'Course Director' roles.
-        // 3. The user has been granted 'write' rights to the cohort's owning program via the 'permissions' system.
-        //    and the user holds 'Developer' and/or 'Course Director' roles.
-        // 4. The user has been granted 'write' rights to the cohort's owning school via the 'permissions' system.
-        //
-        $cohortOwningSchoolId = $cohort->getProgramYear()->getProgram()->getOwningSchool()->getId();
-        $cohortOwningProgramId = $cohort->getProgramYear()->getProgram()->getId();
-        $userPrimarySchoolId = $user->getPrimarySchool()->getId();
-        $hasRole = $this->userHasRole($user, ['Cohort Director', 'Developer']);
-        $permissionGranted = false;
-
-        // 1.
-        if ($cohortOwningSchoolId === $userPrimarySchoolId && $hasRole) {
-            $permissionGranted =  true;
-        }
-        if ($permissionGranted) {
-            return true;
-        }
-
-        // 2.
-        $departments = $this->stewardHandler->findProgramYearStewardsBy([
-            'programYear' => $cohort->getProgramYear()->getId(),
-        ]);
-        foreach ($departments as $department) {
-            if ($department->getSchool() === $userPrimarySchoolId && $hasRole) {
-                $permissionGranted = true;
-                break;
-            }
-        }
-        if ($permissionGranted) {
-            return true;
-        }
-
-        // 3.
-        $schoolPermissions = $this->permissionHandler->findPermissionsBy([
-            'user' => $user->getId(),
-            'tableName' => 'school',
-            'canWrite' => true,
-        ]);
-        foreach ($schoolPermissions as $permission) {
-            if ($permission->getTableRowId() === $cohortOwningSchoolId && $hasRole) {
-                $permissionGranted = true;
-                break;
-            }
-        }
-        if ($permissionGranted) {
-            return true;
-        }
-
-        // 4.
-        $programPermissions =  $this->permissionHandler->findPermissionsBy([
-            'user' => $user->getId(),
-            'tableName' => 'program',
-            'canWrite' => true,
-        ]);
-        foreach ($programPermissions as $permission) {
-            if ($cohortOwningProgramId === $permission->getTableRowId()) {
-                $permissionGranted = true;
-                break;
-            }
-        }
-
-        return $permissionGranted;
     }
 }
