ea353291ef1e252738f5c2e7c2ebd0a4b8248443
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2015-08-04 15:19:11 -0700

    added session voter.

---
+++ src/Ilios/AuthenticationBundle/Voter/SessionVoter.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Ilios\AuthenticationBundle\Voter;
+
+use Ilios\CoreBundle\Entity\SessionInterface;
+use Ilios\CoreBundle\Entity\UserInterface;
+
+/**
+ * Class SessionVoter
+ * @package Ilios\AuthenticationBundle\Voter
+ */
+class SessionVoter extends CourseVoter
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function getSupportedClasses()
+    {
+        return array('Ilios\CoreBundle\Entity\SessionInterface');
+    }
+
+    /**
+     * @param string $attribute
+     * @param SessionInterface $session
+     * @param UserInterface|null $user
+     * @return bool
+     */
+    protected function isGranted($attribute, $session, $user = null)
+    {
+        // grant perms based on the owning course
+        return parent::isGranted($attribute, $session->getCourse(), $user);
+    }
+}
