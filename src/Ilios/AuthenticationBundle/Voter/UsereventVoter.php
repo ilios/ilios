0cdf3f18504858f98cfdc87d4f2354bbfb2bbdf6
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2015-08-04 12:01:42 -0700

    added Userevent Voter.

---
+++ src/Ilios/AuthenticationBundle/Voter/UsereventVoter.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Ilios\AuthenticationBundle\Voter;
+
+use Ilios\CoreBundle\Entity\UserInterface;
+
+/**
+ * Class SchoolVoter
+ * @package Ilios\AuthenticationBundle\Voter
+ */
+class UsereventVoter extends AbstractVoter
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function getSupportedClasses()
+    {
+        return array('Ilios\CoreBundle\Entity\UserInterface');
+    }
+
+    /**
+     * @param string $attribute
+     * @param UserInterface $requestedUser
+     * @param UserInterface|null $user
+     * @return bool
+     */
+    protected function isGranted($attribute, $requestedUser, $user = null)
+    {
+        // make sure there is a user object (i.e. that the user is logged in)
+        if (!$user instanceof UserInterface) {
+            return false;
+        }
+
+        switch ($attribute) {
+            case self::VIEW:
+                // check if requested user is current user
+                return $requestedUser->getId() === $user->getId();
+                break;
+            case self::EDIT:
+            case self::DELETE:
+                // user events cannot be edited or deleted.
+                return false;
+                break;
+        }
+
+        return false;
+    }
+}
