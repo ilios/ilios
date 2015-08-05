ebef0b84320ef178871bb99a99d6380b64d7ec3f
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2015-08-05 13:32:13 -0700

    added offering voter.

---
+++ src/Ilios/AuthenticationBundle/Voter/OfferingVoter.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Ilios\AuthenticationBundle\Voter;
+
+use Ilios\CoreBundle\Entity\OfferingInterface;
+use Ilios\CoreBundle\Entity\UserInterface;
+
+/**
+ * Class SessionVoter
+ * @package Ilios\AuthenticationBundle\Voter
+ */
+class OfferingVoter extends SessionVoter
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function getSupportedClasses()
+    {
+        return array('Ilios\CoreBundle\Entity\OfferingInterface');
+    }
+
+    /**
+     * @param string $attribute
+     * @param OfferingInterface $offering
+     * @param UserInterface|null $user
+     * @return bool
+     */
+    protected function isGranted($attribute, $offering, $user = null)
+    {
+        // grant perms based on the owning session
+        return parent::isGranted($attribute, $offering->getSession(), $user);
+    }
+}
