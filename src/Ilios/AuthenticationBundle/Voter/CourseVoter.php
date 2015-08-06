5803604445fdf24ec7c479fbb9b282ff2ba69467
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2015-08-05 17:35:23 -0700

    refactored CRUD grants checks into methods.

--- src/Ilios/AuthenticationBundle/Voter/CourseVoter.php
+++ src/Ilios/AuthenticationBundle/Voter/CourseVoter.php
@@ -45,55 +45,84 @@ class CourseVoter extends AbstractVoter
 
         switch ($attribute) {
             case self::VIEW:
-                // grant VIEW privileges if at least one of the following
-                // statements is true:
-                // 1. the user's primary school is the course's owning school
-                // 2. the user has READ rights on the course's owning school via the permissions system
-                // 3. the user has READ rights on the course via the permissions system
-                return ($course->getOwningSchool()->getId() === $user->getPrimarySchool()->getId()
-                    || $this->permissionManager->userHasReadPermissionToSchool($user, $course->getOwningSchool())
-                    || $this->permissionManager->userHasReadPermissionToCourse($user, $course)
-                );
+                return $this->isViewGranted($course, $user);
                 break;
             case self::CREATE:
-                // grant CREATE privileges if at least one of the following
-                // statements is true:
-                // 1. the user's primary school is the course's owning school
-                //    and the user has at least one of the 'Faculty', 'Course Director' and 'Developer' roles.
-                // 2. the user has WRITE rights on the course's owning school via the permissions system
-                //    and the user has at least one of the 'Faculty', 'Course Director' and 'Developer' roles.
-                // 3. the user has WRITE rights on the course via the permissions system
-                return (
-                    $this->userHasRole($user, ['Faculty', 'Course Director', 'Developer'])
-                    && ($course->getOwningSchool()->getId() === $user->getPrimarySchool()->getId()
-                        || $this->permissionManager->userHasWritePermissionToSchool($user, $course->getOwningSchool())
-                    )
-                    || $this->permissionManager->userHasWritePermissionToCourse($user, $course)
-                );
+                return $this->isCreateGranted($course, $user);
                 break;
             case self::EDIT:
+                return $this->isEditGranted($course, $user);
+                break;
             case self::DELETE:
-                // HALT!
-                // deny EDIT and DELETE privileges to locked or archived courses.
-                if ($course->isArchived() || $course->isLocked()) {
-                    return false;
-                }
-                // grant EDIT and DELETE privileges if at least one of the following
-                // statements is true:
-                // 1. the user's primary school is the course's owning school
-                //    and the user has at least one of the 'Faculty', 'Course Director' and 'Developer' roles.
-                // 2. the user has WRITE rights on the course's owning school via the permissions system
-                //    and the user has at least one of the 'Faculty', 'Course Director' and 'Developer' roles.
-                // 3. the user has WRITE rights on the course via the permissions system
-                return (
-                    $this->userHasRole($user, ['Faculty', 'Course Director', 'Developer'])
-                    && ($course->getOwningSchool()->getId() === $user->getPrimarySchool()->getId()
-                        || $this->permissionManager->userHasWritePermissionToSchool($user, $course->getOwningSchool())
-                    )
-                    || $this->permissionManager->userHasWritePermissionToCourse($user, $course)
-                );
+                return $this->isDeleteGranted($course, $user);
                 break;
         }
         return false;
+    }
+
+    /**
+     * @param CourseInterface $course
+     * @param UserInterface $user
+     * @return bool
+     */
+    protected function isDeleteGranted($course, $user)
+    {
+        return $this->isEditGranted($course, $user);
+    }
+
+    /**
+     * @param CourseInterface $course
+     * @param UserInterface $user
+     * @return bool
+     */
+    protected function isEditGranted($course, $user)
+    {
+        // HALT!
+        // deny EDIT privileges to locked or archived courses.
+        if ($course->isArchived() || $course->isLocked()) {
+            return false;
+        }
+        return $this->isCreateGranted($course, $user);
+    }
+
+    /**
+     * @param CourseInterface $course
+     * @param UserInterface $user
+     * @return bool
+     */
+    protected function isViewGranted($course, $user)
+    {
+        // grant VIEW privileges if at least one of the following
+        // statements is true:
+        // 1. the user's primary school is the course's owning school
+        // 2. the user has READ rights on the course's owning school via the permissions system
+        // 3. the user has READ rights on the course via the permissions system
+        return ($course->getOwningSchool()->getId() === $user->getPrimarySchool()->getId()
+            || $this->permissionManager->userHasReadPermissionToSchool($user, $course->getOwningSchool())
+            || $this->permissionManager->userHasReadPermissionToCourse($user, $course)
+        );
+    }
+
+    /**
+     * @param CourseInterface $course
+     * @param UserInterface $user
+     * @return bool
+     */
+    protected function isCreateGranted($course, $user)
+    {
+        // grant CREATE privileges if at least one of the following
+        // statements is true:
+        // 1. the user's primary school is the course's owning school
+        //    and the user has at least one of the 'Faculty', 'Course Director' and 'Developer' roles.
+        // 2. the user has WRITE rights on the course's owning school via the permissions system
+        //    and the user has at least one of the 'Faculty', 'Course Director' and 'Developer' roles.
+        // 3. the user has WRITE rights on the course via the permissions system
+        return (
+            $this->userHasRole($user, ['Faculty', 'Course Director', 'Developer'])
+            && ($course->getOwningSchool()->getId() === $user->getPrimarySchool()->getId()
+                || $this->permissionManager->userHasWritePermissionToSchool($user, $course->getOwningSchool())
+            )
+            || $this->permissionManager->userHasWritePermissionToCourse($user, $course)
+        );
     }
 }
