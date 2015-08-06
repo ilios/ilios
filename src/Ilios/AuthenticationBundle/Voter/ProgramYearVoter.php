71d92326b982aeaf6079c8ef6392252e15a56d7c
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2015-08-05 21:28:49 -0700

    broke isGranted checks out into separate utility methods.

--- src/Ilios/AuthenticationBundle/Voter/ProgramYearVoter.php
+++ src/Ilios/AuthenticationBundle/Voter/ProgramYearVoter.php
@@ -58,96 +58,113 @@ class ProgramYearVoter extends AbstractVoter
 
         switch ($attribute) {
             case self::VIEW:
-                // the given user is granted VIEW permissions on the given program year
-                // when at least one of the following statements is true
-                // 1. The user's primary school is the same as the parent program's owning school
-                //    and the user has at least one of 'Course Director', 'Faculty' and 'Developer' role.
-                // 2. The user has READ permissions on the parent program's owning school
-                //    and the user has at least one of 'Course Director', 'Faculty' and 'Developer' role.
-                // 3. The user's primary school matches at least one of the schools owning the
-                //    program years' stewarding department
-                //    and the user has at least one of 'Course Director', 'Faculty' and 'Developer' role.
-                // 4. The user has READ permissions on the program.
-                return (
-                    ($this->userHasRole($user, ['Course Director', 'Developer', 'Faculty'])
-                        && ($programYear->getProgram()->getOwningSchool()->getId()
-                            === $user->getPrimarySchool()->getId()
-                            || $this->permissionManager->userHasReadPermissionToSchool(
-                                $user,
-                                $programYear->getProgram()->getOwningSchool()
-                            )
-                            || $this->stewardManager->schoolIsStewardingProgramYear(
-                                $user->getPrimarySchool(),
-                                $programYear
-                            )
-                        )
-                    )
-                    || $this->permissionManager->userHasReadPermissionToProgram($user, $programYear->getProgram())
-                );
+                return $this->isViewGranted($programYear, $user);
                 break;
             case self::CREATE:
-                // the given user is granted CREATE permissions on the given program year
-                // when at least one of the following statements is true
-                // 1. The user's primary school is the same as the parent program's owning school
-                //    and the user has at least one of 'Course Director' and 'Developer' role.
-                // 2. The user has WRITE permissions on the parent program's owning school
-                //    and the user has at least one of 'Course Director' and 'Developer' role.
-                // 3. The user's primary school matches at least one of the schools owning the
-                //    program years' stewarding department,
-                //    and the user has at least one of 'Course Director' and 'Developer' role.
-                // 4. The user has WRITE permissions on the parent program.
-                return (
-                    ($this->userHasRole($user, ['Course Director', 'Developer'])
-                        && ($programYear->getProgram()->getOwningSchool()->getId()
-                            === $user->getPrimarySchool()->getId()
-                            || $this->permissionManager->userHasWritePermissionToSchool(
-                                $user,
-                                $programYear->getProgram()->getOwningSchool()
-                            )
-                            || $this->stewardManager->schoolIsStewardingProgramYear(
-                                $user->getPrimarySchool(),
-                                $programYear
-                            )
-                        )
-                    )
-                    || $this->permissionManager->userHasWritePermissionToProgram($user, $programYear->getProgram())
-                );
+                return $this->isCreateGranted($programYear, $user);
                 break;
             case self::EDIT:
+                return $this->isEditGranted($programYear, $user);
+                break;
             case self::DELETE:
-                // prevent modifications and deletions of locked or archived program years
-                if ($programYear->isLocked() || $programYear->isArchived()) {
-                    return false;
-                }
-                // the given user is granted EDIT or DELETE permissions on the given program year
-                // when at least one of the following statements is true
-                // 1. The user's primary school is the same as the parent program's owning school
-                //    and the user has at least one of 'Course Director' and 'Developer' role.
-                // 2. The user has WRITE permissions on the parent program's owning school
-                //    and the user has at least one of 'Course Director' and 'Developer' role.
-                // 3. The user's primary school matches at least one of the schools owning the
-                //    program years' stewarding department,
-                //    and the user has at least one of 'Course Director' and 'Developer' role.
-                // 4. The user has WRITE permissions on the parent program.
-                return (
-                    ($this->userHasRole($user, ['Course Director', 'Developer'])
-                        && ($programYear->getProgram()->getOwningSchool()->getId()
-                            === $user->getPrimarySchool()->getId()
-                            || $this->permissionManager->userHasWritePermissionToSchool(
-                                $user,
-                                $programYear->getProgram()->getOwningSchool()
-                            )
-                            || $this->stewardManager->schoolIsStewardingProgramYear(
-                                $user->getPrimarySchool(),
-                                $programYear
-                            )
-                        )
-                    )
-                    || $this->permissionManager->userHasWritePermissionToProgram($user, $programYear->getProgram())
-                );
+                return $this->isDeleteGranted($programYear, $user);
                 break;
         }
 
         return false;
+    }
+
+    /**
+     * @param ProgramYearInterface $programYear
+     * @param UserInterface $user
+     * @return bool
+     */
+    protected function isViewGranted($programYear, $user)
+    {
+        // the given user is granted VIEW permissions on the given program year
+        // when at least one of the following statements is true
+        // 1. The user's primary school is the same as the parent program's owning school
+        //    and the user has at least one of 'Course Director', 'Faculty' and 'Developer' role.
+        // 2. The user has READ permissions on the parent program's owning school
+        //    and the user has at least one of 'Course Director', 'Faculty' and 'Developer' role.
+        // 3. The user's primary school matches at least one of the schools owning the
+        //    program years' stewarding department
+        //    and the user has at least one of 'Course Director', 'Faculty' and 'Developer' role.
+        // 4. The user has READ permissions on the program.
+        return (
+            ($this->userHasRole($user, ['Course Director', 'Developer', 'Faculty'])
+                && ($programYear->getProgram()->getOwningSchool()->getId()
+                    === $user->getPrimarySchool()->getId()
+                    || $this->permissionManager->userHasReadPermissionToSchool(
+                        $user,
+                        $programYear->getProgram()->getOwningSchool()
+                    )
+                    || $this->stewardManager->schoolIsStewardingProgramYear(
+                        $user->getPrimarySchool(),
+                        $programYear
+                    )
+                )
+            )
+            || $this->permissionManager->userHasReadPermissionToProgram($user, $programYear->getProgram())
+        );
+    }
+
+    /**
+     * @param ProgramYearInterface $programYear
+     * @param UserInterface $user
+     * @return bool
+     */
+    protected function isEditGranted($programYear, $user)
+    {
+        // prevent modifications and deletions of locked or archived program years
+        if ($programYear->isLocked() || $programYear->isArchived()) {
+            return false;
+        }
+        return $this->isCreateGranted($programYear, $user);
+    }
+
+    /**
+     * @param ProgramYearInterface $programYear
+     * @param UserInterface $user
+     * @return bool
+     */
+    protected function isCreateGranted($programYear, $user)
+    {
+        // the given user is granted CREATE permissions on the given program year
+        // when at least one of the following statements is true
+        // 1. The user's primary school is the same as the parent program's owning school
+        //    and the user has at least one of 'Course Director' and 'Developer' role.
+        // 2. The user has WRITE permissions on the parent program's owning school
+        //    and the user has at least one of 'Course Director' and 'Developer' role.
+        // 3. The user's primary school matches at least one of the schools owning the
+        //    program years' stewarding department,
+        //    and the user has at least one of 'Course Director' and 'Developer' role.
+        // 4. The user has WRITE permissions on the parent program.
+        return (
+            ($this->userHasRole($user, ['Course Director', 'Developer'])
+                && ($programYear->getProgram()->getOwningSchool()->getId()
+                    === $user->getPrimarySchool()->getId()
+                    || $this->permissionManager->userHasWritePermissionToSchool(
+                        $user,
+                        $programYear->getProgram()->getOwningSchool()
+                    )
+                    || $this->stewardManager->schoolIsStewardingProgramYear(
+                        $user->getPrimarySchool(),
+                        $programYear
+                    )
+                )
+            )
+            || $this->permissionManager->userHasWritePermissionToProgram($user, $programYear->getProgram())
+        );
+    }
+
+    /**
+     * @param ProgramYearInterface $programYear
+     * @param UserInterface $user
+     * @return bool
+     */
+    protected function isDeleteGranted($programYear, $user)
+    {
+        return $this->isEditGranted($programYear, $user);
     }
 }
