12e7598e254337752f0d268b8d38209bb6b1010b
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2015-08-05 17:19:54 -0700

    added course learning material voter.

---
+++ src/Ilios/AuthenticationBundle/Voter/CourseLearningMaterialVoter.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Ilios\AuthenticationBundle\Voter;
+
+use Ilios\CoreBundle\Entity\CourseLearningMaterialInterface;
+use Ilios\CoreBundle\Entity\UserInterface;
+
+/**
+ * Class SessionVoter
+ * @package Ilios\AuthenticationBundle\Voter
+ */
+class CourseLearningMaterialVoter extends CourseVoter
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function getSupportedClasses()
+    {
+        return array('Ilios\CoreBundle\Entity\CourseLearningMaterialInterface');
+    }
+
+    /**
+     * @param string $attribute
+     * @param CourseLearningMaterialInterface $material
+     * @param UserInterface|null $user
+     * @return bool
+     */
+    protected function isGranted($attribute, $material, $user = null)
+    {
+        // grant perms based on the owning session
+        return parent::isGranted($attribute, $material->getCourse(), $user);
+    }
+}
