8bcaff814dc82e0baa42d802fc2a630c2de6c295
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2015-08-07 09:51:04 -0700

    added archivable voter.

---
+++ src/Ilios/AuthenticationBundle/Voter/ArchivableVoter.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Ilios\AuthenticationBundle\Voter;
+
+use Ilios\CoreBundle\Entity\UserInterface;
+use Ilios\CoreBundle\Traits\ArchivableEntityInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\AbstractVoter;
+
+/**
+ * Class ArchivableVoter
+ * @package Ilios\AuthenticationBundle\Voter
+ */
+abstract class ArchivableVoter extends AbstractVoter
+{
+    /**
+     * @var string
+     */
+    const MODIFY = 'modify';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getSupportedAttributes()
+    {
+        return array(self::MODIFY);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getSupportedClasses()
+    {
+        return array('Ilios\CoreBundle\Traits\ArchivableEntityInterface');
+    }
+
+    /**
+     * @param string $attribute
+     * @param ArchivableEntityInterface $archivable
+     * @param UserInterface|null $user
+     * @return bool
+     */
+    protected function isGranted($attribute, $archivable, $user = null)
+    {
+        if (self::MODIFY === $attribute) {
+            return ! $archivable->isArchived();
+        }
+        return false;
+    }
+}
