c0526164e2a5c6fd1bd9a701c9e35f850781cafc
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2015-08-09 13:43:40 -0700

    fixed up user event voter.

--- src/Ilios/AuthenticationBundle/Voter/UsereventVoter.php
+++ src/Ilios/AuthenticationBundle/Voter/UsereventVoter.php
@@ -2,7 +2,9 @@
 
 namespace Ilios\AuthenticationBundle\Voter;
 
+use Ilios\CoreBundle\Entity\Manager\UserManagerInterface;
 use Ilios\CoreBundle\Entity\UserInterface;
+use Ilios\CoreBundle\Classes\UserEvent;
 
 /**
  * Class SchoolVoter
@@ -10,6 +12,18 @@ use Ilios\CoreBundle\Entity\UserInterface;
  */
 class UsereventVoter extends AbstractVoter
 {
+    /**
+     * @var UserManagerInterface
+     */
+    protected $userManager;
+
+    /**
+     * @param UserManagerInterface $userManager
+     */
+    public function __construct(UserManagerInterface $userManager)
+    {
+        $this->userManager = $userManager;
+    }
     /**
      * {@inheritdoc}
      */
@@ -23,16 +37,16 @@ class UsereventVoter extends AbstractVoter
      */
     protected function getSupportedClasses()
     {
-        return array('Ilios\CoreBundle\Entity\UserInterface');
+        return array('Ilios\CoreBundle\Classes\UserEvent');
     }
 
     /**
      * @param string $attribute
-     * @param UserInterface $requestedUser
+     * @param UserEvent $event
      * @param UserInterface|null $user
      * @return bool
      */
-    protected function isGranted($attribute, $requestedUser, $user = null)
+    protected function isGranted($attribute, $event, $user = null)
     {
         // make sure there is a user object (i.e. that the user is logged in)
         if (!$user instanceof UserInterface) {
@@ -41,8 +55,12 @@ class UsereventVoter extends AbstractVoter
 
         switch ($attribute) {
             case self::VIEW:
-                // check if requested user is current user
-                return $requestedUser->getId() === $user->getId();
+                // check if the event-owning user is the given user
+                $eventOwningUser = $this->userManager->findUserBy(['user' => $event->user]);
+                return (
+                    empty($eventOwningUser)
+                    && $user->getId() === $eventOwningUser->getId()
+                );
                 break;
         }
 
