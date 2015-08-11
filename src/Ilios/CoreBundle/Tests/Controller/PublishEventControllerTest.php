53b916b107d1e229acb7f826d59c7272b3cef6f0
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2015-08-11 15:45:46 -0700

    removed obsolete tests.

--- src/Ilios/CoreBundle/Tests/Controller/PublishEventControllerTest.php
+++ src/Ilios/CoreBundle/Tests/Controller/PublishEventControllerTest.php
@@ -127,67 +127,6 @@ class PublishEventControllerTest extends AbstractControllerTest
         $this->assertEquals(Codes::HTTP_BAD_REQUEST, $response->getStatusCode());
     }
 
-    public function testPutPublishEvent()
-    {
-        $data = $this->container
-            ->get('ilioscore.dataloader.publishevent')
-            ->getOne();
-
-        $postData = $data;
-        //unset any parameters which should not be POSTed
-        unset($postData['id']);
-
-        $this->createJsonRequest(
-            'PUT',
-            $this->getUrl(
-                'put_publishevents',
-                ['id' => $data['id']]
-            ),
-            json_encode(['publishEvent' => $postData]),
-            $this->getAuthenticatedUserToken()
-        );
-
-        $response = $this->client->getResponse();
-        $this->assertJsonResponse($response, Codes::HTTP_OK);
-        $this->assertEquals(
-            $this->mockSerialize($data),
-            json_decode($response->getContent(), true)['publishEvent']
-        );
-    }
-
-    public function testDeletePublishEvent()
-    {
-        $publishEvent = $this->container
-            ->get('ilioscore.dataloader.publishevent')
-            ->getOne()
-        ;
-
-        $this->createJsonRequest(
-            'DELETE',
-            $this->getUrl(
-                'delete_publishevents',
-                ['id' => $publishEvent['id']]
-            ),
-            null,
-            $this->getAuthenticatedUserToken()
-        );
-
-        $response = $this->client->getResponse();
-        $this->assertEquals(Codes::HTTP_NO_CONTENT, $response->getStatusCode());
-        $this->createJsonRequest(
-            'GET',
-            $this->getUrl(
-                'get_publishevents',
-                ['id' => $publishEvent['id']]
-            ),
-            null,
-            $this->getAuthenticatedUserToken()
-        );
-
-        $response = $this->client->getResponse();
-        $this->assertEquals(Codes::HTTP_NOT_FOUND, $response->getStatusCode());
-    }
-
     public function testPublishEventNotFound()
     {
         $this->createJsonRequest(
