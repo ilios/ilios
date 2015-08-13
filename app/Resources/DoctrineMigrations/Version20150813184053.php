baf29a071a2d975e6001d2dfac7b9758ece705bc
Stefan Topfstedt <stefan.topfstedt@ucsf.edu>
2015-08-13 13:31:59 -0700

    removed auto-generated comments.

--- app/Resources/DoctrineMigrations/Version20150813184053.php
+++ app/Resources/DoctrineMigrations/Version20150813184053.php
@@ -15,7 +15,6 @@ class Version20150813184053 extends AbstractMigration
      */
     public function up(Schema $schema)
     {
-        // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('DROP TABLE instruction_hours');
@@ -28,7 +27,6 @@ class Version20150813184053 extends AbstractMigration
      */
     public function down(Schema $schema)
     {
-        // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('CREATE TABLE recurring_event (recurring_event_id INT AUTO_INCREMENT NOT NULL, next_recurring_event_id INT DEFAULT NULL, previous_recurring_event_id INT DEFAULT NULL, on_sunday TINYINT(1) NOT NULL, on_monday TINYINT(1) NOT NULL, on_tuesday TINYINT(1) NOT NULL, on_wednesday TINYINT(1) NOT NULL, on_thursday TINYINT(1) NOT NULL, on_friday TINYINT(1) NOT NULL, on_saturday TINYINT(1) NOT NULL, end_date DATETIME NOT NULL, repetition_count SMALLINT DEFAULT NULL, INDEX IDX_51B1C7F8B494B099 (previous_recurring_event_id), INDEX IDX_51B1C7F882312E9D (next_recurring_event_id), PRIMARY KEY(recurring_event_id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
