f27e97315657fc6ecd286ed7298bb79a2b060afd
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2015-08-19 18:51:22 -0700

    Issue 966: added doctrine migration to remote api_key table.

---
+++ app/Resources/DoctrineMigrations/Version20150820013804.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Ilios\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Removes the api_key table.
+ *
+ * @link https://github.com/ilios/ilios/issues/966
+ */
+class Version20150820013804 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE api_key');
+
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        $this->abortIf(
+            $this->connection->getDatabasePlatform()->getName() != 'mysql',
+            'Migration can only be executed safely on \'mysql\'.'
+        );
+
+        $this->addSql(
+            'CREATE TABLE api_key (user_id INT NOT NULL, api_key VARCHAR(64) NOT NULL COLLATE utf8_unicode_ci, UNIQUE INDEX UNIQ_C912ED9DA76ED395 (user_id), UNIQUE INDEX api_key_api_key (api_key), PRIMARY KEY(user_id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB'
+        );
+    }
+}
