42f904ee706733bbbf95edbefe3103fbdf6819f7
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2015-08-24 16:03:34 -0700

    Issue 956: added deletable entity interface and trait.

---
+++ src/Ilios/CoreBundle/Traits/DeletableEntity.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Ilios\CoreBundle\Traits;
+
+
+trait DeletableEntity
+{
+
+    /**
+     * @param boolean $deleted
+     */
+    public function setDeleted($deleted)
+    {
+        $this->deleted = $deleted;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function isDeleted()
+    {
+        return $this->deleted;
+    }
+}
