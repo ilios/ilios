1a3eda0d273ee796c2062188f239dfe3bccc60a1
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2015-08-27 13:21:58 -0700

    baleted obsolete files.

--- app/Resources/install/dataimport/assessment_option_data.sql
+++
@@ -1,5 +0,0 @@
---
--- insert the options for assessment methods
---
-INSERT INTO `assessment_option` (`assessment_option_id`, `name`) VALUES (1, 'summative');
-INSERT INTO `assessment_option` (`assessment_option_id`, `name`) VALUES (2, 'formative');
--- app/Resources/install/dataimport/set_migration_counter.sql
+++
@@ -1,4 +0,0 @@
---
--- Sets the current migration version counter on db install.
---
-INSERT INTO `migrations` VALUES (31);
