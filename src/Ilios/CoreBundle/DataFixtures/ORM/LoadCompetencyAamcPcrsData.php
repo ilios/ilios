c35f46fb9f4a928d074e7fbb81818802b3953154
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2015-08-27 09:46:57 -0700

    Issue 892: load competency/aamc-pcrs relationships.

---
+++ src/Ilios/CoreBundle/DataFixtures/ORM/LoadCompetencyAamcPcrsData.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Ilios\CoreBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+
+use Ilios\CoreBundle\Entity\Competency;
+
+/**
+ * Class LoadCompetencyAamcPcrsData
+ * @package Ilios\CoreBundle\DataFixtures\ORM
+ */
+class LoadCompetencyAamcPcrsData extends AbstractFixture implements DependentFixtureInterface
+
+{
+    public function __construct()
+    {
+        parent::__construct('competency_x_aamc_pcrs', false);
+    }
+    /**
+     * {@inheritdoc}
+     */
+    protected function createEntity(array $data)
+    {
+        /**
+         * @var Competency $entity
+         */
+        $entity = $this->getReference('competency' . $data[0]);
+        $entity->addAamcPcrs($this->getReference('aamc_pcrs' . $data[1]));
+        return $entity;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    function getDependencies()
+    {
+        return [
+            'Ilios\CoreBundle\DataFixtures\ORM\LoadAamcPcrsData',
+            'Ilios\CoreBundle\DataFixtures\ORM\LoadCompetencyData',
+        ];
+    }
+}
