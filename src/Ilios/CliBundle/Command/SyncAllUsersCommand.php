01fea667f72f2ab6a034bc75892a818e477db0ad
Jonathan Johnson <jon.johnson@ucsf.edu>
2015-09-13 23:53:10 -0700

    Chunk and flush inserts in groups of 500

--- src/Ilios/CliBundle/Command/SyncAllUsersCommand.php
+++ src/Ilios/CliBundle/Command/SyncAllUsersCommand.php
@@ -74,86 +74,87 @@ class SyncAllUsersCommand extends Command
     {
         $this->userManager->resetExaminedFlagForAllUsers();
         $campusIds = $this->userManager->getAllCampusIds(false, false);
-        $userRecords = $this->directory->findByCampusIds($campusIds);
+        $allUserRecoreds = $this->directory->findByCampusIds($campusIds);
         
-        if (!$userRecords) {
+        if (!$allUserRecoreds) {
             $output->writeln('<error>Unable to find any users in the directory');
             return;
         }
-        $totalRecords = count($userRecords);
+        $totalRecords = count($allUserRecoreds);
         $updated = 0;
-        foreach ($userRecords as $recordArray) {
-            $user = $this->userManager->findUserBy([
-                'campusId' => $recordArray['campusId'],
-                'enabled' => true,
-                'userSyncIgnore' => false
-            ]);
-            if (!$user) {
-                //this shouldn't happen unless the user gets updated between
-                //listing all the IDs on line 68 and getting them back from
-                //the directory
+        $chunks = array_chunk($allUserRecoreds, 500);
+        foreach ($chunks as $userRecords) {
+            foreach ($userRecords as $recordArray) {
+                $user = $this->userManager->findUserBy([
+                    'campusId' => $recordArray['campusId'],
+                    'enabled' => true,
+                    'userSyncIgnore' => false
+                ]);
+                if (!$user) {
+                    //this shouldn't happen unless the user gets updated between
+                    //listing all the IDs on line 68 and getting them back from
+                    //the directory
+                    $output->writeln(
+                        '<error>Unable to find an active sync user with ' .
+                        'campus id ' . $recordArray['campusId'] . '</error>'
+                    );
+                    continue;
+                }
+                $update = false;
                 $output->writeln(
-                    '<error>Unable to find an active sync user with ' .
-                    'campus id ' . $recordArray['campusId'] . '</error>'
+                    '<info>Comparing User #' . $user->getId() . ' ' .
+                    $user->getFirstAndLastName() . ' to directory user by campus id ' .
+                    $user->getCampusId() . '</info>'
                 );
-                continue;
+                if ($user->getFirstName() != $recordArray['firstName']) {
+                    $update = true;
+                    $user->setFirstName($recordArray['firstName']);
+                    $output->writeln(
+                        '<info>Updating first name from "' . $user->getFirstName() .
+                        '" to "' . $recordArray['firstName'] . '"</info>'
+                    );
+                }
+                if ($user->getLastName() != $recordArray['lastName']) {
+                    $update = true;
+                    $user->setLastName($recordArray['lastName']);
+                    $output->writeln(
+                        '<info>Updating last name from "' . $user->getLastName() .
+                        '" to "' . $recordArray['lastName'] . '"</info>'
+                    );
+                }
+                if ($user->getPhone() != $recordArray['telephoneNumber']) {
+                    $update = true;
+                    $user->setPhone($recordArray['telephoneNumber']);
+                    $output->writeln(
+                        '<info>Updating phone number from "' . $user->getPhone() .
+                        '" to "' . $recordArray['telephoneNumber'] . '"</info>'
+                    );
+                }
+                
+                $authentication = $user->getAuthentication();
+                if (!$authentication) {
+                    $authentication = $this->authenticationManager->createAuthentication();
+                    $authentication->setUser($user);
+                }
+                if ($authentication->getUsername() != $recordArray['username']) {
+                    $update = true;
+                    $authentication->setUsername($recordArray['username']);
+                    $output->writeln(
+                        '<info>Updating username from "' . $authentication->getUsername() .
+                        '" to "' . $recordArray['username'] . '"</info>'
+                    );
+                    $this->authenticationManager->updateAuthentication($authentication, false);
+                }
+                
+                if ($update) {
+                    $updated++;
+                }
+                $user->setExamined(true);
+                $this->userManager->updateUser($user, false);
             }
-            $update = false;
-            $output->writeln(
-                '<info>Comparing User #' . $user->getId() . ' ' .
-                $user->getFirstAndLastName() . ' to directory user by campus id ' .
-                $user->getCampusId() . '</info>'
-            );
-            if ($user->getFirstName() != $recordArray['firstName']) {
-                $update = true;
-                $user->setFirstName($recordArray['firstName']);
-                $output->writeln(
-                    '<info>Updating first name from "' . $user->getFirstName() .
-                    '" to "' . $recordArray['firstName'] . '"</info>'
-                );
-            }
-            if ($user->getLastName() != $recordArray['lastName']) {
-                $update = true;
-                $user->setLastName($recordArray['lastName']);
-                $output->writeln(
-                    '<info>Updating last name from "' . $user->getLastName() .
-                    '" to "' . $recordArray['lastName'] . '"</info>'
-                );
-            }
-            if ($user->getPhone() != $recordArray['telephoneNumber']) {
-                $update = true;
-                $user->setPhone($recordArray['telephoneNumber']);
-                $output->writeln(
-                    '<info>Updating phone number from "' . $user->getPhone() .
-                    '" to "' . $recordArray['telephoneNumber'] . '"</info>'
-                );
-            }
-            
-            $authentication = $user->getAuthentication();
-            if (!$authentication) {
-                $authentication = $this->authenticationManager->createAuthentication();
-                $authentication->setUser($user);
-            }
-            if ($authentication->getUsername() != $recordArray['username']) {
-                $update = true;
-                $authentication->setUsername($recordArray['username']);
-                $output->writeln(
-                    '<info>Updating username from "' . $authentication->getUsername() .
-                    '" to "' . $recordArray['username'] . '"</info>'
-                );
-                $this->authenticationManager->updateAuthentication($authentication, false);
-            }
-            
-            if ($update) {
-                $updated++;
-            }
-            $user->setExamined(true);
-            $this->userManager->updateUser($user, false);
-            
             $this->em->flush();
             $this->em->clear();
         }
-        
         $output->writeln(
             "<info>Completed Sync Process {$totalRecords} users found in the directory; " .
             "{$updated} users updated.</info>"
