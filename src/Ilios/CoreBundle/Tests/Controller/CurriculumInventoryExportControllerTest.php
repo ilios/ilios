c54e6b12d3cbc6939977bcb9a68a6564c3013f48
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2015-09-18 17:21:36 -0700

    provide test coverage for post action on ci export controller.

--- src/Ilios/CoreBundle/Tests/Controller/CurriculumInventoryExportControllerTest.php
+++ src/Ilios/CoreBundle/Tests/Controller/CurriculumInventoryExportControllerTest.php
@@ -17,9 +17,9 @@ class CurriculumInventoryExportControllerTest extends AbstractControllerTest
     {
         $fixtures = parent::getFixtures();
         return array_merge($fixtures, [
-            'Ilios\CoreBundle\Tests\Fixture\LoadCurriculumInventoryExportData',
+            'Ilios\CoreBundle\Tests\Fixture\LoadUserData',
             'Ilios\CoreBundle\Tests\Fixture\LoadCurriculumInventoryReportData',
-            'Ilios\CoreBundle\Tests\Fixture\LoadUserData'
+            'Ilios\CoreBundle\Tests\Fixture\LoadCurriculumInventoryExportData',
         ]);
     }
 
@@ -31,5 +31,25 @@ class CurriculumInventoryExportControllerTest extends AbstractControllerTest
         return [
             'createdAt'
         ];
+    }
+
+    /**
+     * @covers CurriculumInventoryExportController::postAction
+     */
+    public function testPostCurriculumInventoryExport()
+    {
+        $data = $this->container->get('ilioscore.dataloader.curriculuminventoryexport')->create();
+        $postData = $data;
+
+        $this->createJsonRequest(
+            'POST',
+            $this->getUrl('post_curriculuminventoryexports'),
+            json_encode(['curriculumInventoryExport' => $postData]),
+            $this->getAuthenticatedUserToken()
+        );
+
+        $response = $this->client->getResponse();
+
+        $this->assertEquals(Codes::HTTP_CREATED, $response->getStatusCode(), $response->getContent());
     }
 }
