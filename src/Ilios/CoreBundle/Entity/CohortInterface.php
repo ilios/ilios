980c56d9fc6d0a8c00a00ad57310596ce136aff1
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2015-09-22 10:07:12 -0700

    declared getters/setters in entity interfaces.

--- src/Ilios/CoreBundle/Entity/CohortInterface.php
+++ src/Ilios/CoreBundle/Entity/CohortInterface.php
@@ -30,4 +30,40 @@ interface CohortInterface extends
      * @return ProgramYearInterface
      */
     public function getProgramYear();
+
+    /**
+     * Check if a cohorts program year is deleted
+     * @return boolean
+     */
+    public function isDeleted();
+
+    /**
+    * @return LearnerGroupInterface[]|ArrayCollection
+    */
+    public function getLearnerGroups();
+
+    /**
+    * @param Collection $learnerGroups
+    */
+    public function setLearnerGroups(Collection $learnerGroups);
+
+    /**
+    * @param LearnerGroupInterface $learnerGroup
+    */
+    public function addLearnerGroup(LearnerGroupInterface $learnerGroup);
+
+    /**
+     * @param Collection $users
+     */
+    public function setUsers(Collection $users = null);
+
+    /**
+     * @param UserInterface $user
+     */
+    public function addUser(UserInterface $user);
+
+    /**
+     * @return ArrayCollection|UserInterface[]
+     */
+    public function getUsers();
 }
