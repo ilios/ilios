3860128ec674074e34a2f696126e2f4186fc2bc2
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2015-10-08 09:46:24 -0700

    added getAllLearners() method to learner group entity.

--- src/Ilios/CoreBundle/Entity/LearnerGroup.php
+++ src/Ilios/CoreBundle/Entity/LearnerGroup.php
@@ -404,4 +404,16 @@ class LearnerGroup implements LearnerGroupInterface
     {
         return $this->instructors;
     }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAllLearners()
+    {
+        $learners = $this->getUsers()->toArray();
+        foreach ($this->getChildren() as $group) {
+            $learners = array_merge($learners, $group->getAllLearners()->toArray());
+        }
+        return new ArrayCollection($learners);
+    }
 }
