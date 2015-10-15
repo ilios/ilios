7d9eb5504cc6866e4295d349c16d1eacbda026f9
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2015-10-15 16:34:52 -0700

    Added transformer for stripping  tags.

---
+++ src/Ilios/CoreBundle/Form/DataTransformer/RemoveMarkupTransformer.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Ilios\CoreBundle\Form\DataTransformer;
+
+use Symfony\Component\Form\DataTransformerInterface;
+
+/**
+ * Data sanitation transformer.
+ *
+ * Class RemoveMarkupTransformer
+ * @package Ilios\CoreBundle\Form\DataTransformer
+ */
+class RemoveMarkupTransformer implements DataTransformerInterface
+{
+    /**
+     * Strips all tags from a given string and returns it.
+     * @param mixed $value
+     * @return mixed
+     */
+    public function transform($value)
+    {
+        if (! is_string($value)) {
+            return $value;
+        }
+        return strip_tags($value);
+    }
+
+    /**
+     * Does nothing, it just returns the given value.
+     *
+     * @param string $value
+     * @return string
+     */
+    public function reverseTransform($value)
+    {
+        return $value;
+    }
+}
