42d290b41f59ec5cc018763361f665f87f9da499
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2015-11-23 16:36:54 -0800

    implemented session type repository.

---
+++ src/Ilios/CoreBundle/Entity/Repository/SessionTypeRepository.php
@@ -0,0 +1,61 @@
+<?php
+namespace Ilios\CoreBundle\Entity\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+class SessionTypeRepository extends EntityRepository
+{
+    /**
+     * Custom findBy so we can filter by related entities
+     *
+     * @param array $criteria
+     * @param array|null $orderBy
+     * @param null $limit
+     * @param null $offset
+     *
+     * @return array
+     */
+    public function findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+    {
+        $qb = $this->_em->createQueryBuilder();
+
+
+        $qb->select('DISTINCT s')->from('IliosCoreBundle:SessionType', 's');
+
+        if (empty($orderBy)) {
+            $orderBy = ['id' => 'ASC'];
+        }
+
+        if (is_array($orderBy)) {
+            foreach ($orderBy as $sort => $order) {
+                $qb->addOrderBy('s.'.$sort, $order);
+            }
+        }
+
+        if (array_key_exists('sessions', $criteria)) {
+            $ids = is_array($criteria['sessions']) ? $criteria['sessions'] : [$criteria['sessions']];
+            $qb->join('s.sessions', 'session');
+            $qb->andWhere($qb->expr()->in('session.id', ':sessions'));
+            $qb->setParameter(':sessions', $ids);
+        }
+
+        unset($criteria['sessions']);
+
+        if (count($criteria)) {
+            foreach ($criteria as $key => $value) {
+                $values = is_array($value) ? $value : [$value];
+                $qb->andWhere($qb->expr()->in("s.{$key}", ":{$key}"));
+                $qb->setParameter(":{$key}", $values);
+            }
+        }
+        if ($offset) {
+            $qb->setFirstResult($offset);
+        }
+
+        if ($limit) {
+            $qb->setMaxResults($limit);
+        }
+
+        return $qb->getQuery()->getResult();
+    }
+}
