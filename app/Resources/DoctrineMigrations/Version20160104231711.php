b30c88fab976c27e8acd91ba35b095288d200695
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2016-01-04 15:19:56 -0800

    added migration to convert 'due date' column.

---
+++ app/Resources/DoctrineMigrations/Version20160104231711.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Ilios\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Converts the ILM Session's "due date" column from "date" to "datetime".
+ */
+class Version20160104231711 extends AbstractMigration
+{
+    /**
+     * @inheritdoc
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE ilm_session_facet CHANGE due_date due_date DATETIME NOT NULL');
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE ilm_session_facet CHANGE due_date due_date DATE NOT NULL');
+    }
+}
