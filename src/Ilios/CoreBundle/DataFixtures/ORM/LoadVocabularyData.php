29dab60040f8ce1f78c75a70c2c4b038ad9116fb
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2016-01-25 17:34:23 -0800

    added data loaders for terms and vocabs.

---
+++ src/Ilios/CoreBundle/DataFixtures/ORM/LoadVocabularyData.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Ilios\CoreBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+
+use Ilios\CoreBundle\Entity\Vocabulary;
+use Ilios\CoreBundle\Entity\VocabularyInterface;
+
+/**
+ * Class LoadVocabularyData
+ * @package Ilios\CoreBundle\DataFixtures\ORM
+ */
+class LoadVocabularyData extends AbstractFixture implements DependentFixtureInterface
+{
+    public function __construct()
+    {
+        parent::__construct('vocabulary');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getDependencies()
+    {
+        return [
+            'Ilios\CoreBundle\DataFixtures\ORM\LoadSchoolData',
+        ];
+    }
+
+    /**
+     * @return VocabularyInterface
+     *
+     * @see AbstractFixture::createEntity()
+     */
+    protected function createEntity()
+    {
+        return new Vocabulary();
+    }
+
+    /**
+     * @param VocabularyInterface $entity
+     * @param array $data
+     * @return VocabularyInterface
+     *
+     * @see AbstractFixture::populateEntity()
+     */
+    protected function populateEntity($entity, array $data)
+    {
+        // `vocabulary_id`,`title`,`hierarchical`,`school_id`
+        $entity->setId($data[0]);
+        $entity->setTitle($data[1]);
+        $entity->setHierarchical((boolean) $data[2]);
+        $entity->setSchool($this->getReference('school' . $data[3]));
+        return $entity;
+    }
+}
