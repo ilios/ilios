4a33ed1186ffcb1bbeb04ae26ae3d317f7f0f1d1
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2016-01-25 17:03:38 -0800

    Added term and vocab voters.

---
+++ src/Ilios/AuthenticationBundle/Voter/VocabularyVoter.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Ilios\AuthenticationBundle\Voter;
+
+use Ilios\CoreBundle\Entity\VocabularyInterface;
+use Ilios\CoreBundle\Entity\Manager\PermissionManagerInterface;
+use Ilios\CoreBundle\Entity\UserInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+
+/**
+ * Class VocabularyVoter
+ * @package Ilios\AuthenticationBundle\Voter
+ */
+class VocabularyVoter extends AbstractVoter
+{
+    /**
+     * @var PermissionManagerInterface
+     */
+    protected $permissionManager;
+
+    /**
+     * @param PermissionManagerInterface $permissionManager
+     */
+    public function __construct(PermissionManagerInterface $permissionManager)
+    {
+        $this->permissionManager = $permissionManager;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function supports($attribute, $subject)
+    {
+        return $subject instanceof VocabularyInterface && in_array($attribute, array(
+            self::CREATE, self::VIEW, self::EDIT, self::DELETE
+        ));
+    }
+
+    /**
+     * @param string $attribute
+     * @param VocabularyInterface $topic
+     * @param TokenInterface $token
+     * @return bool
+     * @todo Review implemented rules. [ST 2016/01/25]
+     */
+    protected function voteOnAttribute($attribute, $topic, TokenInterface $token)
+    {
+        $user = $token->getUser();
+        if (!$user instanceof UserInterface) {
+            return false;
+        }
+
+        switch ($attribute) {
+            // grant VIEW privileges to all authenticated users.
+            case self::VIEW:
+                return true;
+                break;
+            case self::CREATE:
+            case self::EDIT:
+            case self::DELETE:
+                // grant CREATE, EDIT and DELETE privileges
+                // if the user has the 'Developer' role
+                return $this->userHasRole($user, ['Developer']);
+                break;
+        }
+
+        return false;
+    }
+}
