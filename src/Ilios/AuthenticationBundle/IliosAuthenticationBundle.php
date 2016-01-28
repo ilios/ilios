e040c3fb213afd7b73455962b78508c621dca55d
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2016-01-27 17:23:09 -0800

    added docblock to class.

--- src/Ilios/AuthenticationBundle/IliosAuthenticationBundle.php
+++ src/Ilios/AuthenticationBundle/IliosAuthenticationBundle.php
@@ -5,7 +5,10 @@ namespace Ilios\AuthenticationBundle;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
-
+/**
+ * Class IliosAuthenticationBundle
+ * @package Ilios\AuthenticationBundle
+ */
 class IliosAuthenticationBundle extends Bundle
 {
     public function build(ContainerBuilder $container)
