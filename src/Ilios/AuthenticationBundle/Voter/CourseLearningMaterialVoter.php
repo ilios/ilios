66f95c92fc303eb7349bce9f00de2628d387213c
Jonathan Johnson <jon.johnson@ucsf.edu>
2016-02-10 00:00:26 -0800

    Fix Voters which inherit from CourseVoter
    Needed touchup after the split of course entity and dto voters.

--- src/Ilios/AuthenticationBundle/Voter/CourseLearningMaterialVoter.php
+++ src/Ilios/AuthenticationBundle/Voter/CourseLearningMaterialVoter.php
@@ -2,8 +2,8 @@
 
 namespace Ilios\AuthenticationBundle\Voter;
 
+use Ilios\CoreBundle\Entity\UserInterface;
 use Ilios\CoreBundle\Entity\CourseLearningMaterialInterface;
-use Ilios\CoreBundle\Entity\CourseInterface;
 use Ilios\CoreBundle\Entity\LearningMaterialStatusInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 
@@ -31,31 +31,38 @@ class CourseLearningMaterialVoter extends CourseVoter
      */
     protected function voteOnAttribute($attribute, $material, TokenInterface $token)
     {
+        $user = $token->getUser();
+        if (!$user instanceof UserInterface) {
+            return false;
+        }
+
         $course = $material->getCourse();
         if (! $course) {
             return false;
         }
-        // grant perms based on the owning course
-        $granted = parent::voteOnAttribute($attribute, $course, $token);
 
-        // prevent access if associated LM is in draft, and the current user has no elevated privileges.
-        if ($granted && self::VIEW === $attribute) {
-            $granted = $this->userHasRole($token->getUser(), ['Faculty', 'Course Director', 'Developer'])
-                || LearningMaterialStatusInterface::IN_DRAFT !== $material->getLearningMaterial()->getStatus()->getId();
+        switch ($attribute) {
+            case self::VIEW:
+                $granted =  $this->isViewGranted($course->getId(), $course->getSchool()->getId(), $user);
+                // prevent access if associated LM is in draft, and the current user has no elevated privileges.
+                if ($granted) {
+                    $granted = $this->userHasRole($token->getUser(), ['Faculty', 'Course Director', 'Developer'])
+                    || LearningMaterialStatusInterface::IN_DRAFT !== $material->getLearningMaterial()
+                            ->getStatus()->getId();
+                }
+
+                return $granted;
+                break;
+            case self::CREATE:
+            case self::EDIT:
+            case self::DELETE:
+                // prevent any sort of write operation (create/edit/delete) if the parent course is locked or archived.
+                if ($course->isLocked() || $course->isArchived()) {
+                    return false;
+                }
+                return $this->isWriteGranted($course->getId(), $course->getSchool()->getId(), $user);
+                break;
         }
-
-        return $granted;
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    protected function isWriteGranted(CourseInterface $course, $user)
-    {
-        // prevent any sort of write operation (create/edit/delete) if the parent course is locked or archived.
-        if ($course->isLocked() || $course->isArchived()) {
-            return false;
-        }
-        return parent::isWriteGranted($course, $user);
+        return false;
     }
 }
--- src/Ilios/AuthenticationBundle/Voter/SessionVoter.php
+++ src/Ilios/AuthenticationBundle/Voter/SessionVoter.php
@@ -3,7 +3,7 @@
 namespace Ilios\AuthenticationBundle\Voter;
 
 use Ilios\CoreBundle\Entity\SessionInterface;
-use Ilios\CoreBundle\Entity\CourseInterface;
+use Ilios\CoreBundle\Entity\UserInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 
 /**
@@ -30,23 +30,30 @@ class SessionVoter extends CourseVoter
      */
     protected function voteOnAttribute($attribute, $session, TokenInterface $token)
     {
+        $user = $token->getUser();
+        if (!$user instanceof UserInterface) {
+            return false;
+        }
+
         $course = $session->getCourse();
         if (! $course) {
             return false;
         }
-        // grant perms based on the owning course
-        return parent::voteOnAttribute($attribute, $course, $token);
-    }
 
-    /**
-     * {@inheritdoc}
-     */
-    protected function isWriteGranted(CourseInterface $course, $user)
-    {
-        // prevent any sort of write operation (create/edit/delete) if the parent course is locked or archived.
-        if ($course->isLocked() || $course->isArchived()) {
-            return false;
+        switch ($attribute) {
+            case self::VIEW:
+                return $this->isViewGranted($course->getId(), $course->getSchool()->getId(), $user);
+                break;
+            case self::CREATE:
+            case self::EDIT:
+            case self::DELETE:
+                // prevent any sort of write operation (create/edit/delete) if the parent course is locked or archived.
+                if ($course->isLocked() || $course->isArchived()) {
+                    return false;
+                }
+                return $this->isWriteGranted($course->getId(), $course->getSchool()->getId(), $user);
+                break;
         }
-        return parent::isWriteGranted($course, $user);
+        return false;
     }
 }
