e608a825971027aab5d0e9f19bd4943f96bed586
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2016-02-10 16:23:15 -0800

    added session dto.

---
+++ src/Ilios/CoreBundle/Entity/DTO/SessionDTO.php
@@ -0,0 +1,169 @@
+<?php
+
+namespace Ilios\CoreBundle\Entity\DTO;
+
+use JMS\Serializer\Annotation as JMS;
+
+/**
+ * Class SessionDTO
+ * Data transfer object for a session.
+ * @package Ilios\CoreBundle\Entity\DTO
+
+ */
+class SessionDTO
+{
+    /**
+     * @var int
+     * @JMS\Type("integer")
+     */
+    public $id;
+
+    /**
+     * @var string
+     * @JMS\Type("string")
+     */
+    public $title;
+
+    /**
+     * @var boolean
+     * @JMS\Type("boolean")
+     * @JMS\SerializedName("attireRequired")
+     */
+    public $attireRequired;
+
+    /**
+     * @var boolean
+     * @JMS\Type("boolean")
+     * @JMS\SerializedName("equipmentRequired")
+     */
+    public $equipmentRequired;
+
+    /**
+     * @var boolean
+     * @JMS\Type("boolean")
+     */
+    public $supplemental;
+
+    /**
+     * @var boolean
+     * @JMS\Type("boolean")
+     * @JMS\SerializedName("publishedAsTbd")
+     */
+    public $publishedAsTbd;
+
+    /**
+     * @var boolean
+     * @JMS\Type("boolean")
+     */
+    public $published;
+
+    /**
+     * @var \DateTime
+     * @JMS\Type("DateTime<'c'>")
+     * @JMS\SerializedName("updatedAt")
+     */
+    public $updatedAt;
+
+    /**
+     * @var int
+     * @JMS\Type("string")
+     * @JMS\SerializedName("sessionType")
+     */
+    public $sessionType;
+
+    /**
+     * @var int
+     * @JMS\Type("string")
+     */
+    public $course;
+
+    /**
+     * @var int
+     * @JMS\Type("string")
+     * @JMS\SerializedName("ilmSession")
+     */
+    public $ilmSession;
+
+    /**
+     * @deprecated
+     * @var int[]
+     * @JMS\Type("array<string>")
+     */
+    public $topics;
+
+    /**
+     * @var int[]
+     * @JMS\Expose
+     * @JMS\Type("array<string>")
+     */
+    public $terms;
+
+    /**
+     * @var int[]
+     * @JMS\Expose
+     * @JMS\Type("array<string>")
+     */
+    public $objectives;
+
+    /**
+     * @var int[]
+     * @JMS\Type("array<string>")
+     * @JMS\SerializedName("meshDescriptors")
+     */
+    public $meshDescriptors;
+
+    /**
+     * @var int
+     * @JMS\Type("string")
+     * @JMS\SerializedName("sessionDescription")
+     */
+    public $sessionDescription;
+
+    /**
+     * @var int[]
+     * @JMS\Type("array<string>")
+     * @JMS\SerializedName("learningMaterials")
+     */
+    public $learningMaterials;
+
+    /**
+     * @var int[]
+     * @JMS\Type("array<string>")
+     */
+    protected $offerings;
+
+    public function __construct(
+        $id,
+        $title,
+        $attireRequired,
+        $equipmentRequired,
+        $supplemental,
+        $publishedAsTbd,
+        $published,
+        $updatedAt,
+        $sessionType,
+        $sessionDescription,
+        $course,
+        $ilmSession
+    ) {
+        $this->id = $id;
+        $this->title = $title;
+        $this->attireRequired = $attireRequired;
+        $this->equipmentRequired = $equipmentRequired;
+        $this->supplemental = $supplemental;
+        $this->publishedAsTbd = $publishedAsTbd;
+        $this->published = $published;
+        $this->updatedAt = $updatedAt;
+        $this->sessionType = $sessionType;
+        $this->sessionDescription = $sessionDescription;
+        $this->course = $course;
+        $this->ilmSession = $ilmSession;
+
+        $this->topics = [];
+        $this->terms = [];
+        $this->objectives = [];
+        $this->meshDescriptors = [];
+        $this->learningMaterials = [];
+        $this->offerings = [];
+    }
+}
