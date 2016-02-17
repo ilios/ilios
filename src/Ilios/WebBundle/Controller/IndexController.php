6b124c73f735d44c602ff6f52865f1822457e0c9
Jonathan Johnson <jon.johnson@ucsf.edu>
2016-02-16 23:07:54 -0800

    Use the index.html file in the IndexController

--- src/Ilios/WebBundle/Controller/IndexController.php
+++ src/Ilios/WebBundle/Controller/IndexController.php
@@ -4,6 +4,7 @@ namespace Ilios\WebBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Ilios\CliBundle\Command\UpdateFrontendCommand;
 
 use Ilios\WebBundle\Service\WebIndexFromJson;
 
@@ -11,13 +12,17 @@ class IndexController extends Controller
 {
     public function indexAction()
     {
+        $fs = $this->get('ilioscore.symfonyfilesystem');
+        $path = $this->getParameter('kernel.cache_dir') . '/' . UpdateFrontendCommand::CACHE_FILE_NAME;
 
-        $file = $this->get('iliosweb.jsonindex')->getIndex(WebIndexFromJson::DEVELOPMENT);
-        if (!$file) {
-            throw new \Exception('Unable to retrieve the index file');
+        if (!$fs->exists($path)) {
+            throw new \Exception(
+                'Unable to load the index file.  Run ilios:maintenance:update-frontend to create it.'
+            );
         }
+        $contents = $fs->readFile($path);
 
-        $response = new Response($file);
+        $response = new Response($contents);
         $response->headers->set('Content-Type', 'text/html');
 
         $response->setPublic();
