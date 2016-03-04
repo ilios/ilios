31325d328248cd28140fcb74519dea05ae9d3613
Jonathan Johnson <jon.johnson@ucsf.edu>
2016-03-04 13:51:54 -0800

    Bump the api version to 1.2
    Big change with the removal of topics, lets increment the API version
    we provide accordingly.

--- src/Ilios/WebBundle/Service/WebIndexFromJson.php
+++ src/Ilios/WebBundle/Service/WebIndexFromJson.php
@@ -11,7 +11,7 @@ class WebIndexFromJson
      * @var string
      */
     const DEFAULT_TEMPLATE_NAME = 'webindex.html.twig';
-    const API_VERSION = 'v1.1';
+    const API_VERSION = 'v1.2';
     const AWS_BUCKER = 'https://s3-us-west-2.amazonaws.com/frontend-json-config/';
 
     const PRODUCTION = 'prod';
