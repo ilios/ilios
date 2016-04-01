dd12481764343004e997288232b3e30f82f74aaf
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2016-04-01 11:50:01 -0700

    added mesh descriptor DTO voter.

---
+++ src/Ilios/AuthenticationBundle/Voter/DTO/MeshDescriptorDTOVoter.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Ilios\AuthenticationBundle\Voter\DTO;
+
+use Ilios\AuthenticationBundle\Voter\Entity\TermEntityVoter;
+use Ilios\CoreBundle\Entity\DTO\MeshDescriptorDTO;
+use Ilios\CoreBundle\Entity\UserInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+
+/**
+ * Class MeshDescriptorDTOVoter
+ * @package Ilios\AuthenticationBundle\Voter\DTO
+ */
+class MeshDescriptorDTOVoter extends TermEntityVoter
+{
+    /**
+     * @inheritdoc
+     */
+    protected function supports($attribute, $subject)
+    {
+        return $subject instanceof MeshDescriptorDTO && in_array($attribute, array(self::VIEW));
+    }
+
+    /**
+     * @param string $attribute
+     * @param MeshDescriptorDTO $requestedDescriptor
+     * @param TokenInterface $token
+     * @return bool
+     */
+    protected function voteOnAttribute($attribute, $requestedDescriptor, TokenInterface $token)
+    {
+        $user = $token->getUser();
+        if (!$user instanceof UserInterface) {
+            return false;
+        }
+
+        switch ($attribute) {
+            case self::VIEW:
+                return true;
+                break;
+        }
+
+        return false;
+    }
+}
