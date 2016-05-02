4748c4a20898e7a98a4456c94ef9efed9261e3c5
Jonathan Johnson <jon.johnson@ucsf.edu>
2016-05-01 19:33:33 -0700

    Update symfony standard edition files to 2.3
    Following the guide at:
    https://github.com/symfony/symfony-standard/blob/2.8/UPGRADE-2.3.md

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -47,7 +47,7 @@ class AppKernel extends Kernel
             new Ilios\CliBundle\IliosCliBundle(),
         );
 
-        if (in_array($this->getEnvironment(), array('dev', 'test'))) {
+        if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
@@ -64,7 +64,7 @@ class AppKernel extends Kernel
      */
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
-        $loader->load(__DIR__.'/config/config_'.$this->getEnvironment().'.yml');
+        $loader->load($this->getRootDir().'/config/config_'.$this->getEnvironment().'.yml');
     }
 
     /**
