48546b353b246ed6221aea49d69bda34152c3078
Jonathan Johnson <jon.johnson@ucsf.edu>
2016-05-01 19:54:41 -0700

    Change files for symfony standard edition 2.8
    Followed guide at:
    https://github.com/symfony/symfony-standard/blob/2.8/UPGRADE-2.8.md

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -59,6 +59,21 @@ class AppKernel extends Kernel
         return $bundles;
     }
 
+    public function getRootDir()
+    {
+        return __DIR__;
+    }
+
+    public function getCacheDir()
+    {
+        return dirname(__DIR__).'/var/cache/'.$this->getEnvironment();
+    }
+
+    public function getLogDir()
+    {
+        return dirname(__DIR__).'/var/logs';
+    }
+
     /**
      * @inheritdoc
      */
