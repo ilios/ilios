788b01ca6fbdd644e0e1357500d1bdfee549b571
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2016-07-21 21:07:10 -0700

    added class docblock.

--- src/Ilios/CoreBundle/EventListener/TimestampListener.php
+++ src/Ilios/CoreBundle/EventListener/TimestampListener.php
@@ -3,9 +3,14 @@
 namespace Ilios\CoreBundle\EventListener;
 
 use Doctrine\ORM\Event\OnFlushEventArgs;
-
 use Ilios\CoreBundle\Traits\TimestampableEntityInterface;
 
+/**
+ * Event listener that updates timestamp-properties of eligible entities during insertion/update.
+ *
+ * Class TimestampListener
+ * @package Ilios\CoreBundle\EventListener
+ */
 class TimestampListener
 {
      public function onFlush(OnFlushEventArgs $eventArgs)
