3c0dbbad25da23fb8979640036deed01fd355a66
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2017-02-02 11:56:03 -0800

    added voters.

---
+++ src/Ilios/AuthenticationBundle/Voter/ApplicationConfigVoter.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Ilios\AuthenticationBundle\Voter;
+
+use Ilios\CoreBundle\Entity\ApplicationConfigInterface;
+use Ilios\CoreBundle\Entity\UserInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+
+/**
+ * Class ApplicationConfigVoter
+ * @package Ilios\AuthenticationBundle\Voter
+ */
+class ApplicationConfigVoter extends AbstractVoter
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function supports($attribute, $subject)
+    {
+        return $subject instanceof ApplicationConfigInterface && in_array($attribute, array(
+                self::CREATE, self::VIEW, self::EDIT, self::DELETE
+            ));
+    }
+
+    /**
+     * @param string $attribute
+     * @param ApplicationConfigInterface $applicationConfig
+     * @param TokenInterface $token
+     * @return bool
+     */
+    protected function voteOnAttribute($attribute, $applicationConfig, TokenInterface $token)
+    {
+        $user = $token->getUser();
+        if (!$user instanceof UserInterface) {
+            return false;
+        }
+
+        switch ($attribute) {
+            // grant VIEW privileges to all authenticated users.
+            case self::VIEW:
+                return true;
+                break;
+            case self::CREATE:
+            case self::EDIT:
+            case self::DELETE:
+                // grant CREATE, EDIT and DELETE privileges
+                // if the user has the 'Developer' role
+                return $this->userHasRole($user, ['Developer']);
+                break;
+        }
+
+        return false;
+    }
+}
