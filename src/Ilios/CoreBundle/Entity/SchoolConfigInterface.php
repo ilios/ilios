e5564b3b529e874e7e0e7d293970ebeab799436b
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2017-02-03 09:33:59 -0800

    the cake is a lie.

--- src/Ilios/CoreBundle/Entity/SchoolConfigInterface.php
+++ src/Ilios/CoreBundle/Entity/SchoolConfigInterface.php
@@ -2,12 +2,24 @@
 
 namespace Ilios\CoreBundle\Entity;
 
+use Ilios\CoreBundle\Traits\IdentifiableEntityInterface;
+use Ilios\CoreBundle\Traits\NameableEntityInterface;
 use Ilios\CoreBundle\Traits\SchoolEntityInterface;
 
 /**
  * Interface SchoolConfigInterface
  * @package Ilios\CoreBundle\Entity
  */
-interface SchoolConfigInterface extends ApplicationConfigInterface, SchoolEntityInterface
+interface SchoolConfigInterface extends SchoolEntityInterface, NameableEntityInterface, IdentifiableEntityInterface
 {
+    /**
+     * @return string
+     */
+    public function getValue();
+
+    /**
+     * @param string $value
+     */
+    public function setValue($value);
+
 }
