e2d6be0ba6c51f87af0f6651da99228c890eee09
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2017-02-03 08:20:53 -0800

    renamed unique constraints.

--- src/Ilios/CoreBundle/Entity/SchoolConfig.php
+++ src/Ilios/CoreBundle/Entity/SchoolConfig.php
@@ -14,7 +14,7 @@ use Ilios\CoreBundle\Traits\SchoolEntity;
  *
  * @ORM\Table(name="school_config",
  *   uniqueConstraints={
- *     @ORM\UniqueConstraint(name="school_config_unique", columns={"school_id", "name"})
+ *     @ORM\UniqueConstraint(name="school_conf_uniq", columns={"school_id", "name"})
  *   }
  * )
  * @ORM\Entity
