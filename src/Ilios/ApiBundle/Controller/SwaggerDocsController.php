5b5f724296e79f5fd57b63aa6cfeaf1b76e74ee2
Jonathan Johnson <jon.johnson@ucsf.edu>
2017-02-14 14:43:18 -0800

    First pass at swagger docs
    Free handed and very error filled .yml files, but this is a working
    example and template for organizing and creating the documentation.

---
+++ src/Ilios/ApiBundle/Controller/SwaggerDocsController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Ilios\ApiBundle\Controller;
+
+use Ilios\WebBundle\Service\WebIndexFromJson;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Yaml\Yaml;
+use Symfony\Component\Finder\SplFileInfo;
+
+class SwaggerDocsController extends Controller
+{
+    public function indexAction(Request $request)
+    {
+        $swaggerDir = $this->get('kernel')->locateResource("@IliosApiBundle/Resources/swagger");
+        $paths = $this->getSection("{$swaggerDir}/paths");
+        $definitions = $this->getSection("{$swaggerDir}/definitions");
+
+        $swaggerDefinition = $this->getFrontMatter($request);
+        $swaggerDefinition['paths'] = $paths;
+        $swaggerDefinition['definitions'] = $definitions;
+
+        $yaml = Yaml::dump($swaggerDefinition);
+
+        $response = new Response(
+            $yaml,
+            Response::HTTP_OK,
+            ['Content-type' => 'application/x-yaml']
+        );
+
+        return $response;
+    }
+
+    protected function getSection($dir)
+    {
+        $finder = new Finder();
+        $files = $finder->in($dir)->files()->name('*.yml')->sortByName();
+
+        $items = [];
+        /** @var SplFileInfo $file */
+        foreach ($files as $file) {
+            $contents = Yaml::parse($file->getContents());
+
+            $items = array_merge($items, $contents);
+        }
+
+        return $items;
+    }
+
+    protected function getFrontMatter(Request $request)
+    {
+        $arr = [];
+        $arr['swagger'] = '2.0';
+        $arr['info'] = [
+            'title' => 'Ilios API',
+            'description' => 'Ilios API Description',
+            'version' => WebIndexFromJson::API_VERSION,
+        ];
+
+        $arr['host'] = $request->getHttpHost();
+        $arr['schemes'] = [$this->container->getParameter('forceProtocol')];
+        $arr['basePath'] = '/api/v1';
+        $arr['produces'] = ['application/json'];
+
+        return $arr;
+    }
+
+}
