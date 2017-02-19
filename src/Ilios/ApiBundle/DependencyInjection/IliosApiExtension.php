f9b9a7d8123e10af1468a2103070af3e0f34e2d4
Jonathan Johnson <jon.johnson@ucsf.edu>
2017-02-18 21:03:18 -0800

    Improve route organization
    Move special snowflakes into their own file in order to make them easier
    to find and make the generic routes clearer.
    
    Make the non-dto entities a configuration parameter so we don't have to
    keep two strings in sync.

--- src/Ilios/ApiBundle/DependencyInjection/IliosApiExtension.php
+++ src/Ilios/ApiBundle/DependencyInjection/IliosApiExtension.php
@@ -20,6 +20,7 @@ class IliosApiExtension extends Extension
     public function load(array $configs, ContainerBuilder $container)
     {
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('config.yml');
         $loader->load('services.yml');
     }
 }
