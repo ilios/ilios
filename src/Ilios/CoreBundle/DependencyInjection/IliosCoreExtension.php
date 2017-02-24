3cc7453891e3030b17af731cf22a0bcd29f9d0fe
Jonathan Johnson <jon.johnson@ucsf.edu>
2017-02-23 23:34:43 -0800

    Remove Forms and handlers
    We're no longer using these since serialization and validation are handled directly.

--- src/Ilios/CoreBundle/DependencyInjection/IliosCoreExtension.php
+++ src/Ilios/CoreBundle/DependencyInjection/IliosCoreExtension.php
@@ -37,9 +37,7 @@ class IliosCoreExtension extends Extension
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
-        $loader->load('abstract_types.yml');
         $loader->load('managers.yml');
-        $loader->load('handlers.yml');
         $loader->load('dataloaders.yml');
     }
 }
