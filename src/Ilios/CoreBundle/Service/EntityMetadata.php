36bd257cb322cac0f8ee098e42e224314041c802
Jonathan Johnson <jon.johnson@ucsf.edu>
2017-03-05 14:54:04 -0800

    Reduce expensive calls to class_exists and annotation reading methods
    In order to read an annotation we first must build a reflection object
    and that fails when we pass a bad class name.  Since we use
    isIliosEntity and isIliosDto methods when checking the serializer they
    get called many times and have to take care to error check their
    parameters.  To reduce this cost here we scan the directory tree for
    classes which contain these annotations on load and then use that list.
    
    This should be very constant in production so we can cache these values
    safely.

--- src/Ilios/CoreBundle/Service/EntityMetadata.php
+++ src/Ilios/CoreBundle/Service/EntityMetadata.php
@@ -6,11 +6,17 @@ use Doctrine\Common\Annotations\AnnotationReader;
 use Doctrine\Common\Annotations\CachedReader;
 use Doctrine\Common\Annotations\Reader;
 use Doctrine\Common\Cache\Cache;
+use Doctrine\Common\Persistence\Proxy;
 use Ilios\ApiBundle\Annotation\ReadOnly;
 use Ilios\ApiBundle\Annotation\Type;
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\Finder\SplFileInfo;
+use Symfony\Component\HttpKernel\KernelInterface;
 
 class EntityMetadata
 {
+    const CACH_KEY_PREFIX = 'ilios_core-entity-metadata-';
+
     /**
      * @var Reader
      */
@@ -21,54 +27,126 @@ class EntityMetadata
      */
     private $exposedPropertiesForClass;
 
-    public function __construct(Cache $cache, $environment)
+    /**
+     * @var array
+     */
+    private $iliosEntities;
+
+    /**
+     * @var array
+     */
+    private $iliosDtos;
+
+    /**
+     * EntityMetadata constructor
+     *
+     * Build and cache all of the entity and dto class names so
+     * we don't have to constantly run expensive class_exists
+     * and annotation inspection tasks
+     *
+     * @param Cache $cache
+     * @param KernelInterface $kernel
+     */
+    public function __construct(Cache $cache, KernelInterface $kernel)
     {
         $this->exposedPropertiesForClass = [];
 
         $this->annotationReader = new CachedReader(
             new AnnotationReader(),
             $cache,
-            $debug = $environment !== 'prod'
+            $debug = $kernel->getEnvironment() !== 'prod'
         );
+
+        $entityKey = self::CACH_KEY_PREFIX . 'entities';
+        if (!$cache->contains($entityKey)) {
+            $cache->save($entityKey, $this->findIliosEntities($kernel));
+        }
+
+        $this->iliosEntities = $cache->fetch($entityKey);
+
+        $dtoKey = self::CACH_KEY_PREFIX . 'dtos';
+        if (!$cache->contains($dtoKey)) {
+            $cache->save($dtoKey, $this->findIliosDtos($kernel));
+        }
+
+        $this->iliosDtos = $cache->fetch($dtoKey);
     }
 
+    /**
+     * Check if an object or className has the Entity annotation
+     *
+     * @param $classNameOrObject
+     *
+     * @return bool
+     */
     public function isAnIliosEntity($classNameOrObject)
     {
-        if ((is_string($classNameOrObject) && class_exists($classNameOrObject)) ||
-            is_object($classNameOrObject)
-        ) {
-            $reflection = new \ReflectionClass($classNameOrObject);
-            if ($reflection->implementsInterface('Doctrine\Common\Persistence\Proxy')) {
-                $reflection = $reflection->getParentClass();
-            }
-            
-            $annotation = $this->annotationReader->getClassAnnotation(
-                $reflection,
-                'Ilios\ApiBundle\Annotation\Entity'
-            );
+        if ($this->isAStringOrClass($classNameOrObject)) {
+            $className = $this->getClassName($classNameOrObject);
 
-            return !is_null($annotation);
+            if (in_array($className, $this->iliosEntities)) {
+                return true;
+            }
+            if (is_object($classNameOrObject) && $classNameOrObject instanceof Proxy) {
+                $reflection = new \ReflectionClass($classNameOrObject);
+                $reflection = $reflection->getParentClass();
+                $className = $reflection->getName();
+
+                return in_array($className, $this->iliosDtos);
+            }
         }
 
         return false;
     }
 
+    /**
+     * Check if an object or class name has the DTO annotation
+     *
+     * @param $classNameOrObject
+     *
+     * @return bool
+     */
     public function isAnIliosDto($classNameOrObject)
     {
-        if ((is_string($classNameOrObject) && class_exists($classNameOrObject)) ||
-            is_object($classNameOrObject)
-        ) {
-            $annotation = $this->annotationReader->getClassAnnotation(
-                new \ReflectionClass($classNameOrObject),
-                'Ilios\ApiBundle\Annotation\DTO'
-            );
+        if ($this->isAStringOrClass($classNameOrObject)) {
+            $className = $this->getClassName($classNameOrObject);
 
-            return !is_null($annotation);
+            return in_array($className, $this->iliosDtos);
         }
 
         return false;
     }
 
+    /**
+     * Checks to see if what we have been passed is a string or a class
+     * @param $classNameOrObject
+     * @return bool
+     */
+    protected function isAStringOrClass($classNameOrObject)
+    {
+        return is_string($classNameOrObject) || is_object($classNameOrObject);
+    }
+
+    /**
+     * Gets the name of a class
+     *
+     * @param $classNameOrObject
+     * @return string
+     */
+    protected function getClassName($classNameOrObject)
+    {
+        return is_object($classNameOrObject)?get_class($classNameOrObject):$classNameOrObject;
+
+    }
+
+    /**
+     * Get all of the properties of a call which are
+     * marked with the Exposed annotation
+     *
+     * @param \ReflectionClass $reflection
+     *
+     * @return mixed
+     */
     public function extractExposedProperties(\ReflectionClass $reflection)
     {
         $className = $reflection->getName();
@@ -95,6 +173,14 @@ class EntityMetadata
         return $this->exposedPropertiesForClass[$className];
     }
 
+    /**
+     * Get all of the properties of a class which are
+     * not annotated as ReadOnly
+     *
+     * @param \ReflectionClass $reflection
+     *
+     * @return array
+     */
     public function extractWritableProperties(\ReflectionClass $reflection)
     {
         $exposedProperties = $this->extractExposedProperties($reflection);
@@ -104,6 +190,13 @@ class EntityMetadata
         });
     }
 
+    /**
+     * Get all of the annotated ReadOnly properties of a class
+     *
+     * @param \ReflectionClass $reflection
+     *
+     * @return array
+     */
     public function extractReadOnlyProperties(\ReflectionClass $reflection)
     {
         $exposedProperties = $this->extractExposedProperties($reflection);
@@ -113,6 +206,15 @@ class EntityMetadata
         });
     }
 
+    /**
+     * Get the Type annotation of a property
+     *
+     * @param \ReflectionProperty $property
+     *
+     * @return mixed
+     *
+     * @throws \Exception
+     */
     public function getTypeOfProperty(\ReflectionProperty $property)
     {
         /** @var Type $typeAnnotation */
@@ -130,6 +232,13 @@ class EntityMetadata
         return $typeAnnotation->value;
     }
 
+    /**
+     * Check if a property has the ReadOnly annotation
+     *
+     * @param \ReflectionProperty $property
+     *
+     * @return bool
+     */
     public function isPropertyReadOnly(\ReflectionProperty $property)
     {
         /** @var ReadOnly $annotation */
@@ -141,6 +250,13 @@ class EntityMetadata
         return !is_null($annotation);
     }
 
+    /**
+     * Check if a property has the RemoveMarkup annotation
+     *
+     * @param \ReflectionProperty $property
+     *
+     * @return bool
+     */
     public function isPropertyRemoveMarkup(\ReflectionProperty $property)
     {
         /** @var ReadOnly $annotation */
@@ -150,5 +266,82 @@ class EntityMetadata
         );
 
         return !is_null($annotation);
+    }
+
+    /**
+     * Load Entities by scanning the file system
+     * then use that list to discover those which have the
+     * correct annotation
+     *
+     * @param KernelInterface $kernel
+     *
+     * @return array
+     */
+    protected function findIliosEntities(KernelInterface $kernel)
+    {
+        $path = $kernel->locateResource('@IliosCoreBundle/Entity');
+        $finder = new Finder();
+        $files = $finder->in($path)->files()->depth("== 0")->notName('*Interface.php')->sortByName();
+
+        $list = [];
+        /** @var SplFileInfo $file */
+        foreach ($files as $file) {
+            $class = 'Ilios\\CoreBundle\\Entity' . '\\' . $file->getBasename('.php');
+            $annotation = $this->annotationReader->getClassAnnotation(
+                new \ReflectionClass($class),
+                'Ilios\ApiBundle\Annotation\Entity'
+            );
+            if (null !== $annotation) {
+                $list[] = $class;
+            }
+        }
+
+        return $list;
+    }
+
+    /**
+     * Load classes by scanning directories then use
+     * that list to discover classes which have the DTO annotation
+     *
+     * @param KernelInterface $kernel
+     *
+     * @return array
+     */
+    protected function findIliosDtos(KernelInterface $kernel)
+    {
+        $dtoPath = $kernel->locateResource('@IliosCoreBundle/Entity/DTO');
+        $finder = new Finder();
+        $files = $finder->in($dtoPath)->files()->depth("== 0")->sortByName();
+
+        $list = [];
+        /** @var SplFileInfo $file */
+        foreach ($files as $file) {
+            $class = 'Ilios\\CoreBundle\\Entity\\DTO' . '\\' . $file->getBasename('.php');
+            $annotation = $this->annotationReader->getClassAnnotation(
+                new \ReflectionClass($class),
+                'Ilios\ApiBundle\Annotation\DTO'
+            );
+            if (null !== $annotation) {
+                $list[] = $class;
+            }
+        }
+
+        $classPath = $kernel->locateResource('@IliosCoreBundle/Classes');
+        $finder = new Finder();
+        $files = $finder->in($classPath)->files()->depth("== 0")->sortByName();
+
+        /** @var SplFileInfo $file */
+        foreach ($files as $file) {
+            $class = 'Ilios\\CoreBundle\\Classes' . '\\' . $file->getBasename('.php');
+            $annotation = $this->annotationReader->getClassAnnotation(
+                new \ReflectionClass($class),
+                'Ilios\ApiBundle\Annotation\DTO'
+            );
+            if (null !== $annotation) {
+                $list[] = $class;
+            }
+        }
+
+        return $list;
     }
 }
