39a6b93f635e05237ae17929c8c4e681f3f7a783
Jonathan Johnson <jon.johnson@ucsf.edu>
2017-04-25 13:17:53 -0700

    DTO ApplicationConfig

---
+++ src/Ilios/CoreBundle/Entity/DTO/ApplicationConfigDTO.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Ilios\CoreBundle\Entity\DTO;
+
+use Ilios\ApiBundle\Annotation as IS;
+
+/**
+ * Class ApplicationConfigDTO
+ * Data transfer object for an applicationConfig
+ * @package Ilios\CoreBundle\Entity\DTO
+ *
+ * @IS\DTO
+ */
+class ApplicationConfigDTO
+{
+    /**
+     * @var int
+     * @IS\Expose
+     * @IS\Type("integer")
+     */
+    public $id;
+
+    /**
+     * @var int
+     * @IS\Expose
+     * @IS\Type("string")
+     */
+    public $name;
+
+    /**
+     * @var int
+     * @IS\Expose
+     * @IS\Type("string")
+     */
+    public $value;
+
+    public function __construct($id, $name, $value)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->value = $value;
+    }
+}
---
+++ src/Ilios/CoreBundle/Entity/Repository/ApplicationConfigRepository.php
@@ -0,0 +1,94 @@
+<?php
+namespace Ilios\CoreBundle\Entity\Repository;
+
+use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\QueryBuilder;
+use Doctrine\ORM\AbstractQuery;
+use Ilios\CoreBundle\Entity\DTO\ApplicationConfigDTO;
+
+/**
+ * Class ApplicationConfigRepository
+ * @package Ilios\CoreBundle\Entity\Repository
+ */
+class ApplicationConfigRepository extends EntityRepository
+{
+    /**
+     * @inheritdoc
+     */
+    public function findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+    {
+        $qb = $this->_em->createQueryBuilder();
+        $qb->select('DISTINCT x')->from('IliosCoreBundle:ApplicationConfig', 'x');
+
+        $this->attachCriteriaToQueryBuilder($qb, $criteria, $orderBy, $limit, $offset);
+
+        return $qb->getQuery()->getResult();
+    }
+
+    /**
+     * Find and hydrate as DTOs
+     *
+     * @param array $criteria
+     * @param array|null $orderBy
+     * @param null $limit
+     * @param null $offset
+     *
+     * @return array
+     */
+    public function findDTOsBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+    {
+        $qb = $this->_em->createQueryBuilder()->select('x')->distinct()->from('IliosCoreBundle:ApplicationConfig', 'x');
+        $this->attachCriteriaToQueryBuilder($qb, $criteria, $orderBy, $limit, $offset);
+        $applicationConfigDTOs = [];
+        foreach ($qb->getQuery()->getResult(AbstractQuery::HYDRATE_ARRAY) as $arr) {
+            $applicationConfigDTOs[] = new ApplicationConfigDTO(
+                $arr['id'],
+                $arr['name'],
+                $arr['value']
+            );
+        }
+
+        return $applicationConfigDTOs;
+    }
+
+
+    /**
+     * @param QueryBuilder $qb
+     * @param array $criteria
+     * @param array $orderBy
+     * @param int $limit
+     * @param int $offset
+     *
+     * @return QueryBuilder
+     */
+    protected function attachCriteriaToQueryBuilder(QueryBuilder $qb, $criteria, $orderBy, $limit, $offset)
+    {
+        if (count($criteria)) {
+            foreach ($criteria as $key => $value) {
+                $values = is_array($value) ? $value : [$value];
+                $qb->andWhere($qb->expr()->in("x.{$key}", ":{$key}"));
+                $qb->setParameter(":{$key}", $values);
+            }
+        }
+
+        if (empty($orderBy)) {
+            $orderBy = ['id' => 'ASC'];
+        }
+
+        if (is_array($orderBy)) {
+            foreach ($orderBy as $sort => $order) {
+                $qb->addOrderBy('x.'.$sort, $order);
+            }
+        }
+
+        if ($offset) {
+            $qb->setFirstResult($offset);
+        }
+
+        if ($limit) {
+            $qb->setMaxResults($limit);
+        }
+
+        return $qb;
+    }
+}
