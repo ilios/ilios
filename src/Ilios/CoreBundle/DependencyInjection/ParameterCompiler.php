1cb8f2169591c46de0ffba27a6a81e46f94edd50
Jonathan Johnson <jon.johnson@ucsf.edu>
2017-07-21 16:50:35 -0700

    Pull tracking parameters from the DB
    Our analytics setup needs to be configurable, but that configuration has
    to be known at compile time when we don't have access yet. What we can
    do instead is use a fake parameter initially and then load the real one
    afterwards.

---
+++ src/Ilios/CoreBundle/DependencyInjection/ParameterCompiler.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Ilios\CoreBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+/**
+ * Class ParameterCompiler
+ *
+ * Some of our configuration options are stored in the database but still
+ * need to be exposed to third-party services.
+ * This ParameterCompiler pulls those values out of the database and injects them into the
+ * service by replacing the arguments
+ *
+ * @package Ilios\CoreBundle\DependencyInjection
+ */
+class ParameterCompiler implements CompilerPassInterface {
+    public function process(ContainerBuilder $container)
+    {
+        $applicationConfiguration = $container->get('Ilios\CoreBundle\Service\ApplicationConfiguration');
+
+        $trackingCode = $applicationConfiguration->get('tracking_code');
+        $trackerDef = $container->getDefinition('happyr.google_analytics.tracker');
+        $arguments = $trackerDef->getArguments();
+        $trackingIdArgument = array_search('INJECTED FROM DB IN ParameterCompiler', $arguments);
+        $trackerDef->replaceArgument($trackingIdArgument, $trackingCode);
+    }
+}
--- src/Ilios/CoreBundle/IliosCoreBundle.php
+++ src/Ilios/CoreBundle/IliosCoreBundle.php
@@ -1,8 +1,17 @@
 <?php
 namespace Ilios\CoreBundle;
 
+use Ilios\CoreBundle\DependencyInjection\ParameterCompiler;
+use Symfony\Component\DependencyInjection\Compiler\PassConfig;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class IliosCoreBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $container->addCompilerPass(new ParameterCompiler(), PassConfig::TYPE_AFTER_REMOVING);
+    }
 }
