3790d82868f2ca110f89846b621eb5b5e2e0c4db
Jonathan Johnson <jon.johnson@ucsf.edu>
2017-07-23 22:20:12 -0700

    Go DTO for LearningMaterialStatus

---
+++ src/Ilios/AuthenticationBundle/Voter/DTO/LearningMaterialStatusDTOVoter.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Ilios\AuthenticationBundle\Voter\DTO;
+
+use Ilios\CoreBundle\Entity\DTO\LearningMaterialStatusDTO;
+use Ilios\AuthenticationBundle\Voter\AbstractVoter;
+use Ilios\AuthenticationBundle\Classes\SessionUserInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+
+/**
+ * Class LearningMaterialStatusDTOVoter
+ * @package Ilios\AuthenticationBundle\Voter\DTO
+ */
+class LearningMaterialStatusDTOVoter extends AbstractVoter
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function supports($attribute, $subject)
+    {
+        return $subject instanceof LearningMaterialStatusDTO && in_array($attribute, array(self::VIEW));
+    }
+
+    /**
+     * @param string $attribute
+     * @param LearningMaterialStatusDTO $learningMaterialStatusDto
+     * @param TokenInterface $token
+     * @return bool
+     */
+    protected function voteOnAttribute($attribute, $learningMaterialStatusDto, TokenInterface $token)
+    {
+        $user = $token->getUser();
+        if (!$user instanceof SessionUserInterface) {
+            return false;
+        }
+
+        switch ($attribute) {
+            case self::VIEW:
+                return true;
+                break;
+        }
+        return false;
+    }
+}
--- src/Ilios/AuthenticationBundle/Voter/Entity/LearningMaterialStatusEntityVoter.php
+++ src/Ilios/AuthenticationBundle/Voter/Entity/LearningMaterialStatusEntityVoter.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace Ilios\AuthenticationBundle\Voter;
+namespace Ilios\AuthenticationBundle\Voter\Entity;
 
+use Ilios\AuthenticationBundle\Voter\AbstractVoter;
 use Ilios\CoreBundle\Entity\LearningMaterialStatusInterface;
 use Ilios\AuthenticationBundle\Classes\SessionUserInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
@@ -10,7 +11,7 @@ use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
  * Class LearningMaterialStatusVoter
  * @package Ilios\AuthenticationBundle\Voter
  */
-class LearningMaterialStatusVoter extends AbstractVoter
+class LearningMaterialStatusEntityVoter extends AbstractVoter
 {
     /**
      * {@inheritdoc}
