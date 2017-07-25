b6ee9d547bc34b5bf85f049b581a1b6b40247b91
Jonathan Johnson <jon.johnson@ucsf.edu>
2017-07-24 20:54:33 -0700

    Convert Permission endpoint to the DTO pattern

---
+++ src/Ilios/AuthenticationBundle/Voter/DTO/PermissionDTOVoter.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Ilios\AuthenticationBundle\Voter\DTO;
+
+use Ilios\AuthenticationBundle\Classes\SessionUserInterface;
+use Ilios\AuthenticationBundle\Voter\AbstractVoter;
+use Ilios\CoreBundle\Entity\DTO\PermissionDTO;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+
+/**
+ * Class PermissionDTOVoter
+ * @package Ilios\AuthenticationBundle\Voter\DTO
+ */
+class PermissionDTOVoter extends AbstractVoter
+{
+    /**
+     * @inheritdoc
+     */
+    protected function supports($attribute, $subject)
+    {
+        return $subject instanceof PermissionDTO && in_array($attribute, array(self::VIEW));
+    }
+
+    /**
+     * @param string $attribute
+     * @param PermissionDTO $requestedPermission
+     * @param TokenInterface $token
+     * @return bool
+     */
+    protected function voteOnAttribute($attribute, $requestedPermission, TokenInterface $token)
+    {
+        $user = $token->getUser();
+        if (!$user instanceof SessionUserInterface) {
+            return false;
+        }
+
+        switch ($attribute) {
+            case self::VIEW:
+                return (
+                    $user->getId() === $requestedPermission->user
+                    || $user->hasRole(['Developer'])
+                );
+                break;
+        }
+
+        return false;
+    }
+}
