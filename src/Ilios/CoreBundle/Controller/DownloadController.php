7a4cfbd207d8523bfa92de22da2ddb79bcb53241
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2017-08-02 09:58:37 -0700

    conditionally set content disposition header on PDF download requests.

--- src/Ilios/CoreBundle/Controller/DownloadController.php
+++ src/Ilios/CoreBundle/Controller/DownloadController.php
@@ -4,6 +4,7 @@ namespace Ilios\CoreBundle\Controller;
 use Ilios\CoreBundle\Entity\Manager\LearningMaterialManager;
 use Ilios\CoreBundle\Service\IliosFileSystem;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Exception;
@@ -17,10 +18,11 @@ class DownloadController extends AbstractController
     public function learningMaterialAction(
         $token,
         LearningMaterialManager $learningMaterialManager,
-        IliosFileSystem $iliosFileSystem
+        IliosFileSystem $iliosFileSystem,
+        Request $request
     ) {
         $learningMaterial = $learningMaterialManager->findOneBy(['token' => $token]);
-        
+
         if (!$learningMaterial) {
             throw new NotFoundHttpException();
         }
@@ -31,18 +33,22 @@ class DownloadController extends AbstractController
                 "No valid path for learning material with token: " . $token
             );
         }
-        
+
         $file = $iliosFileSystem->getFile($learningMaterial->getRelativePath());
-        
+
         if (false === $file) {
             throw new Exception('File not found for learning material #' . $learningMaterial->getId());
         }
-        
+
         $headers = array(
             'Content-Type' => $learningMaterial->getMimetype(),
             'Content-Disposition' => 'attachment; filename="' . $learningMaterial->getFilename() . '"'
         );
 
+        // d/l PDFs inline if requested so.
+        if ('application/pdf' === $headers['Content-Type'] && $request->query->has('inline')) {
+            $headers['Content-Disposition'] = 'inline';
+        }
 
         return new Response(file_get_contents($file->getPathname()), 200, $headers);
     }
