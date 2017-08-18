bc322f5be6d15a2c1eef462d52710a1a82e3fc43
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2017-08-18 07:50:44 -0700

    activate all session types.

---
+++ app/Resources/DoctrineMigrations/Version20170818144244.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Ilios\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Activate all session types.
+ */
+class Version20170818144244 extends AbstractMigration
+{
+    /**
+     * @inheritdoc
+     */
+    public function up(Schema $schema)
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->addSql('UPDATE session_type SET active = 1');
+
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function down(Schema $schema)
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->addSql('UPDATE session_type SET active = 0');
+
+
+    }
+}
