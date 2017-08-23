6ec52a8a08e21ca493404328ae85ecfacc7a46d7
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2017-08-23 16:13:48 -0700

    blank out user events outside their time release schedule

--- src/Ilios/CoreBundle/Classes/UserEvent.php
+++ src/Ilios/CoreBundle/Classes/UserEvent.php
@@ -79,13 +79,41 @@ class UserEvent extends CalendarEvent
     }
 
     /**
-     * @param int $timestamp
+     * @param \DateTime $dateTime
      */
-    public function clearTimedMaterials($timestamp) {
+    public function clearTimedMaterials(\DateTime $dateTime) {
+        $doNotScrubProps = array(
+            'id',
+            'title',
+            'course',
+            'courseTitle',
+            'session',
+            'sessionTitle',
+            'startDate',
+            'endDate',
+            'isBlanked'
+        );
         foreach ($this->learningMaterials as $lm) {
-            $startDate = $lm['startDate'];
-            $endDate = $lm['endDate'];
-            // @todo implement [ST 2017/08/23]
+            $startDate = $lm->startDate;
+            $endDate = $lm->endDate;
+            $blankThis = false;
+            if (isset($startDate) && isset($endDate)) {
+                $blankThis = ($startDate > $dateTime || $dateTime > $endDate);
+            } elseif (isset($startDate)) {
+                $blankThis = ($startDate > $dateTime);
+            } elseif (isset($enDate)) {
+                $blankThis = ($dateTime > $endDate);
+            }
+
+            if ($blankThis) {
+                $lm->isBlanked = true;
+                $props = array_keys(get_object_vars($lm));
+                foreach($props as $prop) {
+                    if (! in_array($prop, $doNotScrubProps)) {
+                        $lm->$prop = null;
+                    }
+                }
+            }
         }
     }
 }
