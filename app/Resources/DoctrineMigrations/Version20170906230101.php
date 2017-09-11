6ab0b0bd0829a9fd8712b915ad820926cb687aa9
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2017-09-11 10:12:48 -0700

    consolidated migrations.

--- app/Resources/DoctrineMigrations/Version20170906230101.php
+++ app/Resources/DoctrineMigrations/Version20170906230101.php
@@ -6,7 +6,7 @@ use Doctrine\DBAL\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
 
 /**
- * Increase the max width on descriptor uid columns.
+ * Increase the column size on various MeSH tables.
  */
 class Version20170906230101 extends AbstractMigration
 {
@@ -15,9 +15,9 @@ class Version20170906230101 extends AbstractMigration
      */
     public function up(Schema $schema)
     {
-        // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
+        // mesh descriptor uid
         $this->addSql('ALTER TABLE objective_x_mesh CHANGE mesh_descriptor_uid mesh_descriptor_uid VARCHAR(12) NOT NULL');
         $this->addSql('ALTER TABLE course_learning_material_x_mesh CHANGE mesh_descriptor_uid mesh_descriptor_uid VARCHAR(12) NOT NULL');
         $this->addSql('ALTER TABLE session_learning_material_x_mesh CHANGE mesh_descriptor_uid mesh_descriptor_uid VARCHAR(12) NOT NULL');
@@ -28,6 +28,32 @@ class Version20170906230101 extends AbstractMigration
         $this->addSql('ALTER TABLE mesh_descriptor_x_qualifier CHANGE mesh_descriptor_uid mesh_descriptor_uid VARCHAR(12) NOT NULL');
         $this->addSql('ALTER TABLE mesh_tree CHANGE mesh_descriptor_uid mesh_descriptor_uid VARCHAR(12) DEFAULT NULL');
         $this->addSql('ALTER TABLE session_x_mesh CHANGE mesh_descriptor_uid mesh_descriptor_uid VARCHAR(12) NOT NULL');
+
+        // mesh qualifier uid
+        $this->addSql('ALTER TABLE mesh_qualifier CHANGE mesh_qualifier_uid mesh_qualifier_uid VARCHAR(12) NOT NULL');
+        $this->addSql('ALTER TABLE mesh_descriptor_x_qualifier CHANGE mesh_qualifier_uid mesh_qualifier_uid VARCHAR(12) NOT NULL');
+
+        // mesh concept casn1
+        $this->addSql('ALTER TABLE mesh_concept CHANGE casn_1_name casn_1_name VARCHAR(512) DEFAULT NULL');
+
+        // mesh concept uid
+        $this->addSql('ALTER TABLE mesh_concept_x_term DROP FOREIGN KEY FK_100AC50FE34D9FF5');
+        $this->addSql('ALTER TABLE mesh_concept_x_semantic_type DROP FOREIGN KEY FK_CD3D7893E34D9FF5');
+        $this->addSql('ALTER TABLE mesh_descriptor_x_concept DROP FOREIGN KEY FK_1AF85275E34D9FF5');
+        $this->addSql('ALTER TABLE mesh_concept MODIFY mesh_concept_uid VARCHAR(12) NOT NULL');
+        $this->addSql('ALTER TABLE mesh_concept_x_term CHANGE mesh_concept_uid mesh_concept_uid VARCHAR(12) NOT NULL COLLATE utf8_unicode_ci');
+        $this->addSql('ALTER TABLE mesh_concept_x_semantic_type CHANGE mesh_concept_uid mesh_concept_uid VARCHAR(12) NOT NULL COLLATE utf8_unicode_ci');
+        $this->addSql('ALTER TABLE mesh_descriptor_x_concept CHANGE mesh_concept_uid mesh_concept_uid VARCHAR(12) NOT NULL COLLATE utf8_unicode_ci');
+        $this->addSql('ALTER TABLE mesh_concept_x_term ADD CONSTRAINT FK_100AC50FE34D9FF5 FOREIGN KEY (mesh_concept_uid) REFERENCES mesh_concept (mesh_concept_uid)');
+        $this->addSql('ALTER TABLE mesh_concept_x_semantic_type ADD CONSTRAINT FK_CD3D7893E34D9FF5 FOREIGN KEY (mesh_concept_uid) REFERENCES mesh_concept (mesh_concept_uid)');
+        $this->addSql('ALTER TABLE mesh_descriptor_x_concept ADD CONSTRAINT FK_1AF85275E34D9FF5 FOREIGN KEY (mesh_concept_uid) REFERENCES mesh_concept (mesh_concept_uid)');
+
+        // mesh term uid
+        $this->addSql('ALTER TABLE mesh_term CHANGE mesh_term_uid mesh_term_uid VARCHAR(12) NOT NULL');
+
+        // mesh tree tree-number
+        $this->addSql('ALTER TABLE mesh_tree CHANGE tree_number tree_number VARCHAR(50) NOT NULL');
+
     }
 
     /**
@@ -37,6 +63,32 @@ class Version20170906230101 extends AbstractMigration
     {
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
+        // mesh tree tree-number
+        $this->addSql('ALTER TABLE mesh_tree CHANGE tree_number tree_number VARCHAR(31) NOT NULL COLLATE utf8_unicode_ci');
+
+        // mesh term uid
+        $this->addSql('ALTER TABLE mesh_term CHANGE mesh_term_uid mesh_term_uid VARCHAR(9) NOT NULL COLLATE utf8_unicode_ci');
+
+        // mesh concept uid
+        $this->addSql('ALTER TABLE mesh_concept_x_term DROP FOREIGN KEY FK_100AC50FE34D9FF5');
+        $this->addSql('ALTER TABLE mesh_concept_x_semantic_type DROP FOREIGN KEY FK_CD3D7893E34D9FF5');
+        $this->addSql('ALTER TABLE mesh_descriptor_x_concept DROP FOREIGN KEY FK_1AF85275E34D9FF5');
+        $this->addSql('ALTER TABLE mesh_concept MODIFY mesh_concept_uid VARCHAR(9) NOT NULL');
+        $this->addSql('ALTER TABLE mesh_concept_x_term CHANGE mesh_concept_uid mesh_concept_uid VARCHAR(9) NOT NULL COLLATE utf8_unicode_ci');
+        $this->addSql('ALTER TABLE mesh_concept_x_semantic_type CHANGE mesh_concept_uid mesh_concept_uid VARCHAR(9) NOT NULL COLLATE utf8_unicode_ci');
+        $this->addSql('ALTER TABLE mesh_descriptor_x_concept CHANGE mesh_concept_uid mesh_concept_uid VARCHAR(9) NOT NULL COLLATE utf8_unicode_ci');
+        $this->addSql('ALTER TABLE mesh_concept_x_term ADD CONSTRAINT FK_100AC50FE34D9FF5 FOREIGN KEY (mesh_concept_uid) REFERENCES mesh_concept (mesh_concept_uid)');
+        $this->addSql('ALTER TABLE mesh_concept_x_semantic_type ADD CONSTRAINT FK_CD3D7893E34D9FF5 FOREIGN KEY (mesh_concept_uid) REFERENCES mesh_concept (mesh_concept_uid)');
+        $this->addSql('ALTER TABLE mesh_descriptor_x_concept ADD CONSTRAINT FK_1AF85275E34D9FF5 FOREIGN KEY (mesh_concept_uid) REFERENCES mesh_concept (mesh_concept_uid)');
+
+        // mesh concept casn1
+        $this->addSql('ALTER TABLE mesh_concept CHANGE casn_1_name casn_1_name VARCHAR(127) DEFAULT NULL');
+
+        // mesh qualifier uid
+        $this->addSql('ALTER TABLE mesh_descriptor_x_qualifier CHANGE mesh_qualifier_uid mesh_qualifier_uid VARCHAR(9) NOT NULL COLLATE utf8_unicode_ci');
+        $this->addSql('ALTER TABLE mesh_qualifier CHANGE mesh_qualifier_uid mesh_qualifier_uid VARCHAR(9) NOT NULL COLLATE utf8_unicode_ci');
+
+        // mesh descriptor uid
         $this->addSql('ALTER TABLE course_learning_material_x_mesh CHANGE mesh_descriptor_uid mesh_descriptor_uid VARCHAR(9) NOT NULL COLLATE utf8_unicode_ci');
         $this->addSql('ALTER TABLE course_x_mesh CHANGE mesh_descriptor_uid mesh_descriptor_uid VARCHAR(9) NOT NULL COLLATE utf8_unicode_ci');
         $this->addSql('ALTER TABLE mesh_descriptor CHANGE mesh_descriptor_uid mesh_descriptor_uid VARCHAR(9) NOT NULL COLLATE utf8_unicode_ci');
