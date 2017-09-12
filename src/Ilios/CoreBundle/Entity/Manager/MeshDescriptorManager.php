727be4a3d2fc38dfd62a65059bdf5cb89ffc7235
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2017-09-11 19:37:37 -0700

    refactored term hashing and descriptor transmogrification out into testable component.

--- src/Ilios/CoreBundle/Entity/Manager/MeshDescriptorManager.php
+++ src/Ilios/CoreBundle/Entity/Manager/MeshDescriptorManager.php
@@ -2,6 +2,8 @@
 
 namespace Ilios\CoreBundle\Entity\Manager;
 
+use Doctrine\Bundle\DoctrineBundle\Registry;
+use Ilios\CoreBundle\Classes\MeshDescriptorSetTransmogrifier;
 use Ilios\CoreBundle\Entity\MeshDescriptorInterface;
 use Ilios\CoreBundle\Entity\Repository\MeshDescriptorRepository;
 use Ilios\MeSH\Model\DescriptorSet;
@@ -11,6 +13,21 @@ use Ilios\MeSH\Model\DescriptorSet;
  */
 class MeshDescriptorManager extends BaseManager
 {
+    /**
+     * @var MeshDescriptorSetTransmogrifier $transmogrifier
+     */
+    protected $transmogrifier;
+
+    /**
+     * @param Registry $registry
+     * @param string $class
+     * @param MeshDescriptorSetTransmogrifier $transmogrifier
+     */
+    public function __construct(Registry $registry, $class, MeshDescriptorSetTransmogrifier $transmogrifier)
+    {
+        $this->transmogrifier = $transmogrifier;
+        parent::__construct($registry, $class);
+    }
     /**
      * @param string $q
      * @param array $orderBy
@@ -101,7 +118,7 @@ class MeshDescriptorManager extends BaseManager
      */
     public function upsertMeshUniverse(DescriptorSet $descriptorSet, array $existingDescriptorIds)
     {
-        $data = $this->transmogrifyMeSHDataForImport($descriptorSet, $existingDescriptorIds);
+        $data = $this->transmogrifier->transmogrify($descriptorSet, $existingDescriptorIds);
         $this->getRepository()->upsertMeshUniverse($data);
     }
 
@@ -112,84 +129,5 @@ class MeshDescriptorManager extends BaseManager
     public function flagDescriptorsAsDeleted(array $meshDescriptors)
     {
         $this->getRepository()->flagDescriptorsAsDeleted($meshDescriptors);
-    }
-
-    /**
-     * @param DescriptorSet $descriptors
-     * @param string[] $existingDescriptorIds
-     * @return array
-     */
-    private function transmogrifyMeSHDataForImport(DescriptorSet $descriptors, array $existingDescriptorIds)
-    {
-        $rhett = [
-            'insert' => [
-                'mesh_concept' => [],
-                'mesh_concept_x_term' => [],
-                'mesh_descriptor' => [],
-                'mesh_descriptor_x_concept' => [],
-                'mesh_descriptor_x_qualifier' => [],
-                'mesh_qualifier' => [],
-                'mesh_previous_indexing' => [],
-                'mesh_term' => [],
-                'mesh_tree' => [],
-            ],
-            'update' => [
-                'mesh_descriptor' => [],
-            ],
-        ];
-
-        foreach ($descriptors->getDescriptors() as $descriptor) {
-            if (in_array($descriptor->getUi(), $existingDescriptorIds)) {
-                $rhett['update']['mesh_descriptor'][$descriptor->getUi()] = $descriptor;
-            } else {
-                $rhett['insert']['mesh_descriptor'][$descriptor->getUi()] = $descriptor;
-            }
-            foreach ($descriptor->getConcepts() as $concept) {
-                $rhett['insert']['mesh_concept'][$concept->getUi()] = $concept;
-                $rhett['insert']['mesh_descriptor_x_concept'][] = [$descriptor->getUi(), $concept->getUi()];
-                foreach ($concept->getTerms() as $term) {
-                    // ACHTUNG MINEN!
-                    // Unlike all other MeSH data points, terms do *not* possess unique UID.
-                    // Generate a unique pseudo-key by hashing all relevant term properties,
-                    // Use this hash instead of UID to keep track of term relationships
-                    // and all relevant term permutations.
-                    // [ST 2017/09/07]
-                    $hash = md5(
-                        implode(
-                            ',',
-                            [
-                                $term->getUi(),
-                                $term->getName(),
-                                $term->getLexicalTag(),
-                                $term->isConceptPreferred(),
-                                $term->isRecordPreferred(),
-                                $term->isPermuted(),
-                            ]
-                        )
-                    );
-                    $rhett['insert']['mesh_term'][$hash] = $term;
-                    $rhett['insert']['mesh_concept_x_term'][] = [$concept->getUi(), $hash];
-                }
-            }
-            $rhett['insert']['mesh_tree'][$descriptor->getUi()] = $descriptor->getTreeNumbers();
-            $prevIndexing = $descriptor->getPreviousIndexing();
-            if (!empty($prevIndexing)) {
-                // KNOWN ISSUE
-                // despite the one-to-many relationship of descriptors to their previous indexing records,
-                // we currently treat this relationship as a one-to-one, only taking
-                // the last previous indexing record into account.
-                // @todo revisit and fix. [ST 2017/09/08]
-                $rhett['insert']['mesh_previous_indexing'][$descriptor->getUi()] = array_reverse($prevIndexing)[0];
-            }
-            foreach ($descriptor->getAllowableQualifiers() as $qualifier) {
-                $rhett['insert']['mesh_qualifier'][$qualifier->getQualifierReference()->getUi()] = $qualifier;
-                $rhett['insert']['mesh_descriptor_x_qualifier'][] = [
-                    $descriptor->getUi(),
-                    $qualifier->getQualifierReference()->getUi(),
-                ];
-            }
-        }
-
-        return $rhett;
     }
 }
