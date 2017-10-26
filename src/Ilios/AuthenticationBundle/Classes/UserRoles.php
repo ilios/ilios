a7ce3abdded8b3b3068f145b1b3188a15cd74f78
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2017-10-25 18:10:54 -0700

    declared user roles in interface as constants.

---
+++ src/Ilios/AuthenticationBundle/Classes/UserRoles.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Ilios\AuthenticationBundle\Classes;
+
+/**
+ * Constant interface, defines all user roles in Ilios.
+ * Interface UserRoleInterface
+ * @package Ilios\AuthenticationBundle\Classes
+ */
+interface UserRoles
+{
+    /** @var string */
+    const COURSE_DIRECTOR = 'courseDirector';
+    /** @var string */
+    const COURSE_ADMINISTRATOR = 'courseAdministrator';
+    /** @var string */
+    const COURSE_INSTRUCTOR = 'courseInstructor';
+    /** @var string */
+    const SESSION_INSTRUCTOR = 'sessionInstructor';
+    /** @var string */
+    const SCHOOL_ADMINISTRATOR = 'schoolAdministrator';
+    /** @var string */
+    const SCHOOL_DIRECTOR = 'schoolDirector';
+}
