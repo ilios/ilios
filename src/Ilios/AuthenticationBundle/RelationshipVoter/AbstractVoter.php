dd76a5f28262212fc3d6c8e0589a804ff042407e
Jonathan Johnson <jon.johnson@ucsf.edu>
2017-10-31 13:05:37 -0700

    Move checks out of SessionUser and into PermissionChecker
    This avoids polluting SessionUser with unnecessary responsibilities and keeps it
    isolated to just containing the current users session.

--- src/Ilios/AuthenticationBundle/RelationshipVoter/AbstractVoter.php
+++ src/Ilios/AuthenticationBundle/RelationshipVoter/AbstractVoter.php
@@ -2,6 +2,7 @@
 
 namespace Ilios\AuthenticationBundle\RelationshipVoter;
 
+use Ilios\AuthenticationBundle\Service\PermissionChecker;
 use Symfony\Component\Security\Core\Authorization\Voter\Voter as SymfonyVoter;
 
 abstract class AbstractVoter extends SymfonyVoter
@@ -25,4 +26,14 @@ abstract class AbstractVoter extends SymfonyVoter
      * @var string
      */
     const CREATE = 'create';
+
+    /**
+     * @var PermissionChecker
+     */
+    protected $permissionChecker;
+
+    public function __construct(PermissionChecker $permissionChecker)
+    {
+        $this->permissionChecker = $permissionChecker;
+    }
 }
