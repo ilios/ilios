c2f3799101b4e65bfc87171af80ec9ebf561670b
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2017-11-20 10:59:40 -0800

    sorted constants alphabetically.

--- src/Ilios/AuthenticationBundle/Classes/UserRoles.php
+++ src/Ilios/AuthenticationBundle/Classes/UserRoles.php
@@ -10,17 +10,18 @@ namespace Ilios\AuthenticationBundle\Classes;
 interface UserRoles
 {
     /** @var string */
-    const COURSE_DIRECTOR = 'courseDirector';
-    /** @var string */
     const COURSE_ADMINISTRATOR = 'courseAdministrator';
     /** @var string */
+    const COURSE_DIRECTOR = 'courseDirector';
+    /** @var string */
     const COURSE_INSTRUCTOR = 'courseInstructor';
-    /** @var string */
-    const SESSION_ADMINISTRATOR = 'sessionAdministrator';
-    /** @var string */
-    const SESSION_INSTRUCTOR = 'sessionInstructor';
     /** @var string */
     const SCHOOL_ADMINISTRATOR = 'schoolAdministrator';
     /** @var string */
     const SCHOOL_DIRECTOR = 'schoolDirector';
+    /** @var string */
+    const SESSION_ADMINISTRATOR = 'sessionAdministrator';
+    /** @var string */
+    const SESSION_INSTRUCTOR = 'sessionInstructor';
+
 }
