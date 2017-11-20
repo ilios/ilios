d5da9e70fe53ec8de4a4c7313fdcc6d41f9cc582
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2017-11-20 11:03:03 -0800

    defined new roles for program and program-year directors.

--- src/Ilios/AuthenticationBundle/Classes/UserRoles.php
+++ src/Ilios/AuthenticationBundle/Classes/UserRoles.php
@@ -16,6 +16,10 @@ interface UserRoles
     /** @var string */
     const COURSE_INSTRUCTOR = 'courseInstructor';
     /** @var string */
+    const PROGRAM_DIRECTOR = 'programDirector';
+    /** @var string */
+    const PROGRAM_YEAR_DIRECTOR = 'programYearDirector';
+    /** @var string */
     const SCHOOL_ADMINISTRATOR = 'schoolAdministrator';
     /** @var string */
     const SCHOOL_DIRECTOR = 'schoolDirector';
