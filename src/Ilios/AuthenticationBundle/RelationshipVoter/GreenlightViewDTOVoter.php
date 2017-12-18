7633fe28c79b121df7824ef546739d78dc6f2bcb
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2017-12-18 15:51:48 -0800

    new aggregated voters for dtos.

---
+++ src/Ilios/AuthenticationBundle/RelationshipVoter/GreenlightViewDTOVoter.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace Ilios\AuthenticationBundle\RelationshipVoter;
+
+use Ilios\AuthenticationBundle\Classes\SessionUserInterface;
+use Ilios\CoreBundle\Entity\DTO\AamcMethodDTO;
+use Ilios\CoreBundle\Entity\DTO\AamcPcrsDTO;
+use Ilios\CoreBundle\Entity\DTO\AamcResourceTypeDTO;
+use Ilios\CoreBundle\Entity\DTO\AssessmentOptionDTO;
+use Ilios\CoreBundle\Entity\DTO\CohortDTO;
+use Ilios\CoreBundle\Entity\DTO\CompetencyDTO;
+use Ilios\CoreBundle\Entity\DTO\CourseClerkshipTypeDTO;
+use Ilios\CoreBundle\Entity\DTO\CourseDTO;
+use Ilios\CoreBundle\Entity\DTO\CurriculumInventoryAcademicLevelDTO;
+use Ilios\CoreBundle\Entity\DTO\CurriculumInventoryInstitutionDTO;
+use Ilios\CoreBundle\Entity\DTO\CurriculumInventoryReportDTO;
+use Ilios\CoreBundle\Entity\DTO\CurriculumInventorySequenceBlockDTO;
+use Ilios\CoreBundle\Entity\DTO\CurriculumInventorySequenceDTO;
+use Ilios\CoreBundle\Entity\DTO\DepartmentDTO;
+use Ilios\CoreBundle\Entity\DTO\IlmSessionDTO;
+use Ilios\CoreBundle\Entity\DTO\InstructorGroupDTO;
+use Ilios\CoreBundle\Entity\DTO\MeshConceptDTO;
+use Ilios\CoreBundle\Entity\DTO\MeshDescriptorDTO;
+use Ilios\CoreBundle\Entity\DTO\MeshPreviousIndexingDTO;
+use Ilios\CoreBundle\Entity\DTO\MeshQualifierDTO;
+use Ilios\CoreBundle\Entity\DTO\MeshTermDTO;
+use Ilios\CoreBundle\Entity\DTO\MeshTreeDTO;
+use Ilios\CoreBundle\Entity\DTO\ProgramDTO;
+use Ilios\CoreBundle\Entity\DTO\ProgramYearDTO;
+use Ilios\CoreBundle\Entity\DTO\SchoolDTO;
+use Ilios\CoreBundle\Entity\DTO\SessionDescriptionDTO;
+use Ilios\CoreBundle\Entity\DTO\SessionDTO;
+use Ilios\CoreBundle\Entity\DTO\SessionTypeDTO;
+use Ilios\CoreBundle\Entity\DTO\TermDTO;
+use Ilios\CoreBundle\Entity\DTO\VocabularyDTO;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+
+/**
+ * Always grants VIEW permissions on all supported DTOs.
+ *
+ * @package Ilios\AuthenticationBundle\RelationshipVoter
+ */
+class GreenlightViewDTOVoter extends AbstractVoter
+{
+    protected function supports($attribute, $subject)
+    {
+        return (
+            array($attribute, [self::VIEW]) && (
+                $subject instanceof AamcMethodDTO
+                || $subject instanceof AamcPcrsDTO
+                || $subject instanceof AamcResourceTypeDTO
+                || $subject instanceof AssessmentOptionDTO
+                || $subject instanceof CohortDTO
+                || $subject instanceof CompetencyDTO
+                || $subject instanceof CourseDTO
+                || $subject instanceof CourseClerkshipTypeDTO
+                || $subject instanceof CurriculumInventoryAcademicLevelDTO
+                || $subject instanceof CurriculumInventoryInstitutionDTO
+                || $subject instanceof CurriculumInventoryReportDTO
+                || $subject instanceof CurriculumInventorySequenceDTO
+                || $subject instanceof CurriculumInventorySequenceBlockDTO
+                || $subject instanceof DepartmentDTO
+                || $subject instanceof IlmSessionDTO
+                || $subject instanceof InstructorGroupDTO
+                || $subject instanceof MeshConceptDTO
+                || $subject instanceof MeshDescriptorDTO
+                || $subject instanceof MeshPreviousIndexingDTO
+                || $subject instanceof MeshQualifierDTO
+                || $subject instanceof MeshTermDTO
+                || $subject instanceof MeshTreeDTO
+                || $subject instanceof ProgramDTO
+                || $subject instanceof ProgramYearDTO
+                || $subject instanceof SchoolDTO
+                || $subject instanceof SessionDTO
+                || $subject instanceof SessionDescriptionDTO
+                || $subject instanceof SessionTypeDTO
+                || $subject instanceof TermDTO
+                || $subject instanceof VocabularyDTO
+            )
+        );
+    }
+    protected function voteOnAttribute($attribute, $subject, TokenInterface $token)
+    {
+        $user = $token->getUser();
+        if (!$user instanceof SessionUserInterface) {
+            return false;
+        }
+
+        return true;
+    }
+}
---
+++ tests/AuthenticationBundle/RelationshipVoter/GreenlightViewDtoVoterTest.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace Tests\AuthenticationBundle\RelationshipVoter;
+
+use Ilios\AuthenticationBundle\RelationshipVoter\AbstractVoter;
+use Ilios\AuthenticationBundle\RelationshipVoter\GreenlightViewDTOVoter as Voter;
+use Ilios\AuthenticationBundle\Service\PermissionChecker;
+use Ilios\CoreBundle\Entity\DTO\AamcMethodDTO;
+use Ilios\CoreBundle\Entity\DTO\AamcPcrsDTO;
+use Ilios\CoreBundle\Entity\DTO\AamcResourceTypeDTO;
+use Ilios\CoreBundle\Entity\DTO\AssessmentOptionDTO;
+use Ilios\CoreBundle\Entity\DTO\CohortDTO;
+use Ilios\CoreBundle\Entity\DTO\CompetencyDTO;
+use Ilios\CoreBundle\Entity\DTO\CourseClerkshipTypeDTO;
+use Ilios\CoreBundle\Entity\DTO\CourseDTO;
+use Ilios\CoreBundle\Entity\DTO\CurriculumInventoryAcademicLevelDTO;
+use Ilios\CoreBundle\Entity\DTO\CurriculumInventoryInstitutionDTO;
+use Ilios\CoreBundle\Entity\DTO\CurriculumInventoryReportDTO;
+use Ilios\CoreBundle\Entity\DTO\CurriculumInventorySequenceBlockDTO;
+use Ilios\CoreBundle\Entity\DTO\CurriculumInventorySequenceDTO;
+use Ilios\CoreBundle\Entity\DTO\DepartmentDTO;
+use Ilios\CoreBundle\Entity\DTO\IlmSessionDTO;
+use Ilios\CoreBundle\Entity\DTO\InstructorGroupDTO;
+use Ilios\CoreBundle\Entity\DTO\MeshConceptDTO;
+use Ilios\CoreBundle\Entity\DTO\MeshDescriptorDTO;
+use Ilios\CoreBundle\Entity\DTO\MeshPreviousIndexingDTO;
+use Ilios\CoreBundle\Entity\DTO\MeshQualifierDTO;
+use Ilios\CoreBundle\Entity\DTO\MeshTermDTO;
+use Ilios\CoreBundle\Entity\DTO\MeshTreeDTO;
+use Ilios\CoreBundle\Entity\DTO\ProgramDTO;
+use Ilios\CoreBundle\Entity\DTO\ProgramYearDTO;
+use Ilios\CoreBundle\Entity\DTO\SchoolDTO;
+use Ilios\CoreBundle\Entity\DTO\SessionDescriptionDTO;
+use Ilios\CoreBundle\Entity\DTO\SessionDTO;
+use Ilios\CoreBundle\Entity\DTO\SessionTypeDTO;
+use Ilios\CoreBundle\Entity\DTO\TermDTO;
+use Ilios\CoreBundle\Entity\DTO\VocabularyDTO;
+use Mockery as m;
+use Symfony\Component\Security\Core\Authorization\Voter\VoterInterface;
+
+/**
+ * Class GreenlightViewDtoVoterTest
+ * @package Tests\AuthenticationBundle\RelationshipVoter
+ */
+class GreenlightViewDtoVoterTest extends AbstractBase
+{
+    /**
+     * @inheritdoc
+     */
+    public function setup()
+    {
+        $this->permissionChecker = m::mock(PermissionChecker::class);
+        $this->voter = new Voter($this->permissionChecker);
+    }
+
+    public function canViewDTOProvider()
+    {
+        return [
+            [AamcMethodDTO::class],
+            [AamcPcrsDTO::class],
+            [AamcResourceTypeDTO::class],
+            [AssessmentOptionDTO::class],
+            [CohortDTO::class],
+            [CompetencyDTO::class],
+            [CourseDTO::class],
+            [CourseClerkshipTypeDTO::class],
+            [CurriculumInventoryAcademicLevelDTO::class],
+            [CurriculumInventoryInstitutionDTO::class],
+            [CurriculumInventoryReportDTO::class],
+            [CurriculumInventorySequenceDTO::class],
+            [CurriculumInventorySequenceBlockDTO::class],
+            [DepartmentDTO::class],
+            [IlmSessionDTO::class],
+            [InstructorGroupDTO::class],
+            [MeshConceptDTO::class],
+            [MeshDescriptorDTO::class],
+            [MeshPreviousIndexingDTO::class],
+            [MeshQualifierDTO::class],
+            [MeshTermDTO::class],
+            [MeshTreeDTO::class],
+            [ProgramDTO::class],
+            [ProgramYearDTO::class],
+            [SchoolDTO::class],
+            [SessionDTO::class],
+            [SessionDescriptionDTO::class],
+            [SessionTypeDTO::class],
+            [TermDTO::class],
+            [VocabularyDTO::class],
+        ];
+    }
+
+    /**
+     * @dataProvider canViewDTOProvider
+     * @covers GreenlightViewDTOVoter::voteOnAttribute()
+     */
+    public function testCanViewDTO($class)
+    {
+        $token = $this->createMockTokenWithNonRootSessionUser();
+        $dto = m::mock($class);
+        $response = $this->voter->vote($token, $dto, [AbstractVoter::VIEW]);
+        $this->assertEquals(VoterInterface::ACCESS_GRANTED, $response, "DTO View allowed");
+    }
+}
+
