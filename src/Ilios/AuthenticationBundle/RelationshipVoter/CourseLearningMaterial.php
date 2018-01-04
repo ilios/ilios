9084ead2fe74b257fde6954c867d829cd6c7930c
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2018-01-04 14:31:45 -0800

    new perms and voters for learning materials and associates.

---
+++ src/Ilios/AuthenticationBundle/RelationshipVoter/CourseLearningMaterial.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Ilios\AuthenticationBundle\RelationshipVoter;
+
+use Ilios\AuthenticationBundle\Classes\SessionUserInterface;
+use Ilios\CoreBundle\Entity\CourseLearningMaterialInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+
+class CourseLearningMaterial extends AbstractVoter
+{
+    protected function supports($attribute, $subject)
+    {
+        return $subject instanceof CourseLearningMaterialInterface
+            && in_array(
+                $attribute,
+                [self::CREATE, self::VIEW, self::EDIT, self::DELETE]
+            );
+    }
+
+    protected function voteOnAttribute($attribute, $subject, TokenInterface $token)
+    {
+        $user = $token->getUser();
+        if (!$user instanceof SessionUserInterface) {
+            return false;
+        }
+        if ($user->isRoot()) {
+            return true;
+        }
+
+        switch ($attribute) {
+            case self::VIEW:
+                return true;
+                break;
+            case self::EDIT:
+            case self::CREATE:
+            case self::DELETE:
+                return $this->permissionChecker->canUpdateCourse(
+                    $user,
+                    $subject->getCourse()->getId(),
+                    $subject->getCourse()->getSchool()->getId()
+                );
+                break;
+        }
+
+        return false;
+    }
+}
