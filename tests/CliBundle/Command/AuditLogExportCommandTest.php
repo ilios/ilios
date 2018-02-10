8d78232d9b599d3e941fbb4fac2e9e356f3891c5
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2018-02-09 16:59:59 -0800

    inject dependencies instead of pulling them from the ci container.

--- tests/CliBundle/Command/AuditLogExportCommandTest.php
+++ tests/CliBundle/Command/AuditLogExportCommandTest.php
@@ -3,10 +3,12 @@
 namespace Tests\CliBundle\Command;
 
 use Ilios\CliBundle\Command\AuditLogExportCommand;
-use Liip\FunctionalTestBundle\Test\WebTestCase;
-use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Ilios\CoreBundle\Entity\Manager\AuditLogManager;
+use Mockery as m;
+use PHPUnit\Framework\TestCase;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Tester\CommandTester;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Class AuditLogExportCommandTest
@@ -15,30 +17,28 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  * @link http://symfony.com/doc/current/cookbook/console/console_command.html#testing-commands
  * @link http://www.ardianys.com/2013/04/symfony2-test-console-command-which-use.html
  */
-class AuditLogExportCommandTest extends WebTestCase
+class AuditLogExportCommandTest extends TestCase
 {
-    /**
-     * @var ContainerInterface
-     */
-    protected $container;
+    use m\Adapter\Phpunit\MockeryPHPUnitIntegration;
 
     /**
      * @var CommandTester
      */
     protected $commandTester;
 
+    protected $logger;
+
+    protected $auditLogManager;
+
     /**
      * {@inheritdoc}
      */
     public function setUp()
     {
-        $this->container = static::createClient()->getContainer();
-        $this->loadFixtures([
-            'Tests\CliBundle\Fixture\LoadAuditLogData',
-        ]);
-
-        $application = new Application(static::$kernel);
-        $command = new AuditLogExportCommand();
+        $this->logger = m::mock(LoggerInterface::class);
+        $this->auditLogManager = m::mock(AuditLogManager::class);
+        $application = new Application();
+        $command = new AuditLogExportCommand($this->logger, $this->auditLogManager);
         $application->add($command);
 
         $command = $application->find('ilios:maintenance:export-audit-log');
@@ -50,13 +50,20 @@ class AuditLogExportCommandTest extends WebTestCase
      */
     public function testExecuteWithDefaultRange()
     {
+        $this->auditLogManager
+            ->shouldReceive('findInRange')
+            ->withArgs(function (\DateTime $from, \DateTime $to) {
+                $format = 'Y-m-d H:i:s';
+                $midnightYesterday = new \DateTime('midnight yesterday', new \DateTimeZone('UTC'));
+                $midnightToday = new \DateTime('midnight today', new \DateTimeZone('UTC'));
+                return $from->format($format)  ===  $midnightYesterday->format($format)
+                    && $to->format($format)  ===  $midnightToday->format($format);
+            })
+            ->andReturn([]);
+
+        $this->logger->shouldReceive('info');
 
         $this->commandTester->execute([]);
-
-        $output = $this->commandTester->getDisplay();
-        $this->assertContains('YesterdaysEvent', $output);
-        $this->assertContains('TodaysEvent', $output);
-        $this->assertNotContains('LastYearsEvent', $output);
     }
 
     /**
@@ -64,51 +71,124 @@ class AuditLogExportCommandTest extends WebTestCase
      */
     public function testExecuteWithCustomRange()
     {
+        $this->auditLogManager
+            ->shouldReceive('findInRange')
+            ->withArgs(function (\DateTime $from, \DateTime $to) {
+                $format = 'Y-m-d';
+                $twoYearsAgo = new \DateTime('2 years ago', new \DateTimeZone('UTC'));
+                $twoDaysAgo = new \DateTime('2 days ago', new \DateTimeZone('UTC'));
+                return $from->format($format)  ===  $twoYearsAgo->format($format)
+                    && $to->format($format)  ===  $twoDaysAgo->format($format);
+            })
+            ->andReturn([]);
+
+        $this->logger->shouldReceive('info');
+
         $this->commandTester->execute([
             'from' => '2 years ago',
             'to' => '2 days ago',
         ]);
-
-        $output = $this->commandTester->getDisplay();
-        $this->assertNotContains('TodaysEvent', $output);
-        $this->assertNotContains('YesterdaysEvent', $output);
-        $this->assertContains('LastYearsEvent', $output);
     }
 
     /**
      * @covers \Ilios\CliBundle\Command\InstallFirstUserCommand::execute
      */
+    public function testExecuteCheckTableOutput()
+    {
+        $now = new \DateTime();
+
+        $this->auditLogManager
+            ->shouldReceive('findInRange')
+            ->andReturn([
+                [
+                    'id' => '1',
+                    'userId' => '10',
+                    'action' => 'update',
+                    'createdAt' => $now,
+                    'objectId' => '20',
+                    'valuesChanged' => 'phone',
+                    'objectClass' => 'FooBar',
+                ]
+            ]);
+
+        $this->logger->shouldReceive('info');
+        $this->commandTester->execute([]);
+
+        $output = $this->commandTester->getDisplay();
+        $cleanNow = preg_quote($now->format('c'));
+        $this->assertRegExp(
+            "/1\s+\|\s+10\s+\|\s+update\s+\|\s+{$cleanNow}\s+\|\s+20\s+\|\s+FooBar\s+\|\s+phone/",
+            $output
+        );
+    }
+
+    /**
+     * @covers \Ilios\CliBundle\Command\InstallFirstUserCommand::execute
+     */
+    public function testExecuteCheckLogging()
+    {
+        $from = (new \DateTime('midnight yesterday', new \DateTimeZone('UTC')))->format('c');
+        $to = (new \DateTime('midnight today', new \DateTimeZone('UTC')))->format('c');
+
+        $this->auditLogManager
+            ->shouldReceive('findInRange')
+            ->andReturn([
+                [
+                    'id' => '1',
+                    'userId' => '10',
+                    'action' => 'update',
+                    'createdAt' => new \DateTime(),
+                    'objectId' => '20',
+                    'valuesChanged' => 'phone',
+                    'objectClass' => 'FooBar',
+                ],
+                [
+                    'id' => '2',
+                    'userId' => null,
+                    'action' => 'insert',
+                    'createdAt' => new \DateTime(),
+                    'objectId' => '21',
+                    'valuesChanged' => 'email',
+                    'objectClass' => 'Baz',
+                ]
+            ]);
+        $this->auditLogManager->shouldReceive('deleteInRange')->once();
+
+        $this->logger->shouldReceive('info')->with('Starting Audit Log Export.')->once();
+        $this->logger->shouldReceive('info')->with(
+            "Exporting 2 audit log entries which were created between {$from} and {$to}."
+        )->once();
+        $this->logger->shouldReceive('info')->with(
+            "Deleting all audit log entries that were created between {$from} and {$to}."
+        )->once();
+        $this->logger->shouldReceive('info')->with('Finished Audit Log Export.')->once();
+
+        $this->commandTester->execute(['--delete' => 'true']);
+    }
+
+    /**
+     * @covers \Ilios\CliBundle\Command\InstallFirstUserCommand::execute
+     */
+
     public function testExecuteWithDeletion()
     {
-        // set the delete flag
-        $this->commandTester->execute([
-            'from' => '2 days ago',
-            'to' => 'now',
-            '--delete' => true,
-        ]);
+        $midnightYesterday = new \DateTime('midnight yesterday', new \DateTimeZone('UTC'));
+        $midnightToday = new \DateTime('midnight today', new \DateTimeZone('UTC'));
 
-        $output = $this->commandTester->getDisplay();
-        $this->assertContains('YesterdaysEvent', $output);
-        $this->assertContains('TodaysEvent', $output);
+        $this->auditLogManager
+            ->shouldReceive('findInRange')
+            ->andReturn([]);
 
-        // re-run date range - they should be gone now.
-        $this->commandTester->execute([
-            'from' => '2 days ago',
-            'to' => 'now',
-        ]);
+        $this->logger->shouldReceive('info');
 
-        $output = $this->commandTester->getDisplay();
-        $this->assertNotContains('YesterdaysEvent', $output);
-        $this->assertNotContains('TodaysEvent', $output);
+        $this->auditLogManager
+        ->shouldReceive('deleteInRange')
+        ->withArgs(function (\DateTime $from, \DateTime $to) use ($midnightYesterday, $midnightToday) {
+            $format = 'Y-m-d H:i:s';
+            return $from->format($format)  ===  $midnightYesterday->format($format)
+                && $to->format($format)  ===  $midnightToday->format($format);
+        })->once();
 
-        // last year's event should still be there tho.
-        // running this just to verify that no overreach in deletion occurred.
-        $this->commandTester->execute([
-            'from' => '2 years ago',
-            'to' => 'now',
-        ]);
-
-        $output = $this->commandTester->getDisplay();
-        $this->assertContains('LastYearsEvent', $output);
+        $this->commandTester->execute(['--delete' => 'true']);
     }
 }
