a541e302ee0ce2e93fc88ca7be4cb6842ba58990
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2018-03-07 09:44:12 -0800

    increased column size on mesh_tree::tree_number.

---
+++ app/Resources/DoctrineMigrations/Version20180307174249.php
@@ -0,0 +1,32 @@
+<?php declare(strict_types = 1);
+
+namespace Ilios\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Increase the column size on mesh_tree::tree_number.
+ */
+class Version20180307174249 extends AbstractMigration
+{
+    /**
+     * @inheritdoc
+     */
+    public function up(Schema $schema)
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE mesh_tree CHANGE tree_number tree_number VARCHAR(80) NOT NULL');
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function down(Schema $schema)
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE mesh_tree CHANGE tree_number tree_number VARCHAR(50) NOT NULL COLLATE utf8_unicode_ci');
+    }
+}
