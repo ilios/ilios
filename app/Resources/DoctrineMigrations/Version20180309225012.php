a28c00a6c90007449ba6f95099885b564c213724
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2018-03-09 14:52:28 -0800

    migration for activating all vocabs and competencies.

---
+++ app/Resources/DoctrineMigrations/Version20180309225012.php
@@ -0,0 +1,32 @@
+<?php declare(strict_types = 1);
+
+namespace Ilios\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Activate all competencies, vocabularies and vocabulary terms.
+ */
+class Version20180309225012 extends AbstractMigration
+{
+    /**
+     * @inheritdoc
+     */
+    public function up(Schema $schema)
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->addSql('UPDATE vocabulary set active=true');
+        $this->addSql('UPDATE competency set active=true');
+        $this->addSql('UPDATE term set active=true');
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function down(Schema $schema)
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->throwIrreversibleMigrationException();
+    }
+}
