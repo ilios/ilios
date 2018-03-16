8ceb49e22304d8f99d329cfcda37c6b87807d799
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2018-03-16 11:22:10 -0700

    thou shall not pass!

--- src/Ilios/ApiBundle/Controller/MeshConceptController.php
+++ src/Ilios/ApiBundle/Controller/MeshConceptController.php
@@ -2,9 +2,7 @@
 
 namespace Ilios\ApiBundle\Controller;
 
-use Ilios\CoreBundle\Entity\Manager\MeshDescriptorManager;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\GoneHttpException;
 
 /**
@@ -14,8 +12,33 @@ use Symfony\Component\HttpKernel\Exception\GoneHttpException;
 class MeshConceptController extends ApiController
 {
     /**
+     * @inheritdoc
+     */
+    public function postAction($version, $object, Request $request)
+    {
+        $this->fourTenAction();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function putAction($version, $object, $id, Request $request)
+    {
+        $this->fourTenAction();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function deleteAction($version, $object, $id)
+    {
+        $this->fourTenAction();
+    }
+
+    /**
      * Generic action used by the router to send a 410 GONE
      * to anyone trying to POST, PUT or DELETE a MeSH Concept
+     * @throws GoneHttpException
      */
     public function fourTenAction()
     {
--- src/Ilios/ApiBundle/Controller/MeshDescriptorController.php
+++ src/Ilios/ApiBundle/Controller/MeshDescriptorController.php
@@ -41,6 +41,30 @@ class MeshDescriptorController extends ApiController
     }
 
     /**
+     * @inheritdoc
+     */
+    public function postAction($version, $object, Request $request)
+    {
+        $this->fourTenAction();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function putAction($version, $object, $id, Request $request)
+    {
+        $this->fourTenAction();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function deleteAction($version, $object, $id)
+    {
+        $this->fourTenAction();
+    }
+
+    /**
      * Generic action used by the router to send a 410 GONE
      * to anyone trying to POST, PUT or DELETE a MeSH Descriptor
      */
