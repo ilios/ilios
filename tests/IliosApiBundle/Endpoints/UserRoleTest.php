a39b964ae7cb18d4abfcadb6e48b652424c300a4
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2018-03-23 14:34:48 -0700

    unborked userrole endpoint test afer botched rebase.

--- tests/IliosApiBundle/Endpoints/UserRoleTest.php
+++ tests/IliosApiBundle/Endpoints/UserRoleTest.php
@@ -34,31 +34,4 @@ class UserRoleTest extends ReadEndpointTest
             'title' => [[1], ['title' => 'Something Else']],
         ];
     }
-<<<<<<< 39e06c1d87472f4a7e133dabfc5d378dbc11c825
-=======
-
-    /**
-     * We can't change the title on the Developer role
-     * Doing that leaves us without the permissions to change the title
-     * @inheritdoc
-     * @dataProvider putsToTest
-     */
-    public function testPut($key, $value, $skipped = false)
-    {
-        if ($skipped) {
-            $this->markTestSkipped();
-        }
-
-        $dataLoader = $this->getDataLoader();
-        $data = $dataLoader->getOne();
-
-        //extract the ID before changing anything in case
-        // the key we are changing is the ID
-        $id = $data['id'];
-        $data[$key] = $value;
-
-        $postData = $data;
-        $this->putTest($data, $postData, $id);
-    }
->>>>>>> WIP: bolshoi refactoring of API endpoints tests.
 }
