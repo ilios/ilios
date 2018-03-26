d3a3cb0ea6c20402ace4555dfc6cc6f17c2b484b
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2018-03-26 14:52:12 -0700

    rm old/broken test.

--- tests/AuthenticationBundle/Classes/SessionUserTest.php
+++ tests/AuthenticationBundle/Classes/SessionUserTest.php
@@ -18,8 +18,6 @@ class SessionUserTest extends TestCase
 {
     use m\Adapter\Phpunit\MockeryPHPUnitIntegration;
 
-    protected $relationships;
-
     protected $iliosUser;
 
     protected $userManager;
@@ -31,28 +29,6 @@ class SessionUserTest extends TestCase
     {
         $this->userManager = m::mock(UserManager::class);
         $this->iliosUser = m::mock(UserInterface::class);
-
-        $this->relationships = [
-            'nonStudentSchoolIds' => [],
-            'directedCourseIds' => [],
-            'administeredCourseIds' => [],
-            'directedSchoolIds' => [],
-            'administeredSchoolIds' => [],
-            'directedCourseSchoolIds' => [],
-            'administeredCourseSchoolIds' => [],
-            'administeredSessionSchoolIds' => [],
-            'administeredSessionCourseIds' => [],
-            'taughtCourseIds' => [],
-            'taughtCourseSchoolIds' => [],
-            'administeredSessionIds' => [],
-            'instructedSessionIds' => [],
-            'directedProgramIds' => [],
-            'directedProgramYearIds' => [],
-            'directedProgramYearProgramIds' => [],
-            'directedCohortIds' => [],
-            'administeredCurriculumInventoryReportIds' => [],
-            'administeredCurriculumInventoryReportSchoolIds' => [],
-        ];
 
         $school = m::mock(School::class);
         $school->shouldReceive('getId')->andReturn(1);
@@ -69,40 +45,8 @@ class SessionUserTest extends TestCase
      */
     public function tearDown()
     {
-        unset($this->relationships);
         unset($this->iliosUser);
         unset($this->userManager);
         unset($this->config);
-    }
-    /**
-     * @return array
-     */
-    public function performsNonLearnerFunctionProvider()
-    {
-        return [
-            [[], false],
-            [['directedCourseIds' => [1]], true],
-            [['administeredCourseIds' => [1]], true],
-            [['administeredSchoolIds' => [1]], true],
-            [['taughtCourseIds' => [1]], true],
-            [['administeredSessionIds' => [1]], true],
-            [['instructedSessionIds' => [1]], true],
-            [['directedProgramIds' => [1]], true],
-            [['directedProgramYearIds' => [1]], true],
-            [['directedCohortIds' => [1]], true],
-            [['administeredCurriculumInventoryReportIds' => [1]], true],
-        ];
-    }
-
-    /**
-     * @dataProvider performsNonLearnerFunctionProvider
-     * @covers \Ilios\AuthenticationBundle\Classes\SessionUser::performsNonLearnerFunction
-     */
-    public function testPerformsNonLearnerFunction($modifiedRelationships, $expectedResult)
-    {
-        $relationships = array_merge($this->relationships, $modifiedRelationships);
-        $this->userManager->shouldReceive('buildSessionRelationships')->andReturn($relationships);
-        $sessionUser = new SessionUser($this->iliosUser, $this->userManager);
-        $this->assertEquals($expectedResult, $sessionUser->performsNonLearnerFunction());
     }
 }
