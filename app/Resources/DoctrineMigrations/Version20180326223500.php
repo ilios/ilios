c0f0ea078e9df3c6be1809c41846ef0596ff3c0e
Jonathan Johnson <jon.johnson@ucsf.edu>
2018-03-26 15:36:37 -0700

    Convert audit_log objectId to be a varchar instead of an integer
    This allows entities with a string ID like AAMC Resource Type to be
    logged.

---
+++ app/Resources/DoctrineMigrations/Version20180326223500.php
@@ -0,0 +1,26 @@
+<?php declare(strict_types = 1);
+
+namespace Ilios\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Convert audit_log objectId to be a varchar instead of an integer
+ */
+class Version20180326223500 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE audit_log CHANGE objectId objectId VARCHAR(255) NOT NULL');
+    }
+
+    public function down(Schema $schema)
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE audit_log CHANGE objectId objectId INT NOT NULL');
+    }
+}
