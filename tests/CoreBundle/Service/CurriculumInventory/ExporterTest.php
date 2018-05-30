8ebc824a2ca975ae66dabeed3021c445be4e29a3
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2018-05-30 16:04:04 -0700

    updated test coverage.

--- tests/CoreBundle/Service/CurriculumInventory/ExporterTest.php
+++ tests/CoreBundle/Service/CurriculumInventory/ExporterTest.php
@@ -1,10 +1,9 @@
 <?php
 namespace Tests\CoreBundle\Service\CurriculumInventory;
 
-use Ilios\CoreBundle\Entity\Manager\CurriculumInventoryInstitutionManager;
-use Ilios\CoreBundle\Service\Config;
+use Ilios\CoreBundle\Service\CurriculumInventory\Export\Aggregator;
+use Ilios\CoreBundle\Service\CurriculumInventory\Export\XmlPrinter;
 use Ilios\CoreBundle\Service\CurriculumInventory\Exporter;
-use Ilios\CoreBundle\Entity\Manager\CurriculumInventoryReportManager;
 use Symfony\Bundle\FrameworkBundle\Tests\TestCase;
 
 use Mockery as m;
@@ -21,13 +20,9 @@ class ExporterTest extends TestCase
      */
     public function testConstructor()
     {
-        $reportManager = m::mock(CurriculumInventoryReportManager::class);
-        $institutionManager = m::mock(CurriculumInventoryInstitutionManager::class);
-        $config = m::mock(Config::class);
-        $config->shouldReceive('get')->once()->with('institution_domain')->andReturn('');
-        $config->shouldReceive('get')->once()->with('supporting_link')->andReturn('');
-
-        $exporter = new Exporter($reportManager, $institutionManager, $config);
+        $aggregator = m::mock(Aggregator::class);
+        $printer = m::mock(XmlPrinter::class);
+        $exporter = new Exporter($aggregator, $printer);
 
         $this->assertTrue($exporter instanceof Exporter);
     }
