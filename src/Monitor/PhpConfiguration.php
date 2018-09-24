b44dda52b5f678e1ef40de4665d4469287aca9a7
Jonathan Johnson <jon.johnson@ucsf.edu>
2018-09-24 01:59:01 -0700

    Add check for some PHP performance configuration options
    These are recommended by symfony, but not checked by their requirements
    checker.

---
+++ src/Monitor/PhpConfiguration.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Monitor;
+
+use ZendDiagnostics\Check\CheckInterface;
+use ZendDiagnostics\Result\Success;
+use ZendDiagnostics\Result\Warning;
+
+class PhpConfiguration implements CheckInterface
+{
+    /**
+     * @inheritdoc
+     */
+    public function check()
+    {
+        $gtOptions = [
+            'opcache.memory_consumption' => 256,
+            'opcache.max_accelerated_files' => 20000,
+            'realpath_cache_ttl' => 600,
+        ];
+
+        foreach ($gtOptions as $option => $required) {
+            $value = (int) ini_get($option);
+            if ($value < $required) {
+                return new Warning(
+                    "${option} setting is too low, should be at least ${required}"
+                );
+            }
+        }
+        $realPathCacheSizeConfig = ini_get('realpath_cache_size');
+        $value = $this->valueToBytes($realPathCacheSizeConfig);
+        if ($value < 4194304) {
+            return new Warning(
+                "realpath_cache_size setting is too low, should be at least 4096K"
+            );
+        }
+
+        return new Success('has been loaded');
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getLabel()
+    {
+        return 'PHP Configuration';
+    }
+
+    /**
+     * Can't believe there isn't a builtin for this.
+     * I stole this from https://stackoverflow.com/a/44767616/796999 thanks!
+     * @param string $value
+     * @return int
+     */
+    protected function valueToBytes(string $value): int
+    {
+        preg_match('/^(?<value>\d+)(?<option>[K|M|G]*)$/i', $value, $matches);
+
+        $value = (int) $matches['value'];
+        $option = strtoupper($matches['option']);
+
+        if ($option) {
+            if ($option === 'K') {
+                $value *= 1024;
+            } elseif ($option === 'M') {
+                $value *= 1024 * 1024;
+            } elseif ($option === 'G') {
+                $value *= 1024 * 1024 * 1024;
+            }
+        }
+
+        return $value;
+    }
+}
