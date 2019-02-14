3ca62c7476d4f3b06abf313ab4b71cbea6087f1d
Jonathan Johnson <jon.johnson@ucsf.edu>
2019-02-13 22:31:48 -0800

    When S3 is configured use it to store files

--- src/Service/FilesystemFactory.php
+++ src/Service/FilesystemFactory.php
@@ -2,6 +2,8 @@
 
 namespace App\Service;
 
+use Aws\S3\S3Client;
+use League\Flysystem\AwsS3v3\AwsS3Adapter;
 use League\Flysystem\Adapter\Local;
 use League\Flysystem\Filesystem as LeagueFilesystem;
 
@@ -20,8 +22,42 @@ class FilesystemFactory
 
     public function getFilesystem() : LeagueFilesystem
     {
+        $s3Url = $this->config->get('storage_s3_url');
+
+        if ($s3Url) {
+            $configuration = $this->parseS3URL($s3Url);
+            //extract bucket from configuration, it's not required here
+            $bucket = $configuration['bucket'];
+            unset($configuration['bucket']);
+
+            $client = new S3Client($configuration);
+            $adapter = new AwsS3Adapter($client, $bucket);
+            return new LeagueFilesystem($adapter);
+        }
         $path = $this->config->get('file_system_storage_path');
         $adapter = new Local($path);
         return new LeagueFilesystem($adapter, ['visibility' => 'private']);
+    }
+
+    protected function parseS3URL(string $url) : array
+    {
+        $result = preg_match(
+            '#^(s3)://([a-zA-Z0-9]+):([A-Za-z0-9/+=]{40})@([a-z0-9-]+.)\.([a-z0-9-]+)#',
+            $url,
+            $matches
+        );
+        if (!$result) {
+            throw new \Exception("Bad S3 URL, should be formatted as `s3://KEY:SECRET@bucket.region`");
+        }
+
+        return [
+            'credentials' => [
+                'key'    => $matches[2],
+                'secret' => $matches[3],
+            ],
+            'region' => $matches[5],
+            'version' => 'latest',
+            'bucket' => $matches[4]
+        ];
     }
 }
---
+++ tests/Service/FilesystemFactoryTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Tests\Service;
+
+use App\Service\Config;
+use App\Service\FilesystemFactory;
+use League\Flysystem\Adapter\Local;
+use League\Flysystem\Filesystem;
+use Mockery as m;
+use Symfony\Bundle\FrameworkBundle\Tests\TestCase;
+
+class FilesystemFactoryTest extends TestCase
+{
+    use m\Adapter\Phpunit\MockeryPHPUnitIntegration;
+
+    /** @var m\Mock */
+    private $config;
+
+    /** @var FilesystemFactory */
+    private $filesystemFactory;
+
+    public function setUp()
+    {
+        $this->config = m::mock(Config::class);
+        $this->filesystemFactory = new FilesystemFactory($this->config);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function tearDown()
+    {
+        unset($this->config);
+        unset($this->filesystemFactory);
+    }
+
+    public function testNoS3GivesLocal()
+    {
+        $this->config->shouldReceive('get')->with('storage_s3_url')->andReturn(null);
+        $this->config->shouldReceive('get')->with('file_system_storage_path')->andReturn('/tmp');
+        $result = $this->filesystemFactory->getFilesystem();
+        $this->assertInstanceOf(Filesystem::class, $result);
+        $this->assertInstanceOf(Local::class, $result->getAdapter());
+    }
+
+    public function testGetFilesystemFailsFromBadS3Url()
+    {
+        $this->expectException(\Exception::class);
+        $this->config->shouldReceive('get')->with('storage_s3_url')->andReturn('bad');
+        $this->filesystemFactory->getFilesystem();
+    }
+}
