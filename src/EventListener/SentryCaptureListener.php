8ecbb3b69802c43674c0373a6fd2a8b7c9c25b71
Jonathan Johnson <jon.johnson@ucsf.edu>
2019-02-20 13:22:50 -0800

    Report errors to sentry
    Captures all exceptions and sends them along to sentry when error
    reporting is turned on. Uses the same configuration variable as the
    frontend since the context is identical.

---
+++ src/EventListener/SentryCaptureListener.php
@@ -0,0 +1,36 @@
+<?php
+namespace App\EventListener;
+
+use App\Service\Config;
+use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+
+use function Sentry\init as sentryInit;
+use function Sentry\captureException as sentryCaptureException;
+
+/**
+ * Sends errors to symfony
+ */
+class SentryCaptureListener
+{
+    /**
+     * @var bool
+     */
+    protected $errorCaptureEnabled;
+
+    public function __construct(
+        Config $config,
+        string $sentryDSN
+    ) {
+        $this->errorCaptureEnabled = $config->get('errorCaptureEnabled');
+        sentryInit(['dsn' => $sentryDSN]);
+    }
+
+    public function onKernelException(GetResponseForExceptionEvent $event)
+    {
+        if (!$this->errorCaptureEnabled) {
+            return;
+        }
+
+        sentryCaptureException($event->getException());
+    }
+}
