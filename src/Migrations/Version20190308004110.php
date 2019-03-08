39b4310ad7405c7c31b65fed59c8f992070d35a1
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2019-03-07 16:43:03 -0800

    db migration to add/drop preferred email from user table.

---
+++ src/Migrations/Version20190308004110.php
@@ -0,0 +1,34 @@
+<?php declare(strict_types=1);
+
+namespace Ilios\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190308004110 extends AbstractMigration
+{
+    /**
+     * @inheritdoc
+     */
+    public function up(Schema $schema) : void
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE user ADD preferred_email VARCHAR(100) DEFAULT NULL');
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function down(Schema $schema) : void
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE user DROP preferred_email');
+    }
+}
+
+
