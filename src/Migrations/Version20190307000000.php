493ff5d11a3a79d4c5ab84644e532c95c71c33f3
Jonathan Johnson <jon.johnson@ucsf.edu>
2019-03-07 23:59:33 -0800

    Correctly bump API and reset objectives
    This API bump is happening after a release, any changes saved by users
    to objectives would have included a null active status which would put
    the DB into a state that probably wasn't intended. Changing this back to
    active seems less likely to cause issues as any change to inactive would
    have been intentional and repeatable.

--- src/Migrations/Version20190307000000.php
+++ src/Migrations/Version20190307000000.php
@@ -6,22 +6,17 @@ use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
 /**
- * Add active flag to objective table
+ * Reset objective active to true in case it was changed accidentally
  */
-final class Version20190225000000 extends AbstractMigration
+final class Version20190307000000 extends AbstractMigration
 {
     public function up(Schema $schema) : void
     {
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
-
-        $this->addSql('ALTER TABLE objective ADD active TINYINT(1) NOT NULL');
         $this->addSql('UPDATE objective SET active=1');
     }
 
     public function down(Schema $schema) : void
     {
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
-
-        $this->addSql('ALTER TABLE objective DROP active');
     }
 }
