13df17de1465dbd7ee52aa77c5ddbb23996ac55d
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2019-07-01 13:49:23 -0700

    use postX lifecycle events for timestamping entities.
    Relace onFlush() with postUpdate/postPersist/postDelete so that
    we can set the updateAt timestamp on the given entities.

--- src/EventListener/TimestampEntityChanges.php
+++ src/EventListener/TimestampEntityChanges.php
@@ -1,11 +1,14 @@
 <?php
 namespace App\EventListener;
 
+use DateTime;
+use Doctrine\Common\Persistence\Event\LifecycleEventArgs;
 use Doctrine\ORM\Event\OnFlushEventArgs;
 use App\Service\Timestamper;
 use App\Traits\TimestampableEntityInterface;
 use App\Traits\OfferingsEntityInterface;
 use App\Entity\SessionStampableInterface;
+use Exception;
 
 /**
  * Doctrine event listener.
@@ -29,38 +32,60 @@ class TimestampEntityChanges
         $this->timeStamper = $timeStamper;
     }
 
-    public function onFlush(OnFlushEventArgs $eventArgs)
-    {
-        $entityManager = $eventArgs->getEntityManager();
-        $uow = $entityManager->getUnitOfWork();
-        $entities = array_merge(
-            $uow->getScheduledEntityInsertions(),
-            $uow->getScheduledEntityUpdates(),
-            $uow->getScheduledEntityDeletions()
-        );
-
-        foreach ($entities as $entity) {
-            $this->stamp($entity);
-        }
+    /**
+     * @param LifecycleEventArgs $args
+     *
+     * @throws Exception
+     */
+    public function postUpdate(LifecycleEventArgs $args) {
+        $this->stamp($args->getObject(), new DateTime());
     }
 
-    protected function stamp($entity)
+    /**
+     * @param LifecycleEventArgs $args
+     *
+     * @throws Exception
+     */
+    public function postRemove(LifecycleEventArgs $args) {
+        $this->stamp($args->getObject(), new DateTime());
+
+    }
+
+    /**
+     * @param LifecycleEventArgs $args
+     *
+     * @throws Exception
+     */
+    public function postPersist(LifecycleEventArgs $args) {
+        $this->stamp($args->getObject(), new DateTime());
+    }
+
+    /**
+     * @param $entity
+     * @param DateTime $timestamp
+     *
+     * @throws Exception
+     */
+    protected function stamp($entity, Datetime $timestamp)
     {
         if ($entity instanceof TimestampableEntityInterface) {
-            $this->timeStamper->add($entity);
+            $this->timeStamper->add($entity, $timestamp);
+            $entity->setUpdatedAt($timestamp);
         }
 
         if ($entity instanceof OfferingsEntityInterface) {
             $offerings = $entity->getOfferings();
             foreach ($offerings as $offering) {
-                $this->timeStamper->add($offering);
+                $this->timeStamper->add($offering, $timestamp);
+                $offering->setUpdatedAt($timestamp);
             }
         }
 
         if ($entity instanceof SessionStampableInterface) {
             $sessions = $entity->getSessions();
             foreach ($sessions as $session) {
-                $this->timeStamper->add($session);
+                $this->timeStamper->add($session, $timestamp);
+                $session->setUpdatedAt($timestamp);
             }
         }
     }
