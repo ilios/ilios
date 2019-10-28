4fc49b94d5aebcf173eba4c1c25897f68182dab3
Jonathan Johnson <jon.johnson@ucsf.edu>
2019-10-28 11:43:34 -0700

    Add logging for material indexing
    In order to track down issues it's helpful to know when an indexing run
    actually starts and what materials it is working with.

--- src/MessageHandler/LearningMaterialIndexHandler.php
+++ src/MessageHandler/LearningMaterialIndexHandler.php
@@ -6,6 +6,7 @@ use App\Entity\Manager\LearningMaterialManager;
 use App\Message\LearningMaterialIndexRequest;
 use App\Service\IliosFileSystem;
 use App\Service\Index;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
 
 class LearningMaterialIndexHandler implements MessageHandlerInterface
@@ -25,14 +26,21 @@ class LearningMaterialIndexHandler implements MessageHandlerInterface
      */
     private $iliosFileSystem;
 
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
     public function __construct(
         Index $index,
         LearningMaterialManager $manager,
-        IliosFileSystem $iliosFileSystem
+        IliosFileSystem $iliosFileSystem,
+        LoggerInterface $logger
     ) {
         $this->index = $index;
         $this->manager = $manager;
         $this->iliosFileSystem = $iliosFileSystem;
+        $this->logger = $logger;
     }
 
     public function __invoke(LearningMaterialIndexRequest $message)
@@ -42,7 +50,13 @@ class LearningMaterialIndexHandler implements MessageHandlerInterface
             return $this->iliosFileSystem->checkLearningMaterialRelativePath($dto->relativePath);
         });
         if (count($filteredDtos)) {
+            $this->logger->debug('Start Indexing Learning Materials', [
+                'material_ids' => array_column($filteredDtos, 'id'),
+            ]);
             $this->index->indexLearningMaterials($filteredDtos);
+            $this->logger->debug('Complete Indexing Learning Materials', [
+                'material_ids' => array_column($filteredDtos, 'id'),
+            ]);
         }
     }
 }
