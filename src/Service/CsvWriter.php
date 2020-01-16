ab66c848a5666eb8da4772be92118ada633c296d
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2020-01-16 14:09:08 -0800

    adds csv exporter service.

---
+++ src/Service/CsvWriter.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service;
+
+use Exception;
+use League\Csv\Writer;
+
+/**
+ * Class CsvWriter
+ * @package App\Service
+ */
+class CsvWriter
+{
+    /**
+     * @param array $header
+     * @param array $data
+     * @param string $path
+     * @param string $openMode
+     * @throws Exception
+     */
+    public static function writeToFile(array $header, array $data, string $path, string $openMode = 'w+'): void
+    {
+        $writer = Writer::createFromPath($path, $openMode);
+        $writer->insertOne($header);
+        $writer->insertAll($data);
+    }
+}
