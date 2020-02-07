7857faf2fda20bc422ff9bc29ec763191d519c8f
Jonathan Johnson <jon.johnson@ucsf.edu>
2020-02-06 21:40:02 -0800

    Add some light caching of database configuration values
    Each request loads 9-15 of these and each load was making a DB query to
    get the value. Loading them all at once saves about 100ms on each
    request which is a substantial speed improvement over several
    requests.

--- src/Entity/Manager/ApplicationConfigManager.php
+++ src/Entity/Manager/ApplicationConfigManager.php
@@ -12,11 +12,30 @@ use App\Entity\Repository\ApplicationConfigRepository;
  */
 class ApplicationConfigManager extends BaseManager
 {
+    protected $cache;
+
+    protected function buildCache()
+    {
+        if (!$this->cache) {
+            $this->cache = [];
+
+            /** @var ApplicationConfigRepository $repository */
+            $repository = $this->getRepository();
+            $configs = $repository->getAllValues();
+
+            foreach ($configs as ['name' => $name, 'value' => $value]) {
+                $this->cache[$name] = $value;
+            }
+        }
+    }
+
     public function getValue($name)
     {
-        /** @var ApplicationConfigRepository $repository */
-        $repository = $this->getRepository();
+        $this->buildCache();
+        if (array_key_exists($name, $this->cache)) {
+            return $this->cache[$name];
+        }
 
-        return $repository->getValue($name);
+        return null;
     }
 }
