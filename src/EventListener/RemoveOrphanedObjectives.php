343701b8ef9fbe457cb776e86cb67aac5606e835
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2020-04-10 10:21:20 -0700

    updates entities and relationship to check when processing orphaned objectives.

--- src/EventListener/RemoveOrphanedObjectives.php
+++ src/EventListener/RemoveOrphanedObjectives.php
@@ -4,25 +4,25 @@ declare(strict_types=1);
 
 namespace App\EventListener;
 
+use App\Entity\ObjectiveInterface;
+use App\Entity\ObjectiveRelationshipInterface;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\Event\OnFlushEventArgs;
+use Doctrine\ORM\ORMException;
 use Doctrine\ORM\UnitOfWork;
-use App\Entity\CourseInterface;
 use App\Entity\Objective;
-use App\Entity\ProgramYearInterface;
-use App\Entity\SessionInterface;
 use App\Traits\IdentifiableEntityInterface;
 
 /**
  * Doctrine event listener.
- * When a ProgramYear, Course, or Session is deleted cleanup any newly orphaned objectives.
+ * When a ProgramYear-, Course-, or Session-Objective is deleted cleanup any newly orphaned objectives.
  */
 class RemoveOrphanedObjectives
 {
     /**
      * @param OnFlushEventArgs $eventArgs
-     * @throws \Doctrine\ORM\ORMException
+     * @throws ORMException
      */
     public function onFlush(OnFlushEventArgs $eventArgs)
     {
@@ -30,25 +30,24 @@ class RemoveOrphanedObjectives
         $uow = $em->getUnitOfWork();
 
         foreach ($uow->getScheduledEntityDeletions() as $entity) {
-            if (
-                $entity instanceof ProgramYearInterface ||
-                $entity instanceof CourseInterface ||
-                $entity instanceof SessionInterface
-            ) {
-                $objectives = $entity->getObjectives();
-                foreach ($objectives as $objective) {
-                    $programYears = $objective->getProgramYears();
-                    $courses = $objective->getCourses();
-                    $sessions = $objective->getSessions();
-                    /** @var IdentifiableEntityInterface[] $allLinks */
-                    $allLinks = array_merge($programYears->toArray(), $courses->toArray(), $sessions->toArray());
-                    //ensure that this Objective is only linked to the deleted entity
-                    if (
-                        count($allLinks) === 0 ||
-                        (count($allLinks) === 1 && $allLinks[0]->getId() === $entity->getId())
-                    ) {
-                        $this->removeLinks($uow, $em, $objective);
-                    }
+            if ($entity instanceof ObjectiveRelationshipInterface) {
+                /* @var ObjectiveInterface $objective */
+                $objective = $entity->getObjective();
+                $programYearObjectives = $objective->getProgramYearObjectives();
+                $courseObjectives = $objective->getCourseObjectives();
+                $sessionObjectives = $objective->getSessionObjectives();
+                /** @var IdentifiableEntityInterface[] $allLinks */
+                $allLinks = array_merge(
+                    $programYearObjectives->toArray(),
+                    $courseObjectives->toArray(),
+                    $sessionObjectives->toArray()
+                );
+                //ensure that this Objective is only linked to the deleted entity
+                if (
+                    count($allLinks) === 0 ||
+                    (count($allLinks) === 1 && $allLinks[0]->getId() === $entity->getId())
+                ) {
+                    $this->removeLinks($uow, $em, $objective);
                 }
             }
         }
@@ -57,10 +56,10 @@ class RemoveOrphanedObjectives
     /**
      * @param UnitOfWork $uow
      * @param EntityManager $em
-     * @param Objective $objective
-     * @throws \Doctrine\ORM\ORMException
+     * @param ObjectiveInterface $objective
+     * @throws ORMException
      */
-    protected function removeLinks(UnitOfWork $uow, EntityManager $em, Objective $objective)
+    protected function removeLinks(UnitOfWork $uow, EntityManager $em, ObjectiveInterface $objective)
     {
         $objective->setParents(new ArrayCollection());
         $objective->setCompetency(null);
