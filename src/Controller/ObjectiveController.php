bd189f20bb4f3e3093c5e0a0f6458a08ee90e622
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2020-05-13 10:37:15 -0700

    cleans up objectivecontroller.

--- src/Controller/ObjectiveController.php
+++ src/Controller/ObjectiveController.php
@@ -4,12 +4,6 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-use App\Entity\DTO\ObjectiveDTO;
-use App\Entity\DTO\ObjectiveV1DTO;
-use App\Entity\Manager\CourseObjectiveManager;
-use App\Entity\Manager\ObjectiveManager;
-use App\Entity\Manager\ProgramYearObjectiveManager;
-use App\Entity\Manager\SessionObjectiveManager;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
@@ -20,35 +14,6 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
  */
 class ObjectiveController extends ApiController
 {
-    /**
-     * @var CourseObjectiveManager
-     */
-    protected $courseObjectiveManager;
-
-    /**
-     * @var ProgramYearObjectiveManager
-     */
-    protected $programYearObjectiveManager;
-
-    /**
-     * @var SessionObjectiveManager
-     */
-    protected $sessionObjectiveManager;
-
-    /**
-     * @required
-     */
-    public function setup(
-        ObjectiveManager $objectiveManager,
-        CourseObjectiveManager $courseObjectiveManager,
-        ProgramYearObjectiveManager $programYearObjectiveManager,
-        SessionObjectiveManager $sessionObjectiveManager
-    ) {
-        $this->courseObjectiveManager = $courseObjectiveManager;
-        $this->programYearObjectiveManager = $programYearObjectiveManager;
-        $this->sessionObjectiveManager = $sessionObjectiveManager;
-    }
-
     public function get($version, $object, $id)
     {
         $manager = $this->getManager($object);
@@ -57,7 +22,6 @@ class ObjectiveController extends ApiController
             $dto = $manager->findV1DTOBy(['id' => $id]);
         } else {
             $dto = $manager->findDTOBy(['id' => $id]);
-
         }
 
         if (! $dto) {
