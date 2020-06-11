5788d61fd0a57e7aff0ac4c8a8717d7011202880
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2020-06-11 14:24:20 -0700

    adds data migration.

--- src/Migrations/Version20200610182857.php
+++ src/Migrations/Version20200610182857.php
@@ -50,6 +50,33 @@ final class Version20200610182857 extends AbstractMigration
         $this->addSql('ALTER TABLE session_x_objective ADD ancestor_id INT DEFAULT NULL, ADD title LONGTEXT NOT NULL, ADD active TINYINT(1) NOT NULL');
         $this->addSql('ALTER TABLE session_x_objective ADD CONSTRAINT FK_C4BF2447C671CEA1 FOREIGN KEY (ancestor_id) REFERENCES session_x_objective (session_objective_id)');
         $this->addSql('CREATE INDEX IDX_C4BF2447C671CEA1 ON session_x_objective (ancestor_id)');
+
+        // ---------------------------
+        // data migration
+        // ---------------------------
+        // copy title and active flag from objectives to linked x-objectives
+        $this->addSql('UPDATE session_x_objective sxo JOIN objective o ON o.objective_id = sxo.objective_id SET sxo.title = o.title, sxo.active = o.active');
+        $this->addSql('UPDATE course_x_objective cxo JOIN objective o ON o.objective_id = cxo.objective_id SET cxo.title = o.title, cxo.active = o.active');
+        $this->addSql('UPDATE program_year_x_objective pyxo JOIN objective o ON o.objective_id = pyxo.objective_id SET pyxo.title = o.title, pyxo.active = o.active');
+
+        // copy mesh associations
+        $this->addSql('INSERT INTO session_objective_x_mesh (session_objective_id, mesh_descriptor_uid) SELECT sxo.session_objective_id, oxm.mesh_descriptor_uid FROM session_x_objective sxo JOIN objective o ON sxo.objective_id = o.objective_id JOIN objective_x_mesh oxm ON o.objective_id = oxm.objective_id');
+        $this->addSql('INSERT INTO course_objective_x_mesh (course_objective_id, mesh_descriptor_uid) SELECT cxo.course_objective_id, oxm.mesh_descriptor_uid FROM course_x_objective cxo JOIN objective o ON cxo.objective_id = o.objective_id JOIN objective_x_mesh oxm ON o.objective_id = oxm.objective_id');
+        $this->addSql('INSERT INTO program_year_objective_x_mesh (program_year_objective_id, mesh_descriptor_uid) SELECT pyxo.program_year_objective_id, oxm.mesh_descriptor_uid FROM program_year_x_objective pyxo JOIN objective o ON pyxo.objective_id = o.objective_id JOIN objective_x_mesh oxm ON o.objective_id = oxm.objective_id');
+
+        // copy parentage associations
+        $this->addSql('INSERT INTO session_objective_x_course_objective(session_objective_id, course_objective_id) SELECT sxo.session_objective_id, cxo.course_objective_id FROM session_x_objective sxo JOIN objective o ON sxo.objective_id = o.objective_id JOIN objective_x_objective oxo ON o.objective_id = oxo.objective_id JOIN objective o2 IN o2.objective_id = oxo.parent_objective_id JOIN course_x_objective cxo ON o2.objective_id = cxo.objective_id');
+        $this->addSql('INSERT INTO course_objective_x_program_year_objective(course_objective_id, program_year_objective_id) SELECT cxo.course_objective_id, pyxo.program_year_objective_id FROM course_x_objective cxo JOIN objective o ON cxo.objective_id = o.objective_id JOIN objective_x_objective oxo ON o.objective_id = oxo.objective_id JOIN objective o2 ON o2.objective_id = oxo.parent_objective_id JOIN program_year_x_objective pyxo ON o2.objective_id = pyxo.objective_id');
+
+        // copy ancestry associations
+        $this->addSql('UPDATE session_x_objective sxo JOIN objective o ON o.objective_id = sxo.objective_id JOIN objective ancestor ON o.ancestor_id = ancestor.objective_id JOIN session_x_objective sxo2 ON sxo2.objective_id = ancestor.objective_id SET sxo.ancestor_id = sxo2.session_objective_id');
+        $this->addSql('UPDATE course_x_objective cxo JOIN objective o ON o.objective_id = cxo.objective_id JOIN objective ancestor ON o.ancestor_id = ancestor.objective_id JOIN course_x_objective cxo2 ON cxo2.objective_id = ancestor.objective_id SET cxo.ancestor_id = cxo2.course_objective_id');
+        $this->addSql('UPDATE program_year_x_objective pyxo JOIN objective o ON o.objective_id = pyxo.objective_id JOIN objective ancestor ON o.ancestor_id = ancestor.objective_id JOIN program_year_x_objective pyxo2 ON pyxo2.objective_id = ancestor.objective_id SET pyxo.ancestor_id = pyxo2.program_year_objective_id');
+
+        // reverse data migration: copy position BACK onto objective (see App\Migrations\Version20200408232411.php)
+        $this->addSql('UPDATE objective o JOIN session_x_objective sxo ON o.objective_id = sxo.objective_id SET o.position = sxo.position');
+        $this->addSql('UPDATE objective o JOIN course_x_objective cxo ON o.objective_id = cxo.objective_id SET o.position = cxo.position');
+        $this->addSql('UPDATE objective o JOIN program_year_x_objective pyxo ON o.objective_id = pyxo.objective_id SET o.position = pyxo.position');
     }
 
     /**
