c187f726c985ec99d087fbacb5019e13e930a882
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2020-06-15 14:11:34 -0700

    objectives controller only supported in API v1.

--- src/Controller/API/Objectives.php
+++ src/Controller/API/Objectives.php
@@ -8,7 +8,7 @@ use App\Entity\Manager\ObjectiveManager;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/api/{version<v1|v2>}/objectives")
+ * @Route("/api/v1/objectives")
  */
 class Objectives extends ReadWriteController
 {
