559c7334a49eba6dde5b4f5636168d7143d5b1f7
Jonathan Johnson <jon.johnson@ucsf.edu>
2020-06-21 22:28:30 -0700

    Index Learning Materials by ID
    Since we're not splitting these up any more we can use the ID and prevent duplicates in our index.

--- src/Message/LearningMaterialIndexRequest.php
+++ src/Message/LearningMaterialIndexRequest.php
@@ -4,17 +4,26 @@ declare(strict_types=1);
 
 namespace App\Message;
 
+use DateTime;
+
 class LearningMaterialIndexRequest
 {
     private $id;
+    private $createdAt;
 
     public function __construct(int $id)
     {
         $this->id = $id;
+        $this->createdAt = new DateTime();
     }
 
     public function getId(): int
     {
         return $this->id;
+    }
+
+    public function getCreatedAt(): DateTime
+    {
+        return $this->createdAt;
     }
 }
