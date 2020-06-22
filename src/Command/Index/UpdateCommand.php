abff3f952168f99cb06c2625e123758b73f7b120
Jonathan Johnson <jon.johnson@ucsf.edu>
2020-06-22 09:33:03 -0700

    Move queue clearing to when the index is dropped
    We don't need to do this on update now since each service will protect
    itself against adding duplicates and double indexing the same data.

--- src/Command/Index/UpdateCommand.php
+++ src/Command/Index/UpdateCommand.php
@@ -12,8 +12,6 @@ use App\Repository\CourseRepository;
 use App\Repository\LearningMaterialRepository;
 use App\Repository\MeshDescriptorRepository;
 use App\Repository\UserRepository;
-use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -34,29 +32,18 @@ class UpdateCommand extends Command
         protected MeshDescriptorRepository $descriptorRepository,
         protected LearningMaterialRepository $learningMaterialRepository,
         protected MessageBusInterface $bus,
-        protected EntityManagerInterface $entityManager,
     ) {
         parent::__construct();
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $this->clearIndexQueue($output);
         $this->queueUsers($output);
         $this->queueLearningMaterials($output);
         $this->queueCourses($output);
         $this->queueMesh($output);
 
         return Command::SUCCESS;
-    }
-
-    protected function clearIndexQueue(OutputInterface $output): void
-    {
-        $sql = 'DELETE FROM messenger_messages WHERE queue_name="search"';
-        $conn = $this->entityManager->getConnection();
-        $removed = $conn->executeStatement($sql);
-
-        $output->writeln("<info>Cleared ${removed} Existing search requests from queue.</info>");
     }
 
     protected function queueUsers(OutputInterface $output): void
