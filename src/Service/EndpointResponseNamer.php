f9301cec8eef719193febc333f225bba7c09320d
Jonathan Johnson <jon.johnson@ucsf.edu>
2020-06-24 23:06:35 -0700

    Cache the entity list
    This get's called a lot during JSON:API serialization and going to the file system each time is slow. Caching it speeds up requests 30-50%.

--- src/Service/EndpointResponseNamer.php
+++ src/Service/EndpointResponseNamer.php
@@ -8,6 +8,8 @@ use Doctrine\Inflector\Inflector;
 use Symfony\Component\Finder\Finder;
 use Symfony\Component\Finder\SplFileInfo;
 use Symfony\Component\HttpKernel\KernelInterface;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\Cache\ItemInterface;
 
 /**
  * Class EndpointResponseNamer
@@ -29,14 +31,24 @@ class EndpointResponseNamer
     protected $inflector;
 
     /**
+     * @var CacheInterface
+     */
+    protected $appCache;
+
+    private const LIST_CACHE_KEY = 'endpoint-response-namer-entity-list';
+
+    /**
      * EndpointResponseNamer constructor.
      * Extracts the entity path from the Kernel
      * @param KernelInterface $kernel
+     * @param Inflector $inflector
+     * @param CacheInterface $appCache
      */
-    public function __construct(KernelInterface $kernel, Inflector $inflector)
+    public function __construct(KernelInterface $kernel, Inflector $inflector, CacheInterface $appCache)
     {
         $this->pathToEntities = $kernel->getProjectDir() . '/src/Entity';
         $this->inflector = $inflector;
+        $this->appCache = $appCache;
     }
 
     /**
@@ -67,25 +79,28 @@ class EndpointResponseNamer
 
     /**
      * Create a list of all the plural and singular names for entities
-     * @return array
      */
-    protected function getEntityList()
+    protected function getEntityList(): array
     {
-        $finder = new Finder();
-        $files = $finder->in($this->pathToEntities)->files()->notName('*Interface.php')->sortByName();
+        $pathToEntities = $this->pathToEntities;
+        $inflector = $this->inflector;
+        return $this->appCache->get(self::LIST_CACHE_KEY, function () use ($pathToEntities, $inflector) {
+            $finder = new Finder();
+            $files = $finder->in($pathToEntities)->files()->notName('*Interface.php')->sortByName();
 
-        $list = [];
-        /** @var SplFileInfo $file */
-        foreach ($files as $file) {
-            $name = $file->getBasename('.php');
-            $plural = $this->inflector->pluralize($name);
-            $key = strtolower($plural);
-            $list[$key] = [
-                'plural' => $this->inflector->camelize($plural),
-                'singular' => $this->inflector->camelize($name),
-            ];
-        }
+            $list = [];
+            /** @var SplFileInfo $file */
+            foreach ($files as $file) {
+                $name = $file->getBasename('.php');
+                $plural = $inflector->pluralize($name);
+                $key = strtolower($plural);
+                $list[$key] = [
+                    'plural' => $inflector->camelize($plural),
+                    'singular' => $inflector->camelize($name),
+                ];
+            }
 
-        return $list;
+            return $list;
+        });
     }
 }
