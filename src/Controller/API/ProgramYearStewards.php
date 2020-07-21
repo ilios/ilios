cb99a59c538a878d38ea29e156d3a303651f3d3b
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2020-07-21 13:06:20 -0700

    put program year stewards controller into readonly v1 API mode.

--- src/Controller/API/ProgramYearStewards.php
+++ src/Controller/API/ProgramYearStewards.php
@@ -8,9 +8,9 @@ use App\Entity\Manager\ProgramYearStewardManager;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/api/{version<v1|v3>}/programyearstewards")
+ * @Route("/api/{version<v1>}/programyearstewards")
  */
-class ProgramYearStewards extends ReadWriteController
+class ProgramYearStewards extends ReadOnlyController
 {
     public function __construct(ProgramYearStewardManager $manager)
     {
