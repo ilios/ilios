e5cf7f41871494b3a29b2b7723c485a86e7b1a1d
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2020-07-22 13:41:47 -0700

    provides dto for v1 api program endpoint.

---
+++ src/Entity/DTO/ProgramV1DTO.php
@@ -0,0 +1,120 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity\DTO;
+
+use App\Annotation as IS;
+
+/**
+ * Class ProgramDTO
+ * Data transfer object for a Program
+ *
+ * @IS\DTO("programs")
+ */
+class ProgramDTO
+{
+    /**
+     * @var int
+     * @IS\Id
+     * @IS\Expose
+     * @IS\Type("integer")
+     */
+    public $id;
+
+    /**
+     * @var string
+     * @IS\Expose
+     * @IS\Type("string")
+     *
+     */
+    public $title;
+
+    /**
+     * @var string
+     * @IS\Expose
+     * @IS\Type("string")
+     *
+     */
+    public $shortTitle;
+
+    /**
+     * @var int
+     * @IS\Expose
+     * @IS\Type("string")
+     *
+     */
+    public $duration;
+
+    /**
+     * @var bool
+     * @IS\Expose
+     * @IS\Type("boolean")
+     * @deprecated
+     */
+    public $publishedAsTbd;
+
+    /**
+     * @var bool
+     * @IS\Expose
+     * @IS\Type("boolean")
+     * @deprecated
+     */
+    public $published;
+
+    /**
+     * @var int
+     * @IS\Expose
+     * @IS\Related("schools")
+     * @IS\Type("string")
+     *
+     */
+    public $school;
+
+    /**
+     * @var int[]
+     * @IS\Expose
+     * @IS\Related
+     * @IS\Type("array<string>")
+     *
+     */
+    public $programYears;
+
+    /**
+     * @var int[]
+     * @IS\Expose
+     * @IS\Related
+     * @IS\Type("array<string>")
+     *
+     */
+    public $curriculumInventoryReports;
+
+    /**
+     * @var int[]
+     * @IS\Expose
+     * @IS\Related("users")
+     * @IS\Type("array<string>")
+     */
+    public $directors;
+
+
+    public function __construct(
+        $id,
+        $title,
+        $shortTitle,
+        $duration,
+        $publishedAsTbd,
+        $published
+    ) {
+        $this->id = $id;
+        $this->title = $title;
+        $this->shortTitle = $shortTitle;
+        $this->duration = $duration;
+        $this->publishedAsTbd = $publishedAsTbd;
+        $this->published = $published;
+
+        $this->programYears = [];
+        $this->curriculumInventoryReports = [];
+        $this->directors = [];
+    }
+}
