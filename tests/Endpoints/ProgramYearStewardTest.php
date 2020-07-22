e42c45fea0fae04444d6207917b15b18ae50a754
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2020-07-21 17:25:14 -0700

    set program year steward endpoint test to v1 and read-only mode.

--- tests/Endpoints/ProgramYearStewardTest.php
+++ tests/Endpoints/ProgramYearStewardTest.php
@@ -4,17 +4,17 @@ declare(strict_types=1);
 
 namespace App\Tests\Endpoints;
 
-use App\Tests\DataLoader\DepartmentData;
-use App\Tests\ReadWriteEndpointTest;
+use App\Tests\ReadEndpointTest;
 
 /**
  * ProgramYearSteward API endpoint Test.
  * @group api_2
  */
-class ProgramYearStewardTest extends ReadWriteEndpointTest
+class ProgramYearStewardTest extends ReadEndpointTest
 {
     protected $testName =  'programYearStewards';
 
+    protected $apiVersion = 'v1';
     /**
      * @inheritdoc
      */
@@ -25,18 +25,6 @@ class ProgramYearStewardTest extends ReadWriteEndpointTest
             'App\Tests\Fixture\LoadDepartmentData',
             'App\Tests\Fixture\LoadProgramYearData',
             'App\Tests\Fixture\LoadSchoolData'
-        ];
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function putsToTest()
-    {
-        return [
-            'department' => ['department', 3],
-            'programYear' => ['programYear', 2],
-            'school' => ['school', 2],
         ];
     }
 
@@ -62,85 +50,5 @@ class ProgramYearStewardTest extends ReadWriteEndpointTest
             'programYear' => [[0, 1], ['programYear' => 1]],
             'school' => [[0, 1], ['school' => 1]],
         ];
-    }
-
-    /**
-     * Creating many runs into UNIQUE constraints quick
-     * so instead build a bunch of new departments to use
-     */
-    protected function createMany(int $count): array
-    {
-        $departmentDataLoader = $this->getContainer()->get(DepartmentData::class);
-        $departments = $departmentDataLoader->createMany($count);
-        $savedDepartments = $this->postMany('departments', 'departments', $departments);
-
-        $dataLoader = $this->getDataLoader();
-
-        $data = [];
-        foreach ($savedDepartments as $i => $department) {
-            $arr = $dataLoader->create();
-            $arr['id'] += $i;
-            $arr['department'] = (string) $department['id'];
-
-            $data[] = $arr;
-        }
-
-        return $data;
-    }
-
-    public function testPostMany()
-    {
-        $data = $this->createMany(51);
-        $this->postManyTest($data);
-    }
-
-    public function testPostManyJsonApi()
-    {
-        $data = $this->createMany(10);
-        $jsonApiData = $this->getDataLoader()->createBulkJsonApi($data);
-        $this->postManyJsonApiTest($jsonApiData, $data);
-    }
-
-
-    /**
-     * Override this so we don't change any values.  Changing something
-     * like department or school causes key conflicts and there isn't really
-     * anything non-unique to change here.
-     */
-    public function testPutForAllData()
-    {
-        $dataLoader = $this->getDataLoader();
-        $all = $dataLoader->getAll();
-        foreach ($all as $data) {
-            $this->putTest($data, $data, $data['id']);
-        }
-    }
-
-    /**
-     * Override this so we don't change any values.  Changing something
-     * like department or school causes key conflicts and there isn't really
-     * anything non-unique to change here.
-     */
-    public function testPatchForAllDataJsonApi()
-    {
-        $dataLoader = $this->getDataLoader();
-        $all = $dataLoader->getAll();
-        foreach ($all as $data) {
-            $jsonApiData = $dataLoader->createJsonApi($data);
-            $this->patchJsonApiTest($data, $jsonApiData);
-        }
-    }
-
-    /**
-     * Tests creation of a new steward at the school level, without department.
-     */
-    public function testPostStewardWithoutDepartment()
-    {
-        $dataLoader = $this->getDataLoader();
-        $data = $dataLoader->create();
-        $this->assertNotEmpty($data['department']);
-        unset($data['department']);
-        $postData = $data;
-        $this->postTest($data, $postData);
     }
 }
