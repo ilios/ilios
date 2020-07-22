a3d1f9e0f784b4836daccbceeffdc5a63d44fc43
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2020-07-22 11:20:03 -0700

    rm borked/unnecessary import.

--- src/Controller/API/Departments.php
+++ src/Controller/API/Departments.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace App\Controller\API;
 
 use App\Entity\Manager\DepartmentManager;
-use App\Tests\V1ReadEndpointTest;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
