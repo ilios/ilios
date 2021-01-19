8ef254b99a1cf876feb0ca9a3f594ad8ce5b167b
Jonathan Johnson <jon.johnson@ucsf.edu>
2021-01-19 09:20:37 -0800

    Add missing interface traits to course/session objectives
    These are implemented, but were not part of the interface causing some
    warnings.

--- src/Entity/CourseObjectiveInterface.php
+++ src/Entity/CourseObjectiveInterface.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Entity;
 
 use App\Traits\ActivatableEntityInterface;
+use App\Traits\IdentifiableEntityInterface;
 use App\Traits\IndexableCoursesEntityInterface;
 use App\Traits\MeshDescriptorsEntityInterface;
 use App\Traits\TitledEntityInterface;
@@ -14,6 +15,7 @@ use Doctrine\Common\Collections\Collection;
  * Interface CourseObjectiveInterface
  */
 interface CourseObjectiveInterface extends
+    IdentifiableEntityInterface,
     IndexableCoursesEntityInterface,
     TitledEntityInterface,
     MeshDescriptorsEntityInterface,
