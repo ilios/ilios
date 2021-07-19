453b87211eb1d2517c81cc02a9500767c4314bb0
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2021-07-19 15:49:10 -0700

    adds new interface.

---
+++ src/Repository/DataImportRepositoryInterface.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Repository;
+
+/**
+ * Interface for repositories that can bulk-import data.
+ *
+ * Interface DataImportRepositoryInterface
+ * @package App\Repository
+ */
+interface DataImportRepositoryInterface
+{
+    /**
+     * Imports a given set of records into their corresponding database table(s).
+     *
+     * @param array $data An associative array containing the data records to import.
+     * @param string|null $type The type of data that's being imported.
+     * @param string|null $now The current time and date as an ANSI SQL compatible string representation.
+     */
+    public function import(array $data, string $type = null, string $now = null): void;
+
+    /**
+     * Checks whether the targeted database table(s) contain data or not.
+     * @return bool
+     */
+    public function hasData(): bool;
+}
