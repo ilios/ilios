825da9e8102d316ea840e6ffc1877a992e1c0c6f
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2021-09-10 10:52:41 +0200

    rm unused import.

--- src/Security/JsonWebTokenAuthenticator.php
+++ src/Security/JsonWebTokenAuthenticator.php
@@ -13,7 +13,6 @@ use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
-use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\CustomCredentials;
