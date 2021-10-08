65ff469267777022593ab991f3d438f63bf8393f
Jonathan Johnson <jon.johnson@ucsf.edu>
2021-10-08 09:37:36 -0700

    Add version from git to application config
    This extracts the tag from git and includes it in the application
    configuration endpoint. We can also use this other places where knowing
    the latest version is important. This package also adds the ability to
    bump the current git tag so we can automate this process.

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Liip\TestFixturesBundle\LiipTestFixturesBundle::class => ['dev' => true, 'test' => true],
+    Shivas\VersioningBundle\ShivasVersioningBundle::class => ['all' => true],
 ];
