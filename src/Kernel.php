10524ba98175cf1d946e25ba2ff24b744309bde0
Jonathan Johnson <jon.johnson@ucsf.edu>
2021-12-13 16:58:30 -0800

    Update framework bundle flex recipe
    The kernel can now be empty, we've only left behind our customizations
    for forcing UTC.

--- src/Kernel.php
+++ src/Kernel.php
@@ -5,10 +5,9 @@ declare(strict_types=1);
 namespace App;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
-use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
-use function dirname;
+
+use function date_default_timezone_set;
 
 class Kernel extends BaseKernel
 {
@@ -23,30 +22,5 @@ class Kernel extends BaseKernel
         // Force a UTC timezone on everyone
         date_default_timezone_set('UTC');
         parent::__construct($environment, $debug);
-    }
-
-    protected function configureContainer(ContainerConfigurator $container): void
-    {
-        $container->import('../config/{packages}/*.yaml');
-        $container->import('../config/{packages}/'.$this->environment.'/*.yaml');
-
-        if (is_file(dirname(__DIR__).'/config/services.yaml')) {
-            $container->import('../config/services.yaml');
-            $container->import('../config/{services}_'.$this->environment.'.yaml');
-        } else {
-            $container->import('../config/{services}.php');
-        }
-    }
-
-    protected function configureRoutes(RoutingConfigurator $routes): void
-    {
-        $routes->import('../config/{routes}/'.$this->environment.'/*.yaml');
-        $routes->import('../config/{routes}/*.yaml');
-
-        if (is_file(dirname(__DIR__).'/config/routes.yaml')) {
-            $routes->import('../config/routes.yaml');
-        } else {
-            $routes->import('../config/{routes}.php');
-        }
     }
 }
