90ef2bfdcf1728e46d7ea46316c6b5c70a207565
Jonathan Johnson <jon.johnson@ucsf.edu>
2022-03-21 15:01:32 -0700

    Check for database to be setup during container boot
    It's required and nothing is going to work without it, but checking
    first means the container can be booted without a DB for testing or
    extending.

--- docker/fpm/docker-entrypoint.sh
+++ docker/fpm/docker-entrypoint.sh
@@ -8,11 +8,15 @@ fi
 
 mkdir -p var/cache var/log
 if [ "$1" = 'php-fpm' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
-	echo "Waiting for db to be ready..."
-	bin/console ilios:wait-for-database
-	echo "The db is now ready and reachable"
-	bin/console cache:warmup
-	bin/console doctrine:migrations:migrate --no-interaction
+	
+	if [ "$ILIOS_DATABASE_URL" ]; then
+		echo "Waiting for db to be ready..."
+		bin/console ilios:wait-for-database
+		echo "The db is now ready and reachable"
+
+		bin/console cache:warmup
+		bin/console doctrine:migrations:migrate --no-interaction
+	fi
 
 	setfacl -R -m u:www-data:rwX -m u:"$(whoami)":rwX var
 	setfacl -dR -m u:www-data:rwX -m u:"$(whoami)":rwX var
