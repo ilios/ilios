f891308d57e1ae70fe5e64ef89b89682750fa62e
Jonathan Johnson <jon.johnson@ucsf.edu>
2022-03-20 22:36:17 -0700

    Set access control after clearing the cache
    Otherwise the htmlpurifier cache ends up not having write permissions if
    the runtime cache is built from a volume or if the environment is not
    prod.

--- docker/fpm/docker-entrypoint.sh
+++ docker/fpm/docker-entrypoint.sh
@@ -20,14 +20,14 @@ if [ "$1" = 'php-fpm' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
 		composer install --prefer-dist --no-progress --no-interaction
 	fi
 
-	setfacl -R -m u:www-data:rwX -m u:"$(whoami)":rwX var
-	setfacl -dR -m u:www-data:rwX -m u:"$(whoami)":rwX var
-
 	echo "Waiting for db to be ready..."
 	bin/console ilios:wait-for-database
 	echo "The db is now ready and reachable"
 	bin/console cache:warmup
 	bin/console doctrine:migrations:migrate --no-interaction
+
+	setfacl -R -m u:www-data:rwX -m u:"$(whoami)":rwX var
+	setfacl -dR -m u:www-data:rwX -m u:"$(whoami)":rwX var
 fi
 
 exec docker-php-entrypoint "$@"
