e3d34820392a06ad3fb3ffc669be2641bf1a0173
Jonathan Johnson <jon.johnson@ucsf.edu>
2022-04-20 12:10:25 -0700

    Use PHPStan console application
    This improves our static analysis as it gives access to the runtime.

---
+++ tests/phpstan-console-application.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests;
+
+use App\Kernel;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Component\Dotenv\Dotenv;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+(new Dotenv())->bootEnv(__DIR__ . '/../.env');
+
+$kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
+return new Application($kernel);
