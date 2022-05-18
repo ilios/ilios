87d338187484c496528a4a351c3bba798c97bffc
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2022-05-18 09:43:15 -0700

    insert our API description via a twig filter.
    i think i just peaked as a coder with this.

---
+++ src/Twig/Extension.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+
+class Extension extends AbstractExtension
+{
+    public function getFilters(): array
+    {
+        return [
+            new TwigFilter('insert_api_doc_description', [Runtime::class, 'insertApiDocDescription']),
+        ];
+    }
+
+}
---
+++ src/Twig/Runtime.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Twig;
+
+use Symfony\Component\Routing\Generator\UrlGenerator;
+use Symfony\Component\Routing\RouterInterface;
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
+use Twig\Extension\RuntimeExtensionInterface;
+
+class Runtime implements RuntimeExtensionInterface
+{
+    public function __construct(
+        protected Environment $twig,
+        protected RouterInterface $router,
+        protected string $environment,
+        protected string $apiVersion
+    ) {
+    }
+
+    /**
+     * KLUDGE!
+     * Twig filter that injects our descriptions into the API docs data structure before it gets rendered.
+     * @param array $swaggerData
+     * @return array
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    public function insertApiDocDescription(array $swaggerData): array
+    {
+        $apiDocsUrl = $this->router->generate(
+            'app.swagger_ui',
+            [],
+            UrlGenerator::ABSOLUTE_URL
+        );
+        $myprofileUrl = $this->router->generate(
+            'app_index_index',
+            [],
+            UrlGenerator::ABSOLUTE_URL
+        );
+        $userApiUrl = $this->router->generate(
+            'app_api_users_getall',
+            ['version' => 'v3'],
+            UrlGenerator::ABSOLUTE_URL
+        );
+        $swaggerData['spec']['info']['description'] = $this->twig->render('swagger/description.markdown.twig', [
+            'apiDocsUrl' => $apiDocsUrl,
+            'myprofileUrl' => $myprofileUrl . 'myprofile',
+            'userApiUrl' => $userApiUrl,
+        ]);
+        return $swaggerData;
+    }
+}
