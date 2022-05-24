4fb49a5dac3b901a8467f260f6e9846959d7eed8
Jonathan Johnson <jon.johnson@ucsf.edu>
2022-05-24 16:21:38 -0700

    Use cache marshaller to encode, compress, and secure data
    When an ILIOS_CACHE_DECRYPTION_KEY is provided we use it to encrypt
    cache data at rest as insurance against the cache being compromised.

--- src/Service/DynamicCacheFactory.php
+++ src/Service/DynamicCacheFactory.php
@@ -8,25 +8,50 @@ use Symfony\Component\Cache\Adapter\FilesystemTagAwareAdapter;
 use Symfony\Component\Cache\Adapter\RedisAdapter;
 use Symfony\Component\Cache\Adapter\RedisTagAwareAdapter;
 use Symfony\Component\Cache\Adapter\TagAwareAdapterInterface;
+use Symfony\Component\Cache\Marshaller\DefaultMarshaller;
+use Symfony\Component\Cache\Marshaller\DeflateMarshaller;
+use Symfony\Component\Cache\Marshaller\MarshallerInterface;
+use Symfony\Component\Cache\Marshaller\SodiumMarshaller;
 
 class DynamicCacheFactory
 {
     private const DEFAULT_LIFETIME_SECONDS = 86400;
 
-    public static function getCache($namespace, $redisUrl, $kernelCacheDirectory): TagAwareAdapterInterface
-    {
+    public static function getCache(
+        string $namespace,
+        ?string $redisUrl,
+        string $kernelCacheDirectory,
+        ?string $cacheDecryptionKey,
+    ): TagAwareAdapterInterface {
+        $marshaller = self::getMarshaller($cacheDecryptionKey);
+
         if ($redisUrl) {
             $client = RedisAdapter::createConnection($redisUrl);
             return new RedisTagAwareAdapter(
                 $client,
                 $namespace,
-                self::DEFAULT_LIFETIME_SECONDS
+                self::DEFAULT_LIFETIME_SECONDS,
+                $marshaller,
             );
         }
         return new FilesystemTagAwareAdapter(
             $namespace,
             self::DEFAULT_LIFETIME_SECONDS,
             $kernelCacheDirectory,
+            $marshaller,
         );
+    }
+
+    /**
+     * By default, encode and compress any data, but if an encryption key is
+     * provided we also encrypt data at rest.
+     */
+    protected static function getMarshaller(?string $key): MarshallerInterface
+    {
+        $marshaller = new DeflateMarshaller(new DefaultMarshaller());
+        if (!$key) {
+            return $marshaller;
+        }
+        return new SodiumMarshaller([$key], $marshaller);
     }
 }
