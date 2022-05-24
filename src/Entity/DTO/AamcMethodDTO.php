2a6a1f6b384aad55e9e55b543260d5bdbe7a44ac
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2022-05-23 17:05:26 -0700

    annotate AamcMethod DTO with openapi schema info.
    even though nelmio/swagger picks up the schema definitions right for the
    "primitive"/non-collection class members,
    declaring them in the schema explicitly give us control over the order
    in which they appear in the output.
    that's what we want, otherwise session-types would appear before id, and
    that's not what we want.

--- src/Entity/DTO/AamcMethodDTO.php
+++ src/Entity/DTO/AamcMethodDTO.php
@@ -5,9 +5,33 @@ declare(strict_types=1);
 namespace App\Entity\DTO;
 
 use App\Attribute as IA;
+use OpenApi\Attributes as OA;
 
 #[IA\DTO("aamcMethods")]
 #[IA\ExposeGraphQL]
+#[OA\Schema(
+    title: "AamcMethod",
+    properties: [
+        new OA\Property(
+            "id",
+            type:"string"
+        ),
+        new OA\Property(
+            "description",
+            type:"string"
+        ),
+        new OA\Property(
+            "active",
+            type:"boolean",
+            default: true
+        ),
+        new OA\Property(
+            "sessionTypes",
+            type:"array",
+            items: new OA\Items("string"),
+        )
+    ]
+)]
 class AamcMethodDTO
 {
     #[IA\Expose]
