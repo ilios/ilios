fa0337ea320c1413eb49ca0ee8b60b16d729aabc
Jonathan Johnson <jon.johnson@ucsf.edu>
2022-05-24 22:01:56 -0700

    Services for tagging and expiring DTOs
    All of the info we need to tag a DTO is in the relationships it
    containers, we can use these to cache the value and tag each entry. Then
    when anything is saved we can use those tags to invalidate each value.

---
+++ src/EventListener/ClearCachedDto.php
@@ -0,0 +1,61 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\EventListener;
+
+use App\Service\DTOCacheTagger;
+use Doctrine\ORM\Event\OnFlushEventArgs;
+use Doctrine\ORM\PersistentCollection;
+use Symfony\Contracts\Cache\TagAwareCacheInterface;
+
+class ClearCachedDto
+{
+    public function __construct(protected TagAwareCacheInterface $cache)
+    {
+    }
+
+    /**
+     * Clear cache entries referencing any tagged entity
+     */
+    public function onFlush(OnFlushEventArgs $eventArgs)
+    {
+        $entityManager = $eventArgs->getEntityManager();
+        $uow = $entityManager->getUnitOfWork();
+        $actions = [];
+
+        $actions['update'] = $uow->getScheduledEntityUpdates();
+        $actions['delete'] = $uow->getScheduledEntityDeletions();
+        $actions['create'] = $uow->getScheduledEntityInsertions();
+
+        $tags = [];
+        foreach ($actions as $action => $entities) {
+            foreach ($entities as $entity) {
+                $entityName = $entityManager->getMetadataFactory()->getMetadataFor($entity::class)->getName();
+                if ($action === 'create') {
+                    $tags[] = DTOCacheTagger::getTag($entityName, false);
+                } else {
+                    $id = (string) $entity;
+                    $tags[] = DTOCacheTagger::getTag($entityName, $id);
+                }
+            }
+        }
+
+        $collections = $uow->getScheduledCollectionUpdates();
+        /** @var PersistentCollection $col */
+        foreach ($collections as $col) {
+            foreach ($col->getDeleteDiff() as $entity) {
+                $entityName = $entityManager->getMetadataFactory()->getMetadataFor($entity::class)->getName();
+                $id = (string) $entity;
+                $tags[] = DTOCacheTagger::getTag($entityName, $id);
+            }
+            foreach ($col->getInsertDiff() as $entity) {
+                $entityName = $entityManager->getMetadataFactory()->getMetadataFor($entity::class)->getName();
+                $id = (string) $entity;
+                $tags[] = DTOCacheTagger::getTag($entityName, $id);
+            }
+        }
+
+        $this->cache->invalidateTags($tags);
+    }
+}
