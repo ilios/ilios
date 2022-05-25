750143ba3558414342bc264a93296034faac468f
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2022-05-25 13:19:30 -0700

    more API docs annotations on the AAMC Pcrs endpoint.

--- src/Controller/API/AamcPcrses.php
+++ src/Controller/API/AamcPcrses.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 
 namespace App\Controller\API;
 
+use App\Entity\DTO\AamcPcrsDTO;
 use App\Repository\AamcPcrsRepository;
 use App\Service\ApiRequestParser;
 use App\Service\ApiResponseBuilder;
+use Nelmio\ApiDocBundle\Annotation\Model;
 use OpenApi\Attributes as OA;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,7 +16,7 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
-#[OA\Tag(name:'Aamcpcrses')]
+#[OA\Tag(name:'AAMC PCRSes')]
 #[Route('/api/{version<v3>}/aamcpcrses')]
 class AamcPcrses extends AbstractApiController
 {
@@ -26,6 +28,31 @@ class AamcPcrses extends AbstractApiController
         '/{id}',
         methods: ['GET']
     )]
+    #[OA\Get(
+        path: '/api/{version}/aamcpcrses/{id}',
+        summary: 'Fetch a single AAMC PCRS.',
+        parameters: [
+            new OA\Parameter(name: 'version', description: 'API Version', in: 'path'),
+            new OA\Parameter(name: 'id', description: 'id', in: 'path', schema: new OA\Schema(type: 'integer'))
+        ]
+    )]
+    #[OA\Response(
+        response: '200',
+        description: 'A single AAMC PCRS.',
+        content: new OA\JsonContent(
+            properties: [
+                new OA\Property(
+                    'aamcPcrses',
+                    type: 'array',
+                    items: new OA\Items(
+                        ref: new Model(type: AamcPcrsDTO::class)
+                    )
+                )
+            ],
+            type: 'object'
+        )
+    )]
+    #[OA\Response(response: '404', description: 'Not found.')]
     public function getOne(
         string $version,
         string $id,
@@ -39,6 +66,65 @@ class AamcPcrses extends AbstractApiController
     #[Route(
         methods: ['GET']
     )]
+    #[OA\Get(
+        path: "/api/{version}/aamcpcrses",
+        summary: "Fetch all AAMC PCRSes.",
+        parameters: [
+            new OA\Parameter(name: 'version', description: 'API Version', in: 'path', required: false),
+            new OA\Parameter(
+                name: 'offset',
+                description: 'Offset',
+                in: 'query',
+                required: false,
+                schema: new OA\Schema(type: 'integer')
+            ),
+            new OA\Parameter(
+                name: 'limit',
+                description: 'Limit results',
+                in: 'query',
+                required: false,
+                schema: new OA\Schema(type: 'integer')
+            ),
+            new OA\Parameter(
+                name: 'order_by',
+                description: 'Order by fields. Must be an array, i.e. <code>&order_by[id]=ASC&order_by[x]=DESC</code>',
+                in: 'query',
+                required: false,
+                schema: new OA\Schema(
+                    type: 'array',
+                    items: new OA\Items(type: 'string'),
+                ),
+                style: "deepObject"
+            ),
+            new OA\Parameter(
+                name: 'filters',
+                description: 'Filter by fields. Must be an array, i.e. <code>&filters[id]=3</code>',
+                in: 'query',
+                required: false,
+                schema: new OA\Schema(
+                    type: 'array',
+                    items: new OA\Items(type: 'string'),
+                ),
+                style: "deepObject"
+            )
+        ]
+    )]
+    #[OA\Response(
+        response: '200',
+        description: 'An array of AAMC PCRSes.',
+        content: new OA\JsonContent(
+            properties: [
+                new OA\Property(
+                    'aamcPcrses',
+                    type: 'array',
+                    items: new OA\Items(
+                        ref: new Model(type: AamcPcrsDTO::class)
+                    )
+                )
+            ],
+            type: 'object'
+        )
+    )]
     public function getAll(
         string $version,
         Request $request,
@@ -49,6 +135,48 @@ class AamcPcrses extends AbstractApiController
     }
 
     #[Route(methods: ['POST'])]
+    #[OA\Post(
+        path: '/api/{version}/aamcpcrses',
+        summary: "Create AAMC PCRSes.",
+        parameters: [
+            new OA\Parameter(name: 'version', description: 'API Version', in: 'path'),
+            new OA\Parameter(
+                name: 'body',
+                in: 'body',
+                required: true,
+                schema: new OA\Schema(
+                    properties: [
+                        new OA\Property(
+                            'aamcPcrs',
+                            type: 'array',
+                            items: new OA\Items(
+                                ref: new Model(type: AamcPcrsDTO::class)
+                            )
+                        )
+                    ],
+                    type: 'object',
+                )
+            )
+        ]
+    )]
+    #[OA\Response(
+        response: '201',
+        description: 'An array of newly created AAMC PCRSes.',
+        content: new OA\JsonContent(
+            properties: [
+                new OA\Property(
+                    'aamcPcrs',
+                    type: 'object',
+                    items: new OA\Items(
+                        ref: new Model(type: AamcPcrsDTO::class)
+                    )
+                )
+            ],
+            type: 'object'
+        )
+    )]
+    #[OA\Response(response: '400', description: 'Bad Request Data.')]
+    #[OA\Response(response: '403', description: 'Access Denied.')]
     public function post(
         string $version,
         Request $request,
@@ -64,6 +192,48 @@ class AamcPcrses extends AbstractApiController
         '/{id}',
         methods: ['PUT']
     )]
+    #[OA\Put(
+        path: '/api/{version}/aamcpcrses/{id}',
+        summary: 'Update an AAMC PCRS.',
+        parameters: [
+            new OA\Parameter(name: 'version', description: 'API Version', in: 'path'),
+            new OA\Parameter(name: 'id', description: 'id', in: 'path'),
+            new OA\Parameter(
+                name: 'body',
+                in: 'body',
+                required: true,
+                schema: new OA\Schema(
+                    properties: [
+                        new OA\Property(
+                            'aamcPcrs',
+                            ref: new Model(type: AamcPcrsDTO::class),
+                            type: 'object'
+                        )
+                    ],
+                    type: 'object',
+                )
+            )
+        ]
+    )]
+    #[OA\Response(
+        response: '200',
+        description: 'The updated AAMC PCRS.',
+        content: new OA\JsonContent(
+            properties: [
+                new OA\Property(
+                    'aamcPcrs',
+                    type: 'array',
+                    items: new OA\Items(
+                        ref: new Model(type: AamcPcrsDTO::class)
+                    )
+                )
+            ],
+            type: 'object'
+        )
+    )]
+    #[OA\Response(response: '400', description: 'Bad Request Data.')]
+    #[OA\Response(response: '403', description: 'Access Denied.')]
+    #[OA\Response(response: '404', description: 'Not Found.')]
     public function put(
         string $version,
         string $id,
@@ -95,6 +265,21 @@ class AamcPcrses extends AbstractApiController
     #[Route(
         '/{id}',
         methods: ['DELETE']
+    )]
+    #[OA\Delete(
+        path: '/api/{version}/aamcpcrses/{id}',
+        summary: 'Delete an AAMC PCRS.',
+        parameters: [
+            new OA\Parameter(name: 'version', description: 'API Version', in: 'path'),
+            new OA\Parameter(name: 'id', description: 'id', in: 'path')
+        ]
+    )]
+    #[OA\Response(response: '204', description: 'Deleted.')]
+    #[OA\Response(response: '403', description: 'Access Denied.')]
+    #[OA\Response(response: '404', description: 'Not Found.')]
+    #[OA\Response(
+        response: '500',
+        description: 'Deletion failed (usually caused by non-cascading relationships)'
     )]
     public function delete(
         string $version,
