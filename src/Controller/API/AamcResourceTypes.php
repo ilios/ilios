8faec6861f7bf7a3f2b89d4596bb76364d8958dd
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2022-05-25 13:46:59 -0700

    add API doc annotations to AAMC resource types endpoint and DTO.

--- src/Controller/API/AamcResourceTypes.php
+++ src/Controller/API/AamcResourceTypes.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 
 namespace App\Controller\API;
 
+use App\Entity\DTO\AamcResourceTypeDTO;
 use App\Repository\AamcResourceTypeRepository;
 use App\Service\ApiRequestParser;
 use App\Service\ApiResponseBuilder;
+use Nelmio\ApiDocBundle\Annotation\Model;
 use OpenApi\Attributes as OA;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,7 +16,7 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
-#[OA\Tag(name:'Aamcresourcetypes')]
+#[OA\Tag(name:'AAMC resource types')]
 #[Route('/api/{version<v3>}/aamcresourcetypes')]
 class AamcResourceTypes extends AbstractApiController
 {
@@ -27,6 +29,31 @@ class AamcResourceTypes extends AbstractApiController
         '/{id}',
         methods: ['GET']
     )]
+    #[OA\Get(
+        path: '/api/{version}/aamcresourcetypes/{id}',
+        summary: 'Fetch a single AAMC resource type.',
+        parameters: [
+            new OA\Parameter(name: 'version', description: 'API Version', in: 'path'),
+            new OA\Parameter(name: 'id', description: 'id', in: 'path')
+        ]
+    )]
+    #[OA\Response(
+        response: '200',
+        description: 'A single AAMC resource type.',
+        content: new OA\JsonContent(
+            properties: [
+                new OA\Property(
+                    'aamcResourceTypes',
+                    type: 'array',
+                    items: new OA\Items(
+                        ref: new Model(type: AamcResourceTypeDTO::class)
+                    )
+                )
+            ],
+            type: 'object'
+        )
+    )]
+    #[OA\Response(response: '404', description: 'Not found.')]
     public function getOne(
         string $version,
         string $id,
@@ -40,6 +67,65 @@ class AamcResourceTypes extends AbstractApiController
     #[Route(
         methods: ['GET']
     )]
+    #[OA\Get(
+        path: "/api/{version}/aamcresourcetypes",
+        summary: "Fetch all AAMC resource types.",
+        parameters: [
+            new OA\Parameter(name: 'version', description: 'API Version', in: 'path', required: false),
+            new OA\Parameter(
+                name: 'offset',
+                description: 'Offset',
+                in: 'query',
+                required: false,
+                schema: new OA\Schema(type: 'integer')
+            ),
+            new OA\Parameter(
+                name: 'limit',
+                description: 'Limit results',
+                in: 'query',
+                required: false,
+                schema: new OA\Schema(type: 'integer')
+            ),
+            new OA\Parameter(
+                name: 'order_by',
+                description: 'Order by fields. Must be an array, i.e. <code>&order_by[id]=ASC&order_by[x]=DESC</code>',
+                in: 'query',
+                required: false,
+                schema: new OA\Schema(
+                    type: 'array',
+                    items: new OA\Items(type: 'string'),
+                ),
+                style: "deepObject"
+            ),
+            new OA\Parameter(
+                name: 'filters',
+                description: 'Filter by fields. Must be an array, i.e. <code>&filters[id]=3</code>',
+                in: 'query',
+                required: false,
+                schema: new OA\Schema(
+                    type: 'array',
+                    items: new OA\Items(type: 'string'),
+                ),
+                style: "deepObject"
+            )
+        ]
+    )]
+    #[OA\Response(
+        response: '200',
+        description: 'An array of AAMC resource types.',
+        content: new OA\JsonContent(
+            properties: [
+                new OA\Property(
+                    'aamcResourceTypes',
+                    type: 'array',
+                    items: new OA\Items(
+                        ref: new Model(type: AamcResourceTypeDTO::class)
+                    )
+                )
+            ],
+            type: 'object'
+        )
+    )]
     public function getAll(
         string $version,
         Request $request,
@@ -50,6 +136,48 @@ class AamcResourceTypes extends AbstractApiController
     }
 
     #[Route(methods: ['POST'])]
+    #[OA\Post(
+        path: '/api/{version}/aamcresourcetypes',
+        summary: "Create AAMC resource types.",
+        parameters: [
+            new OA\Parameter(name: 'version', description: 'API Version', in: 'path'),
+            new OA\Parameter(
+                name: 'body',
+                in: 'body',
+                required: true,
+                schema: new OA\Schema(
+                    properties: [
+                        new OA\Property(
+                            'aamcResourceType',
+                            type: 'array',
+                            items: new OA\Items(
+                                ref: new Model(type: AamcResourceTypeDTO::class)
+                            )
+                        )
+                    ],
+                    type: 'object',
+                )
+            )
+        ]
+    )]
+    #[OA\Response(
+        response: '201',
+        description: 'An array of newly created AAMC resource types.',
+        content: new OA\JsonContent(
+            properties: [
+                new OA\Property(
+                    'aamcResourceType',
+                    type: 'object',
+                    items: new OA\Items(
+                        ref: new Model(type: AamcResourceTypeDTO::class)
+                    )
+                )
+            ],
+            type: 'object'
+        )
+    )]
+    #[OA\Response(response: '400', description: 'Bad Request Data.')]
+    #[OA\Response(response: '403', description: 'Access Denied.')]
     public function post(
         string $version,
         Request $request,
@@ -65,6 +193,48 @@ class AamcResourceTypes extends AbstractApiController
         '/{id}',
         methods: ['PUT']
     )]
+    #[OA\Put(
+        path: '/api/{version}/aamcresourcetypes/{id}',
+        summary: 'Update an AAMC resource type.',
+        parameters: [
+            new OA\Parameter(name: 'version', description: 'API Version', in: 'path'),
+            new OA\Parameter(name: 'id', description: 'id', in: 'path'),
+            new OA\Parameter(
+                name: 'body',
+                in: 'body',
+                required: true,
+                schema: new OA\Schema(
+                    properties: [
+                        new OA\Property(
+                            'aamcResourceType',
+                            ref: new Model(type: AamcResourceTypeDTO::class),
+                            type: 'object'
+                        )
+                    ],
+                    type: 'object',
+                )
+            )
+        ]
+    )]
+    #[OA\Response(
+        response: '200',
+        description: 'The updated AAMC resource type.',
+        content: new OA\JsonContent(
+            properties: [
+                new OA\Property(
+                    'aamcResourceType',
+                    type: 'array',
+                    items: new OA\Items(
+                        ref: new Model(type: AamcResourceTypeDTO::class)
+                    )
+                )
+            ],
+            type: 'object'
+        )
+    )]
+    #[OA\Response(response: '400', description: 'Bad Request Data.')]
+    #[OA\Response(response: '403', description: 'Access Denied.')]
+    #[OA\Response(response: '404', description: 'Not Found.')]
     public function put(
         string $version,
         string $id,
@@ -96,6 +266,21 @@ class AamcResourceTypes extends AbstractApiController
     #[Route(
         '/{id}',
         methods: ['DELETE']
+    )]
+    #[OA\Delete(
+        path: '/api/{version}/aamcresourcetypes/{id}',
+        summary: 'Delete an AAMC resource type.',
+        parameters: [
+            new OA\Parameter(name: 'version', description: 'API Version', in: 'path'),
+            new OA\Parameter(name: 'id', description: 'id', in: 'path')
+        ]
+    )]
+    #[OA\Response(response: '204', description: 'Deleted.')]
+    #[OA\Response(response: '403', description: 'Access Denied.')]
+    #[OA\Response(response: '404', description: 'Not Found.')]
+    #[OA\Response(
+        response: '500',
+        description: 'Deletion failed (usually caused by non-cascading relationships).'
     )]
     public function delete(
         string $version,
