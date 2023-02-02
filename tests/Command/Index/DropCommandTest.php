32007c6f64cd8ca8d9b47d3c5f2b12c8ab1fc77b
Jonathan Johnson <jon.johnson@ucsf.edu>
2023-02-02 09:06:42 -0800

    Delete the correct messages
    These are in the messenger queue by queue name and not transport. Also
    fixed up the Drop test, it was borken.

--- tests/Command/Index/DropCommandTest.php
+++ tests/Command/Index/DropCommandTest.php
@@ -6,6 +6,8 @@ namespace App\Tests\Command\Index;
 
 use App\Command\Index\DropCommand;
 use App\Service\Index\Manager;
+use Doctrine\DBAL\Connection;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Console\Application;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Component\Console\Tester\CommandTester;
@@ -18,19 +20,16 @@ class DropCommandTest extends KernelTestCase
 {
     use m\Adapter\Phpunit\MockeryPHPUnitIntegration;
 
-    /** @var CommandTester */
-    protected $commandTester;
-
-    /** @var m\Mock */
-    protected $indexManager;
-
-
+    protected CommandTester $commandTester;
+    protected m\MockInterface $indexManager;
+    protected m\MockInterface $entityManager;
     public function setUp(): void
     {
         parent::setUp();
         $this->indexManager = m::mock(Manager::class);
+        $this->entityManager = m::mock(EntityManagerInterface::class);
 
-        $command = new DropCommand($this->indexManager);
+        $command = new DropCommand($this->indexManager, $this->entityManager);
         $kernel = self::bootKernel();
         $application = new Application($kernel);
         $application->add($command);
@@ -45,6 +44,7 @@ class DropCommandTest extends KernelTestCase
     {
         parent::tearDown();
         unset($this->indexManager);
+        unset($this->entityManager);
     }
 
     public function testRequireForce()
@@ -63,6 +63,11 @@ class DropCommandTest extends KernelTestCase
     public function testDropsWithForce()
     {
         $this->indexManager->shouldReceive('drop')->once();
+        $connection = m::mock(Connection::class);
+        $connection
+            ->shouldReceive('executeStatement')
+            ->with('DELETE FROM messenger_messages WHERE queue_name="default"');
+        $this->entityManager->shouldReceive('getConnection')->andReturn($connection);
 
         $this->commandTester->execute([
             'command' => DropCommand::COMMAND_NAME,
