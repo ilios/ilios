a429ab39a82e7c9593699e1ad3d86774a15b1551
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2023-09-13 15:54:26 -0700

    alias interface name so that class declaration fits within line limit lengths.
    appeasing the code sniffler.

--- tests/Endpoints/LearningMaterialTest.php
+++ tests/Endpoints/LearningMaterialTest.php
@@ -343,7 +343,7 @@ class LearningMaterialTest extends AbstractReadWriteEndpoint
         $data['filename'] = $responseData['filename'];
 
         $postData = $data;
-        $data['mimetype'] = 'text/x-php';
+        $data['mimetype'] = 'text/plain';
         $data['filesize'] = $filesize;
         unset($data['fileHash']);
         $response = $this->postTest($data, $postData);
--- tests/Endpoints/PendingUserUpdateTest.php
+++ tests/Endpoints/PendingUserUpdateTest.php
@@ -5,12 +5,13 @@ declare(strict_types=1);
 namespace App\Tests\Endpoints;
 
 use App\Tests\Fixture\LoadPendingUserUpdateData;
+use App\Tests\Endpoints\PutEndpointTestInterface as PutEndpointInterface;
 
 /**
  * PendingUserUpdate API endpoint Test.
  * @group api_3
  */
-class PendingUserUpdateTest extends AbstractReadEndpoint implements PutEndpointTestInterface, DeleteEndpointTestInterface
+class PendingUserUpdateTest extends AbstractReadEndpoint implements PutEndpointInterface, DeleteEndpointTestInterface
 {
     use PutEndpointTestable;
     use DeleteEndpointTestable;
