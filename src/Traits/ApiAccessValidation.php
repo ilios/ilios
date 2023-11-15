9d54f74c7c98f0bb981212783a4b99f9603cb538
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2023-11-15 13:39:15 -0800

    fortify code by explictly required declaring class member in trait.

--- src/Traits/ApiAccessValidation.php
+++ src/Traits/ApiAccessValidation.php
@@ -5,10 +5,13 @@ declare(strict_types=1);
 namespace App\Traits;
 
 use App\Classes\SessionUserInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 trait ApiAccessValidation
 {
+    protected TokenStorageInterface $tokenStorage;
+
     /**
      * Checks if the current session user is user-based.
      * If not, then an exception is raised.
