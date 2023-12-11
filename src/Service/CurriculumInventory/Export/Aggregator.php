425cf24938c7e34c1a9d954198ad13493dfec28a
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2023-12-11 14:53:25 -0800

    phpstan lvl4 fix: rm pointless code comments and data  checks.
    throws out the incomplete type hint code comment that we don't need.
    removes pointless data relationship checks - a ci report cannot exist
    without an associated program and school, period. so no need to check
    for that at all.

--- src/Service/CurriculumInventory/Export/Aggregator.php
+++ src/Service/CurriculumInventory/Export/Aggregator.php
@@ -118,18 +118,9 @@ class Aggregator
      */
     public function getData(CurriculumInventoryReportInterface $invReport): array
     {
-        // report validation
         $program = $invReport->getProgram();
-        if (! $program) {
-            throw new Exception('No program found for report with id = ' . $invReport->getId() . '.');
-        }
-
         $school  = $program->getSchool();
-        if (! $school) {
-            throw new Exception('No school found for program with id = ' . $program->getId() . '.');
-        }
 
-        /** @var CurriculumInventoryInstitutionInterface $institution */
         $institution = $this->institutionRepository->findOneBy(['school' => $school->getId()]);
         if (! $institution) {
             throw new Exception(
