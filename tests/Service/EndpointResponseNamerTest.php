7027db1ca1ae7e129b851f257972b69c24184a1d
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2024-01-12 10:30:59 -0800

    provides test coverage for EndpointResponseNamer service.

---
+++ tests/Service/EndpointResponseNamerTest.php
@@ -0,0 +1,73 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Service;
+
+use App\Entity\AamcMethod;
+use App\Service\EndpointResponseNamer;
+use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+/**
+ * @covers \App\Service\EndpointResponseNamer
+ */
+class EndpointResponseNamerTest extends KernelTestCase
+{
+    use MockeryPHPUnitIntegration;
+
+    protected EndpointResponseNamer $service;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        self::bootKernel();
+        $this->service = static::getContainer()->get(EndpointResponseNamer::class);
+    }
+
+    public function tearDown(): void
+    {
+        parent::tearDown();
+        unset($this->service);
+    }
+
+    /**
+     * @dataProvider getSingularNameProvider
+     */
+    public function testGetSingularName(string $object, string $expected): void
+    {
+        $this->assertEquals(
+            $expected,
+            $this->service->getSingularName($object),
+        );
+    }
+
+    /**
+     * @dataProvider getPluralNameProvider
+     */
+    public function testGetPluralName(string $object, string $expected): void
+    {
+        $this->assertEquals(
+            $expected,
+            $this->service->getPluralName($object),
+        );
+    }
+
+    public static function getSingularNameProvider(): array
+    {
+        return [
+            [ 'aamcmethods', 'aamcMethod' ],
+            [ 'aamcpcrses', 'aamcPcrs' ],
+            [ 'vocabularies', 'vocabulary' ],
+        ];
+    }
+
+    public static function getPluralNameProvider(): array
+    {
+        return [
+            [ 'aamcmethods', 'aamcMethods' ],
+            [ 'aamcpcrses', 'aamcPcrses' ],
+            [ 'vocabularies', 'vocabularies' ],
+        ];
+    }
+}
