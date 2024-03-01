febe12f512ef3255b7f535023e472a904b5352f9
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2024-03-01 09:32:57 -0800

    adds test coverage for prefix seed compiler pass.

---
+++ tests/DependencyInjection/PrefixSeedCompilerPassTest.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\DependencyInjection;
+
+use App\DependencyInjection\PrefixSeedCompilerPass;
+use App\Tests\TestCase;
+use Composer\InstalledVersions;
+use Mockery as m;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBag;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+
+/**
+ * @covers \App\DependencyInjection\PrefixSeedCompilerPass
+ */
+class PrefixSeedCompilerPassTest extends TestCase
+{
+    protected m\MockInterface $config;
+    protected PrefixSeedCompilerPass $pass;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->pass = new PrefixSeedCompilerPass();
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+        unset($this->pass);
+    }
+
+    public function testProcess(): void
+    {
+        $container = new ContainerBuilder();
+        $seed = 'seed123456';
+        $iliosVersion = InstalledVersions::getPrettyVersion(InstalledVersions::getRootPackage()['name']);
+        $container->setParameter('cache.prefix.seed', 'seed123456');
+        $this->pass->process($container);
+        $this->assertEquals(
+            $seed . $iliosVersion,
+            $container->getParameterBag()->resolveValue($container->getParameter('cache.prefix.seed'))
+        );
+    }
+}
