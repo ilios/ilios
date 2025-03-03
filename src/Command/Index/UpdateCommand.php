8299574d06407c27e215339b4d82016e01f32bd5
Jonathan Johnson <jon.johnson@ucsf.edu>
2025-03-02 23:17:18 -0800

    Add Missing Import
    Lost this in rebasing somewhere.

--- src/Command/Index/UpdateCommand.php
+++ src/Command/Index/UpdateCommand.php
@@ -12,6 +12,7 @@ use App\Repository\CourseRepository;
 use App\Repository\LearningMaterialRepository;
 use App\Repository\MeshDescriptorRepository;
 use App\Repository\UserRepository;
+use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
