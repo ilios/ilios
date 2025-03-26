f7ef2bbbaacb632af54920b1d4b011ba12300d48
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2025-03-26 10:23:45 -0700

    upgrades symfony recipe for phpunit from 9.6 to 10.0

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,8 +4,6 @@ use Symfony\Component\Dotenv\Dotenv;
 
 require dirname(__DIR__).'/vendor/autoload.php';
 
-(new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
-
-if ($_SERVER['APP_DEBUG']) {
-    umask(0000);
+if (method_exists(Dotenv::class, 'bootEnv')) {
+    (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
 }
