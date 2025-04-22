8307df85db939174b417636941997b562a45b8fd
Jonathan Johnson <jon.johnson@ucsf.edu>
2025-04-21 22:52:37 -0700

    Swap Argument Order
    These were in the wrong order and showing as a sonarqube warning.
    Swapped them.

--- tests/Service/Index/UsersTest.php
+++ tests/Service/Index/UsersTest.php
@@ -84,29 +84,29 @@ class UsersTest extends TestCase
             $filtered = array_filter($arr, 'is_array');
             $this->assertCount(4, $filtered);
 
-            $this->assertEquals($filtered[1]['id'], $user1->id);
-            $this->assertEquals($filtered[1]['firstName'], $user1->firstName);
-            $this->assertEquals($filtered[1]['lastName'], $user1->lastName);
-            $this->assertEquals($filtered[1]['middleName'], $user1->middleName);
-            $this->assertEquals($filtered[1]['displayName'], $user1->displayName);
-            $this->assertEquals($filtered[1]['email'], $user1->email);
-            $this->assertEquals($filtered[1]['campusId'], $user1->campusId);
-            $this->assertEquals($filtered[1]['username'], $user1->username);
-            $this->assertEquals($filtered[1]['enabled'], $user1->enabled);
-            $this->assertEquals($filtered[1]['fullName'], '13 first 13 middle 13 last');
-            $this->assertEquals($filtered[1]['fullNameLastFirst'], '13 last, 13 first 13 middle');
+            $this->assertEquals($user1->id, $filtered[1]['id']);
+            $this->assertEquals($user1->firstName, $filtered[1]['firstName']);
+            $this->assertEquals($user1->lastName, $filtered[1]['lastName']);
+            $this->assertEquals($user1->middleName, $filtered[1]['middleName']);
+            $this->assertEquals($user1->displayName, $filtered[1]['displayName']);
+            $this->assertEquals($user1->email, $filtered[1]['email']);
+            $this->assertEquals($user1->campusId, $filtered[1]['campusId']);
+            $this->assertEquals($user1->username, $filtered[1]['username']);
+            $this->assertEquals($user1->enabled, $filtered[1]['enabled']);
+            $this->assertEquals('13 first 13 middle 13 last', $filtered[1]['fullName']);
+            $this->assertEquals('13 last, 13 first 13 middle', $filtered[1]['fullNameLastFirst']);
 
-            $this->assertEquals($filtered[3]['id'], $user2->id);
-            $this->assertEquals($filtered[3]['firstName'], $user2->firstName);
-            $this->assertEquals($filtered[3]['lastName'], $user2->lastName);
-            $this->assertEquals($filtered[3]['middleName'], $user2->middleName);
-            $this->assertEquals($filtered[3]['displayName'], $user2->displayName);
-            $this->assertEquals($filtered[3]['email'], $user2->email);
-            $this->assertEquals($filtered[3]['campusId'], $user2->campusId);
-            $this->assertEquals($filtered[3]['username'], $user2->username);
-            $this->assertEquals($filtered[3]['enabled'], $user2->enabled);
-            $this->assertEquals($filtered[3]['fullName'], '11 first 11 middle 11 last');
-            $this->assertEquals($filtered[3]['fullNameLastFirst'], '11 last, 11 first 11 middle');
+            $this->assertEquals($user2->id, $filtered[3]['id']);
+            $this->assertEquals($user2->firstName, $filtered[3]['firstName']);
+            $this->assertEquals($user2->lastName, $filtered[3]['lastName']);
+            $this->assertEquals($user2->middleName, $filtered[3]['middleName']);
+            $this->assertEquals($user2->displayName, $filtered[3]['displayName']);
+            $this->assertEquals($user2->email, $filtered[3]['email']);
+            $this->assertEquals($user2->campusId, $filtered[3]['campusId']);
+            $this->assertEquals($user2->username, $filtered[3]['username']);
+            $this->assertEquals($user2->enabled, $filtered[3]['enabled']);
+            $this->assertEquals('11 first 11 middle 11 last', $filtered[3]['fullName']);
+            $this->assertEquals('11 last, 11 first 11 middle', $filtered[3]['fullNameLastFirst']);
 
             return true;
         })
