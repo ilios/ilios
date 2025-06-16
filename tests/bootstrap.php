766efa0fb33530cf04b354bae806f108c60f6f12
Jonathan Johnson <jon.johnson@ucsf.edu>
2025-06-16 16:49:42 -0700

    Appease phpstan
    Because Dotenv, as understood by phpstan, for sure has this method it
    doesn't like looking for it. This is a recipe file, so making this
    change as an individual commit to surface it later in any conflicts.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,7 +4,7 @@ use Symfony\Component\Dotenv\Dotenv;
 
 require dirname(__DIR__).'/vendor/autoload.php';
 
-if (method_exists(Dotenv::class, 'bootEnv')) {
+if (class_exists(Dotenv::class)) {
     (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
 }
 
