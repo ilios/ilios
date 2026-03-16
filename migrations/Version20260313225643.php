e9085d2c63e79a35288d6f9c417eec9502a1740b
Michael Chadwick <michael.chadwick@gmail.com>
2026-03-16 08:43:02 -0700

    created db migration for new lm accessibility attributes

---
+++ migrations/Version20260313225643.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ilios\Migrations;
+
+use App\Classes\MysqlMigration;
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20260313225643 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Adds accessibility columns to learning materials.';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE learning_material ADD accessibility_permission TINYINT DEFAULT NULL, ADD accessibility_rationale LONGTEXT DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE learning_material DROP accessibility_permission, DROP accessibility_rationale');
+    }
+}
