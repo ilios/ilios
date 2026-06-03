1e6af5a89e9b999ce658c7d8e2fbeb27813c3681
Michael Chadwick <michael.chadwick@gmail.com>
2026-06-03 15:18:15 -0700

    added test for GraphQL/FieldResolver

--- tests/Service/GraphQL/FieldResolverTest.php
+++ tests/Service/GraphQL/FieldResolverTest.php
@@ -7,6 +7,7 @@ namespace App\Tests\Service\GraphQL;
 use PHPUnit\Framework\Attributes\CoversClass;
 use App\Service\GraphQL\FieldResolver;
 use App\Tests\TestCase;
+use GraphQL\Type\Definition\ResolveInfo;
 
 /**
  * Class FieldResolverTest
@@ -14,24 +15,122 @@ use App\Tests\TestCase;
 #[CoversClass(FieldResolver::class)]
 final class FieldResolverTest extends TestCase
 {
-    public function testSomething(): void
+    private FieldResolver $resolver;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->resolver = new FieldResolver();
+    }
+
+    private function resolveInfo(string $fieldName): ResolveInfo
+    {
+        /** @var ResolveInfo $info */
+        $info = (new \ReflectionClass(ResolveInfo::class))
+            ->newInstanceWithoutConstructor();
+
+        $info->fieldName = $fieldName;
+
+        return $info;
+    }
+
+    public function testArraySourceWithExistingField(): void
+    {
+        $source = ['name' => 'Lothar'];
+        $result = ($this->resolver)(
+            $source,
+            [],
+            null,
+            $this->resolveInfo('name')
+        );
+
+        $this->assertSame('Lothar', $result);
+    }
+
+    public function testArraySourceWithoutField(): void
+    {
+        $source = ['other' => 'value'];
+        $result = ($this->resolver)(
+            $source,
+            [],
+            null,
+            $this->resolveInfo('missing')
+        );
+
+        $this->assertNull($result);
+    }
+
+    public function testObjectSourceWithProperty(): void
+    {
+        $obj = (object)['age' => 152];
+        $result = ($this->resolver)(
+            $obj,
+            [],
+            null,
+            $this->resolveInfo('age')
+        );
+
+        $this->assertSame(152, $result);
+    }
+
+    public function testObjectSourceWithoutProperty(): void
+    {
+        $obj = (object)['other' => 'x'];
+        $result = ($this->resolver)(
+            $obj,
+            [],
+            null,
+            $this->resolveInfo('nonexistent')
+        );
+
+        $this->assertNull($result);
+    }
+
+    public function testClosureIsInvokedAndReturned(): void
     {
         $source = [
-            "id" => 1,
-            "title" => 'Title 1',
-            "years" => [
-                2025,
-                2026,
-                2027,
-            ],
-            "roles" => [
-                'user',
-                'admin',
-            ],
+            'value' => static fn($src, $args, $ctx, $info) => 'computed-' . $src['id'],
+            'id'    => 7,
         ];
 
-        $fieldResolver = new FieldResolver();
+        $result = ($this->resolver)(
+            $source,
+            [],
+            null,
+            $this->resolveInfo('value')
+        );
 
-        $this->assertEquals('foo', 'foo');
+        $this->assertSame('computed-7', $result);
+    }
+
+    public function testClosureReceivesAllParameters(): void
+    {
+        $source = [
+            'data' => static fn($src, $a, $ctx, $info) => implode(
+                '|',
+                [
+                    json_encode($src),
+                    json_encode($a),
+                    json_encode($ctx),
+                    $info->fieldName,
+                ]
+            ),
+            'x'    => 1,
+        ];
+
+        $args     = ['argA' => 'foobar'];
+        $context  = ['user' => 'boofar'];
+
+        $result = ($this->resolver)(
+            $source,
+            $args,
+            $context,
+            $this->resolveInfo('data')
+        );
+
+        $this->assertStringContainsString('"x":1', $result);
+        $this->assertStringContainsString(json_encode($args), $result);
+        $this->assertStringContainsString(json_encode($context), $result);
+        $this->assertStringEndsWith('|data', $result);
     }
 }
