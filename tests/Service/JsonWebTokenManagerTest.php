2ec95f040d53962a94f6ef2bf21a30782a954ff0
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2026-06-09 15:39:42 -0700

    use the standard audience ("aud") JWT attribute to convey application scope instead of custom attrs.
    according to the JWT spec, audience could be an array of string, or a
    single string.
    we're currently only supporting the latter since that's all we need
    right now.
    
    see the spec for details on the audience claim attribute.
    https://www.rfc-editor.org/info/rfc7519/#section-4.1.3

--- tests/Service/JsonWebTokenManagerTest.php
+++ tests/Service/JsonWebTokenManagerTest.php
@@ -327,7 +327,7 @@ final class JsonWebTokenManagerTest extends KernelTestCase
         $this->sessionUserProvider->shouldReceive('createSessionUserFromUserId')->andReturn($mockSessionUser);
         $jwt = $this->obj->createUserTokenFromServiceToken($mockUser, $issuedWith, $applicationScope);
         $decoded = (array) JWT::decode($jwt, new Key(self::DEFAULT_SECRET_KEY, JsonWebTokenManager::SIGNING_ALGORITHM));
-        $this->assertEquals($applicationScope, $decoded[JsonWebTokenManager::APPLICATION_SCOPE_KEY]);
+        $this->assertEquals($applicationScope, $decoded['aud']);
         $this->assertEquals($issuedWith, $decoded[JsonWebTokenManager::ISSUED_WITH_KEY]);
     }
 }
