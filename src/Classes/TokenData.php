6d027a5e9adab7db52a6b63625d28a8090682642
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2026-08-12 19:31:51 -0700

    move data extraction from data object constructor into factory.

--- src/Classes/TokenData.php
+++ src/Classes/TokenData.php
@@ -14,94 +14,25 @@ readonly class TokenData
     public const int DEFAULT_REFRESH_LIMIT = 12;
     public const string DEFAULT_PERMISSIONS = 'user';
 
-    public int $userId;
-
-    public int $serviceTokenId;
-    public bool $isUserToken;
-    public bool $isServiceToken;
-
-    public DateTimeImmutable $issuedAt;
-    public DateTimeImmutable $expiresAt;
-
-    public bool $isRoot;
-    public bool $performsNonLearnerFunction;
-    public bool $canCreateOrUpdateUserInAnySchool;
-
-    public ?int $issuedWith;
-    public DateTimeImmutable $firstCreatedAt;
-    public int $refreshCount;
-    public int $refreshLimit;
-    public string $permissions;
-    public array $writeableSchoolIds;
-    public bool $canCreateUserTokensFromToken;
-    public array $audience;
-
-    public function __construct(array $data)
-    {
-        $this->userId = (int)$data['user_id'];
-        $this->serviceTokenId = (int)$data['token_id'];
-        $this->isUserToken = array_key_exists('user_id', $data);
-        $this->isServiceToken = array_key_exists('token_id', $data);
-        $issuedAt = DateTimeImmutable::createFromFormat('U', (string)$data['iat']);
-        assert($issuedAt instanceof DateTimeImmutable);
-        $this->issuedAt = $issuedAt;
-        $expiresAt = DateTimeImmutable::createFromFormat('U', (string)$data['exp']);
-        assert($expiresAt instanceof DateTimeImmutable);
-        $this->expiresAt = $expiresAt;
-        $this->isRoot = array_key_exists('is_root', $data) && $data['is_root'];
-        $this->performsNonLearnerFunction =
-            array_key_exists('performs_non_learner_function', $data) && $data['performs_non_learner_function'];
-        $this->canCreateOrUpdateUserInAnySchool =
-            array_key_exists('can_create_or_update_user_in_any_school', $data)
-            && $data['can_create_or_update_user_in_any_school'];
-        $this->issuedWith = array_key_exists('issued_with', $data) ? (int) $data['issued_with'] : null;
-        $firstCreatedAt =
-            array_key_exists('firstCreatedAt', $data)
-                ? DateTimeImmutable::createFromFormat('U', (string)$data['firstCreatedAt'])
-                : $this->issuedAt;
-        assert($firstCreatedAt instanceof DateTimeImmutable);
-        $this->firstCreatedAt = $firstCreatedAt;
-        $this->refreshCount = array_key_exists('refreshCount', $data) ? (int)$data['refreshCount'] : 0;
-        $this->refreshLimit =
-            array_key_exists('refreshLimit', $data)
-                ? (int)$data['refreshLimit']
-                : self::DEFAULT_REFRESH_LIMIT;
-        $this->permissions =
-            array_key_exists('permissions', $data)
-                ? (string) $data['permissions']
-                : self::DEFAULT_PERMISSIONS;
-        $this->writeableSchoolIds = $this->isServiceToken ? $this->getWriteableSchoolIds($data) : [];
-        $this->canCreateUserTokensFromToken =
-            $this->isServiceToken
-            && array_key_exists('can_generate_user_tokens', $data)
-            && $data['can_generate_user_tokens'];
-        $this->audience = $this->isServiceToken ? $this->getAudience($data) : [];
-    }
-
-    protected function getWriteableSchoolIds(array $data): array
-    {
-        if (!array_key_exists('writeable_schools', $data)) {
-            return [];
-        }
-        $rhett = $data['writeable_schools'];
-        assert(is_array($rhett));
-        return $rhett;
-    }
-
-    protected function getAudience(array $data): array
-    {
-        if (!array_key_exists('aud', $data)) {
-            return [];
-        }
-        $aud = $data['aud'];
-
-        if (is_string($aud)) {
-            $aud = [$aud];
-        }
-
-        assert(is_array($aud));
-
-        return $aud;
+    public function __construct(
+        public int $userId,
+        public int $serviceTokenId,
+        public bool $isUserToken,
+        public bool $isServiceToken,
+        public DateTimeImmutable $issuedAt,
+        public DateTimeImmutable $expiresAt,
+        public bool $isRoot,
+        public bool $performsNonLearnerFunction,
+        public bool $canCreateOrUpdateUserInAnySchool,
+        public ?int $issuedWith,
+        public DateTimeImmutable $firstCreatedAt,
+        public int $refreshCount,
+        public int $refreshLimit,
+        public string $permissions,
+        public array $writeableSchoolIds,
+        public bool $canCreateUserTokensFromToken,
+        public array $audience
+    ) {
     }
 
     public function toArray(): array
@@ -125,6 +56,7 @@ readonly class TokenData
         if (!is_null($this->issuedWith)) {
             $rhett['issuedWith'] = $this->issuedWith;
         }
+
         return $rhett;
     }
 }
--- src/Service/TokenDataFactory.php
+++ src/Service/TokenDataFactory.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Service;
 
 use App\Classes\TokenData;
+use DateTimeImmutable;
 
 /**
  * A factory that creates token data objects.
@@ -18,11 +19,93 @@ readonly class TokenDataFactory
     public function createFromJwt(string $jwt): TokenData
     {
         $data = $this->codec->decode($jwt);
+
         return $this->createFromArray($data);
     }
 
     public function createFromArray(array $data): TokenData
     {
-        return new TokenData($data);
+        $userId = (int)$data['user_id'];
+        $serviceTokenId = (int)$data['token_id'];
+        $isUserToken = array_key_exists('user_id', $data);
+        $isServiceToken = array_key_exists('token_id', $data);
+        $issuedAt = DateTimeImmutable::createFromFormat('U', (string)$data['iat']);
+        assert($issuedAt instanceof DateTimeImmutable);
+        $expiresAt = DateTimeImmutable::createFromFormat('U', (string)$data['exp']);
+        assert($expiresAt instanceof DateTimeImmutable);
+        $isRoot = array_key_exists('is_root', $data) && $data['is_root'];
+        $performsNonLearnerFunction =
+            array_key_exists('performs_non_learner_function', $data) && $data['performs_non_learner_function'];
+        $canCreateOrUpdateUserInAnySchool =
+            array_key_exists('can_create_or_update_user_in_any_school', $data)
+            && $data['can_create_or_update_user_in_any_school'];
+        $issuedWith = array_key_exists('issued_with', $data) ? (int)$data['issued_with'] : null;
+        $firstCreatedAt =
+            array_key_exists('firstCreatedAt', $data)
+                ? DateTimeImmutable::createFromFormat('U', (string)$data['firstCreatedAt'])
+                : $issuedAt;
+        assert($firstCreatedAt instanceof DateTimeImmutable);
+        $refreshCount = array_key_exists('refreshCount', $data) ? (int)$data['refreshCount'] : 0;
+        $refreshLimit =
+            array_key_exists('refreshLimit', $data)
+                ? (int)$data['refreshLimit']
+                : TokenData::DEFAULT_REFRESH_LIMIT;
+        $permissions =
+            array_key_exists('permissions', $data)
+                ? (string)$data['permissions']
+                : TokenData::DEFAULT_PERMISSIONS;
+        $writeableSchoolIds = $isServiceToken ? $this->getWriteableSchoolIds($data) : [];
+        $canCreateUserTokensFromToken =
+            $isServiceToken
+            && array_key_exists('can_generate_user_tokens', $data)
+            && $data['can_generate_user_tokens'];
+        $audience = $isServiceToken ? $this->getAudience($data) : [];
+
+        return new TokenData(
+            $userId,
+            $serviceTokenId,
+            $isUserToken,
+            $isServiceToken,
+            $issuedAt,
+            $expiresAt,
+            $isRoot,
+            $performsNonLearnerFunction,
+            $canCreateOrUpdateUserInAnySchool,
+            $issuedWith,
+            $firstCreatedAt,
+            $refreshCount,
+            $refreshLimit,
+            $permissions,
+            $writeableSchoolIds,
+            $canCreateUserTokensFromToken,
+            $audience
+        );
+    }
+
+    protected function getWriteableSchoolIds(array $data): array
+    {
+        if (!array_key_exists('writeable_schools', $data)) {
+            return [];
+        }
+        $rhett = $data['writeable_schools'];
+        assert(is_array($rhett));
+
+        return $rhett;
+    }
+
+    protected function getAudience(array $data): array
+    {
+        if (!array_key_exists('aud', $data)) {
+            return [];
+        }
+        $aud = $data['aud'];
+
+        if (is_string($aud)) {
+            $aud = [$aud];
+        }
+
+        assert(is_array($aud));
+
+        return $aud;
     }
 }
