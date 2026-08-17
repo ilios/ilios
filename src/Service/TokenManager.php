81bb979ef3ea7ba7e28c8581ba0649fecc45171a
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2026-08-17 13:05:45 -0700

    adds method for creating user tokens to manager.

--- src/Service/TokenManager.php
+++ src/Service/TokenManager.php
@@ -5,15 +5,41 @@ declare(strict_types=1);
 namespace App\Service;
 
 use App\Classes\ServiceToken;
+use App\Classes\SessionUserInterface;
 use App\Classes\UserToken;
+use DateInterval;
+use DateTimeImmutable;
 
 /**
  * Utilities for dealing with authentication tokens.
  */
 readonly class TokenManager
 {
-    public function __construct(protected TokenCodec $codec, protected TokenFactory $factory)
-    {
+    /**
+     * The default time-to-live (TTL) for generated user tokens.
+     */
+    public const string USER_TOKEN_DEFAULT_TTL = 'PT8H';
+
+    /**
+     * The maximum time-to-live for any generated token.
+     */
+    public const string TOKEN_MAX_TTL = 'P90D';
+
+    /**
+     * The default token issuer claim.
+     */
+    public const string TOKEN_ISSUER = 'ilios';
+
+    /**
+     * The default token audience claim.
+     */
+    public const string TOKEN_AUDIENCE = 'ilios';
+
+    public function __construct(
+        protected TokenCodec $codec,
+        protected TokenFactory $factory,
+        protected SessionUserPermissionChecker $sessionUserPermissionChecker,
+    ) {
     }
 
     /**
@@ -25,5 +51,45 @@ readonly class TokenManager
     {
         $data = $this->codec->decode($jwt);
         return $this->factory->create($data);
+    }
+
+    /**
+     * Creates a new user token for the given session user with a given time-to-live.
+     */
+    public function createUserTokenForSessionUser(
+        SessionUserInterface $sessionUser,
+        string $ttl = self::USER_TOKEN_DEFAULT_TTL
+    ): UserToken {
+        $issuedAt  = new DateTimeImmutable();
+        $expiresAt = $this->getTokenExpirationDate($issuedAt, $ttl, self::TOKEN_MAX_TTL);
+        $data = [
+            'iat' => $issuedAt->format('U'),
+            'exp' => $expiresAt->format('U'),
+            'iss' => self::TOKEN_ISSUER,
+            'aud' => self::TOKEN_AUDIENCE,
+            'user_id' => $sessionUser->getId(),
+            'is_root' => $sessionUser->isRoot(),
+            'performs_non_learner_function' => $sessionUser->performsNonLearnerFunction(),
+            'can_create_or_update_user_in_any_school' =>
+                $this->sessionUserPermissionChecker->canCreateOrUpdateUsersInAnySchool($sessionUser),
+            'firstCreatedAt' => $issuedAt->format('U'),
+            'refreshCount' => 0,
+        ];
+        return $this->factory->create($data);
+    }
+
+    /**
+     * Creates and returns a token expiration date.
+     * The expiration date will be offset from the given issued-at date by the given time-to-live,
+     * but not exceeding the given maximum time-to-live.
+     */
+    protected function getTokenExpirationDate(
+        DateTimeImmutable $issuedAt,
+        string $ttl,
+        string $maxTtl
+    ): DateTimeImmutable {
+        $expirationDate = $issuedAt->add(new DateInterval($ttl));
+        $maximumExpirationDate = $issuedAt->add(new DateInterval($maxTtl));
+        return min($expirationDate, $maximumExpirationDate);
     }
 }
--- tests/Service/TokenManagerTest.php
+++ tests/Service/TokenManagerTest.php
@@ -5,8 +5,10 @@ declare(strict_types=1);
 namespace App\Tests\Service;
 
 use App\Classes\ServiceToken;
+use App\Classes\SessionUserInterface;
 use App\Classes\UserToken;
 use App\Service\SecretManager;
+use App\Service\SessionUserPermissionChecker;
 use App\Service\TokenCodec;
 use App\Service\TokenFactory;
 use App\Service\TokenManager;
@@ -15,10 +17,12 @@ use DateInterval;
 use DateTimeImmutable;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\DataProvider;
+use Mockery as m;
 
 #[CoversClass(TokenManager::class)]
 final class TokenManagerTest extends TestCase
 {
+    protected m\MockInterface $sessionUserPermissionChecker;
     protected TokenCodec $codec;
     protected TokenFactory $factory;
     protected TokenManager $manager;
@@ -26,13 +30,19 @@ final class TokenManagerTest extends TestCase
     public function setUp(): void
     {
         parent::setUp();
+        $this->sessionUserPermissionChecker = m::mock(SessionUserPermissionChecker::class);
         $this->codec = new TokenCodec(new SecretManager('TheSignOfTheSouthernCross', 'ChildrenOfTheSea'));
         $this->factory = new TokenFactory();
-        $this->manager = new TokenManager($this->codec, $this->factory);
+        $this->manager = new TokenManager(
+            $this->codec,
+            $this->factory,
+            $this->sessionUserPermissionChecker
+        );
     }
 
     public function tearDown(): void
     {
+        unset($this->sessionUserPermissionChecker);
         unset($this->manager);
         unset($this->codec);
         unset($this->factory);
@@ -71,5 +81,66 @@ final class TokenManagerTest extends TestCase
                 ServiceToken::class,
             ],
         ];
+    }
+
+    #[DataProvider('createUserTokenFromSessionUserProvider')]
+    public function testCreateUserTokenFromSessionUser(
+        int $userId,
+        bool $isRoot,
+        bool $performsNonLearnerFunction,
+        string $ttl,
+    ): void {
+        $sessionUserMock = m::mock(SessionUserInterface::class);
+        $sessionUserMock->shouldReceive('getId')->andReturn($userId);
+        $sessionUserMock->shouldReceive('performsNonLearnerFunction')->andReturn($performsNonLearnerFunction);
+        $sessionUserMock->shouldReceive('isRoot')->andReturn($isRoot);
+        $this->sessionUserPermissionChecker
+            ->shouldReceive('canCreateOrUpdateUsersInAnySchool')
+            ->with($sessionUserMock)
+            ->andReturn(true);
+        $token = $this->manager->createUserTokenForSessionUser($sessionUserMock, $ttl);
+
+        $this->assertSame(
+            $token->expiresAt->getTimestamp(),
+            $token->issuedAt->add(new DateInterval($ttl))->getTimestamp()
+        );
+        $this->assertSame($userId, $token->userId);
+        $this->assertSame('ilios', $token->issuer);
+        $this->assertSame(['ilios'], $token->audience);
+        $this->assertSame($token->firstCreatedAt->getTimestamp(), $token->issuedAt->getTimestamp());
+        $this->assertSame($isRoot, $token->isRoot);
+        $this->assertSame($performsNonLearnerFunction, $token->performsNonLearnerFunction);
+        $this->assertSame(0, $token->refreshLimit);
+        $this->assertSame(0, $token->refreshCount);
+    }
+
+    public static function createUserTokenFromSessionUserProvider(): array
+    {
+        return [
+            [10, false, true, 'PT8H'],
+            [20, true, false, 'P30D'],
+        ];
+    }
+
+    public function testCreateUserTokenFromSessionWithTtlExceedingMaximum(): void
+    {
+        $ttl = 'P91D';
+        $ttlInterval = new DateInterval($ttl);
+        $maxTtlInterval = new DateInterval(TokenManager::TOKEN_MAX_TTL);
+        assert($maxTtlInterval > $ttlInterval);
+        $sessionUserMock = m::mock(SessionUserInterface::class);
+        $sessionUserMock->shouldReceive('getId')->andReturn(1);
+        $sessionUserMock->shouldReceive('performsNonLearnerFunction')->andReturn(false);
+        $sessionUserMock->shouldReceive('isRoot')->andReturn(true);
+        $this->sessionUserPermissionChecker
+            ->shouldReceive('canCreateOrUpdateUsersInAnySchool')
+            ->with($sessionUserMock)
+            ->andReturn(true);
+        $token = $this->manager->createUserTokenForSessionUser($sessionUserMock, $ttl);
+
+        $this->assertSame(
+            $token->expiresAt->getTimestamp(),
+            $token->issuedAt->add($maxTtlInterval)->getTimestamp()
+        );
     }
 }
